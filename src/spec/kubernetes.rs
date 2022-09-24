6516a1275d16ff63ec3ecdf322424ef72c82fdb2
Xudong Sun <xudongs1@vmware.com>
2022-09-24 16:52:45 -0500

    use is_None and is_Some for messages in kubernetes.rs
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/spec/kubernetes.rs
+++ src/spec/kubernetes.rs
@@ -4,7 +4,7 @@
 #[allow(unused_imports)]
 use builtin_macros::*;
 #[allow(unused_imports)]
-use builtin::{exists, requires, ensures, equal};
+use builtin::*;
 #[allow(unused_imports)]
 use crate::apis::*;
 #[allow(unused_imports)]
@@ -14,6 +14,7 @@ use crate::pervasive::{*, option::Option, set::*};
 #[allow(unused_imports)]
 use crate::custom_controller_logic::*;
 
+verus! {
 
 pub struct KubernetesConstants {
 
@@ -53,7 +54,7 @@ pub struct KubernetesVariables {
     /// should be sent to the controller
     /// There is no watch notification if the controller's request does not change any state
     /// (e.g., deleting a non-existing object)
-    /// Kubernetes has to send out this watch notifiaction before handling the next request
+    /// Kubernetes has to send out this watch notification before handling the next request
     pub pending_api_watch_notification: Option<APIWatchNotification>,
 }
 
@@ -61,10 +62,8 @@ impl KubernetesVariables {
     #[spec] #[verifier(publish)]
     pub fn well_formed(&self, c:KubernetesConstants) -> bool {
         self.cluster_state.well_formed()
-        && match self.pending_api_watch_notification {
-            Option::None => true,
-            Option::Some(api_watch_notification) => api_watch_notification.well_formed(),
-        }
+        && (self.pending_api_watch_notification.is_None()
+            || (self.pending_api_watch_notification.is_Some() && self.pending_api_watch_notification.get_Some_0().well_formed()))
     }
 }
 
@@ -105,47 +104,43 @@ pub fn handle_workload_submission(c: KubernetesConstants, v: KubernetesVariables
 
     all_well_formed(c, v, v_prime, message_ops)
     && equal(v.pending_api_watch_notification, Option::None)
-    && match message_ops.recv {
-        Option::None => false,
-        Option::Some(message_to_recv) => {
-            match message_to_recv {
-                Message::WorkloadSubmission(api_op_request) => {
-                    let success = kubernetes_api_op_result(v.cluster_state, v_prime.cluster_state, api_op_request.api_op);
-                    if success {
-                        match api_op_request.api_op {
-                            APIOp::Create{object_key, object} =>
-                                is_cr_type(object_key.object_type)
-                                && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: object,
-                                    delta_type: WatchDeltaType::Create,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
-                            APIOp::Update{object_key, object} =>
-                                is_cr_type(object_key.object_type)
-                                && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: object,
-                                    delta_type: WatchDeltaType::Update,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
-                            APIOp::Delete{object_key} =>
-                                is_cr_type(object_key.object_type)
-                                && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: v.cluster_state.get(object_key),
-                                    delta_type: WatchDeltaType::Delete,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
-                            _ => false,
-                        }
-                    } else {
-                        equal(v_prime.pending_api_watch_notification, Option::None)
-                        && equal(v.cluster_state, v_prime.cluster_state)
-                    }
-                },
-                _ => false,
+    && message_ops.send.is_None()
+    && message_ops.recv.is_Some()
+    && match message_ops.recv.get_Some_0() {
+        Message::WorkloadSubmission(api_op_request) => {
+            let success = kubernetes_api_op_result(v.cluster_state, v_prime.cluster_state, api_op_request.api_op);
+            if success {
+                match api_op_request.api_op {
+                    APIOp::Create{object_key, object} =>
+                        is_cr_type(object_key.object_type)
+                        && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: object,
+                            delta_type: WatchDeltaType::Create,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
+                    APIOp::Update{object_key, object} =>
+                        is_cr_type(object_key.object_type)
+                        && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: object,
+                            delta_type: WatchDeltaType::Update,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
+                    APIOp::Delete{object_key} =>
+                        is_cr_type(object_key.object_type)
+                        && equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: v.cluster_state.get(object_key),
+                            delta_type: WatchDeltaType::Delete,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op),
+                    _ => false,
+                }
+            } else {
+                equal(v_prime.pending_api_watch_notification, Option::None)
+                && equal(v.cluster_state, v_prime.cluster_state)
             }
-        }
+        },
+        _ => false,
     }
-    && equal(message_ops.send, Option::None)
 }
 
 #[spec] #[verifier(publish)]
@@ -159,91 +154,83 @@ pub fn handle_api_op_request(c: KubernetesConstants, v: KubernetesVariables, v_p
     // TODO: we should allow processing api op while there are pending responses
     all_well_formed(c, v, v_prime, message_ops)
     && equal(v.pending_api_watch_notification, Option::None)
-    && match message_ops.recv {
-        Option::None => false,
-        Option::Some(message_to_recv) => {
-            match message_to_recv {
-                Message::APIOpRequest(api_op_request) => {
-                    // We should also validate whether this transition is allowed
-                    let success = kubernetes_api_op_result(v.cluster_state, v_prime.cluster_state, api_op_request.api_op);
-                    if success {
-                        match api_op_request.api_op {
-                            APIOp::Get{object_key} =>
-                                equal(v_prime.pending_api_watch_notification, Option::None)
-                                && equal(v.cluster_state, v_prime.cluster_state)
-                                && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
-                                    success:success,
-                                    api_op_request:api_op_request,
-                                    object:v.cluster_state.get(object_key),
-                                }))),
-                            APIOp::Create{object_key, object} =>
-                                equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: object,
-                                    delta_type: WatchDeltaType::Create,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
-                                && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
-                                    success:success,
-                                    api_op_request:api_op_request,
-                                    object:object,
-                                }))),
-                            APIOp::Update{object_key, object} =>
-                                equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: object,
-                                    delta_type: WatchDeltaType::Update,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
-                                && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
-                                    success:success,
-                                    api_op_request:api_op_request,
-                                    object:object,
-                                }))),
-                            APIOp::Delete{object_key} =>
-                                equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
-                                    object: v.cluster_state.get(object_key),
-                                    delta_type: WatchDeltaType::Delete,
-                                }))
-                                && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
-                                && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
-                                    success:success,
-                                    api_op_request:api_op_request,
-                                    object:v.cluster_state.get(object_key),
-                                }))),
-                        }
-                    } else {
+    && message_ops.recv.is_Some()
+    && match message_ops.recv.get_Some_0() {
+        Message::APIOpRequest(api_op_request) => {
+            // We should also validate whether this transition is allowed
+            let success = kubernetes_api_op_result(v.cluster_state, v_prime.cluster_state, api_op_request.api_op);
+            if success {
+                match api_op_request.api_op {
+                    APIOp::Get{object_key} =>
                         equal(v_prime.pending_api_watch_notification, Option::None)
                         && equal(v.cluster_state, v_prime.cluster_state)
                         && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
                             success:success,
                             api_op_request:api_op_request,
-                            object:KubernetesObject::None,
-                        })))
-                    }
-                },
-                _ => false,
+                            object:v.cluster_state.get(object_key),
+                        }))),
+                    APIOp::Create{object_key, object} =>
+                        equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: object,
+                            delta_type: WatchDeltaType::Create,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
+                        && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
+                            success:success,
+                            api_op_request:api_op_request,
+                            object:object,
+                        }))),
+                    APIOp::Update{object_key, object} =>
+                        equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: object,
+                            delta_type: WatchDeltaType::Update,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
+                        && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
+                            success:success,
+                            api_op_request:api_op_request,
+                            object:object,
+                        }))),
+                    APIOp::Delete{object_key} =>
+                        equal(v_prime.pending_api_watch_notification, Option::Some(APIWatchNotification{
+                            object: v.cluster_state.get(object_key),
+                            delta_type: WatchDeltaType::Delete,
+                        }))
+                        && state_transition_by_api_op(v.cluster_state, v_prime.cluster_state, api_op_request.api_op)
+                        && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
+                            success:success,
+                            api_op_request:api_op_request,
+                            object:v.cluster_state.get(object_key),
+                        }))),
+                }
+            } else {
+                equal(v_prime.pending_api_watch_notification, Option::None)
+                && equal(v.cluster_state, v_prime.cluster_state)
+                && equal(message_ops.send, Option::Some(Message::APIOpResponse(APIOpResponse{
+                    success:success,
+                    api_op_request:api_op_request,
+                    object:KubernetesObject::None,
+                })))
             }
-        }
+        },
+        _ => false,
     }
 }
 
 #[spec] #[verifier(publish)]
 pub fn send_api_watch_notification(c: KubernetesConstants, v: KubernetesVariables, v_prime: KubernetesVariables, message_ops: MessageOps) -> bool {
     all_well_formed(c, v, v_prime, message_ops)
-    && equal(v, KubernetesVariables{
+    && v === KubernetesVariables{
         pending_api_watch_notification: v.pending_api_watch_notification,
         ..v_prime
-    })
-    && !equal(v.pending_api_watch_notification, Option::None)
-    && equal(v_prime.pending_api_watch_notification, Option::None)
-    && equal(message_ops.recv, Option::None)
-    && match message_ops.send {
-        Option::None => false,
-        Option::Some(message_to_send) => {
-            match message_to_send {
-                Message::APIWatchNotification(api_watch_notification) => equal(Option::Some(api_watch_notification), v.pending_api_watch_notification),
-                _ => false,
-            }
-        }
+    }
+    && v.pending_api_watch_notification !== Option::None
+    && v_prime.pending_api_watch_notification === Option::None
+    && message_ops.recv.is_None()
+    && message_ops.send.is_Some()
+    && match message_ops.send.get_Some_0() {
+        Message::APIWatchNotification(api_watch_notification) => Option::Some(api_watch_notification) === v.pending_api_watch_notification,
+        _ => false,
     }
 }
 
@@ -259,4 +246,6 @@ pub fn next_step(c: KubernetesConstants, v: KubernetesVariables, v_prime: Kubern
 #[spec] #[verifier(publish)]
 pub fn next(c: KubernetesConstants, v: KubernetesVariables, v_prime: KubernetesVariables, message_ops: MessageOps) -> bool {
     exists(|step: KubernetesStep| next_step(c, v, v_prime, message_ops, step))
+}
+
 }
