13f8376a5b86f9d70703bfcdaac25b69ceb6d45e
Xudong Sun <xudongs1@vmware.com>
2022-09-28 14:17:52 -0500

    move kubernetes resource data types to kubernetes_core_resources.rs
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/spec/apis.rs
+++ src/spec/apis.rs
@@ -20,65 +20,6 @@ pub enum ReconcileStep {
     Done,
 }
 
-// TODO: we should strictly follow the object layout in
-// https://github.com/kubernetes/kubernetes/blob/release-1.25/staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
-
-#[derive(PartialEq, Eq)]
-pub struct ObjectMetaL {
-    pub name: StringL,
-    pub namespace: StringL,
-    // TODO: use OwnerReferences instead of owner_key
-    pub owner_key: ObjectKey,
-}
-
-#[derive(PartialEq, Eq)]
-pub struct PodL {
-    pub metadata: ObjectMetaL,
-}
-
-impl PodL {
-    pub open spec fn key(&self) -> ObjectKey {
-        ObjectKey{
-            object_type: StringL::Pod,
-            namespace: self.metadata.namespace,
-            name: self.metadata.name,
-        }
-    }
-}
-
-#[derive(PartialEq, Eq)]
-pub struct ConfigMapL {
-    pub metadata: ObjectMetaL,
-}
-
-impl ConfigMapL {
-    pub open spec fn key(&self) -> ObjectKey {
-        ObjectKey{
-            object_type: StringL::ConfigMap,
-            namespace: self.metadata.namespace,
-            name: self.metadata.name,
-        }
-    }
-}
-
-#[derive(PartialEq, Eq)]
-#[is_variant]
-pub enum KubernetesObject {
-    Pod(PodL),
-    ConfigMap(ConfigMapL),
-    CustomResourceObject(CustomResourceObject),
-}
-
-impl KubernetesObject {
-    pub open spec fn key(&self) -> ObjectKey {
-        match *self {
-            KubernetesObject::Pod(p) => p.key(),
-            KubernetesObject::ConfigMap(cm) => cm.key(),
-            KubernetesObject::CustomResourceObject(cro) => cro.key(),
-        }
-    }
-}
-
 pub struct ClusterStateImpl {
     pub state: Dict<ObjectKey, KubernetesObject>
 }
