f95f1fe14fb30aa3525872dc2ab38654e1c10bdd
Xudong Sun <xudongs1@vmware.com>
2022-09-28 19:44:17 -0500

    Implement temporal operators using Set
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/spec/liveness_property.rs
+++ src/spec/liveness_property.rs
@@ -17,12 +17,10 @@ use crate::distributed_system::*;
 
 #[allow(unused_imports)]
 use crate::pervasive::seq::*;
+#[allow(unused_imports)]
+use crate::pervasive::set::*;
 
 verus! {
-
-// The following code does not compile as Verus reports many errors
-// including not support returning closure type,
-// DSConstants not implementing Copy/Clone traits and so on.
 
 pub open spec fn configmapgenerator_exists_in_etcd(c: DSConstants, v: DSVariables) -> bool {
     exists |object_key:ObjectKey|
@@ -35,54 +33,82 @@ pub open spec fn two_configmaps_exist_in_etcd(c: DSConstants, v: DSVariables) ->
     && v.kubernetes_variables.cluster_state.contains(configmap_2_key())
 }
 
-type ClosureType = Box<Fn(i32) -> i32>;
-
-pub open spec fn lift_state
-(state_pred: impl Fn(DSConstants, DSVariables) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| state_pred(c, behavior[0])
+pub struct Execution {
+    pub constants: DSConstants,
+    pub variables_seq: Seq<DSVariables>, // Pretend it is infinite
 }
 
-pub open spec fn temp_not
-(temp_pred: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| !temp_pred(c, behavior)
+type TempPred = Set<Execution>;
+
+pub open spec fn lift_state(state_pred: impl Fn(DSConstants, DSVariables) -> bool) -> TempPred {
+    Set::new(|ex: Execution| state_pred(ex.constants, ex.variables_seq[0]))
 }
 
-pub open spec fn temp_and
-(temp_pred_a: impl Fn(DSConstants, Seq<DSVariables>) -> bool, temp_pred_b: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| {
-        temp_pred_a(c, behavior) && temp_pred_b(c, behavior)
+pub open spec fn lift_action(action_pred: impl Fn(DSConstants, DSVariables, DSVariables) -> bool) -> TempPred {
+    Set::new(|ex: Execution| action_pred(ex.constants, ex.variables_seq[0], ex.variables_seq[1]))
+}
+
+pub open spec fn drop(ex: Execution, idx: nat) -> Execution {
+    Execution {
+        constants: ex.constants,
+        variables_seq: ex.variables_seq.subrange(idx as int, ex.variables_seq.len() as int)
     }
 }
 
-pub open spec fn temp_or
-(temp_pred_a: impl Fn(DSConstants, Seq<DSVariables>) -> bool, temp_pred_b: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| temp_not(temp_and(temp_not(temp_pred_a(c, behavior)), temp_not(temp_pred_b(c, behavior))))
+pub open spec fn temp_always(temp_pred: TempPred) -> TempPred {
+    Set::new(|ex:Execution| forall |i:nat| i<ex.variables_seq.len() && #[trigger] temp_pred.contains(drop(ex, i)))
 }
 
-pub open spec fn temp_implies
-(temp_pred_a: impl Fn(DSConstants, Seq<DSVariables>) -> bool, temp_pred_b: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| temp_or(temp_not(temp_pred_a(c, behavior)), temp_pred_b(c, behavior))
+pub open spec fn temp_not(temp_pred: TempPred) -> TempPred {
+    // Set::new(|ex:Execution| !temp_pred.contains(ex))
+    temp_pred.complement()
 }
 
-pub open spec fn temp_always
-(temp_pred: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| {
-        let len = behavior.clone().len(); // Verus complains that behavior does not implement Clone/Copy
-        return forall |i:int| i<len && temp_pred(c, behavior.subrange(i, len))
-    }
+pub open spec fn temp_and(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
+    temp_pred_a.intersect(temp_pred_b)
 }
 
-pub open spec fn temp_eventually
-(temp_pred: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| temp_not(temp_always(temp_not(temp_pred(c, behavior))))
+pub open spec fn temp_or(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
+    temp_pred_a.union(temp_pred_b)
 }
 
-pub open spec fn leads_to
-(temp_pred_a: impl Fn(DSConstants, Seq<DSVariables>) -> bool, temp_pred_b: impl Fn(DSConstants, Seq<DSVariables>) -> bool) -> impl Fn(DSConstants, Seq<DSVariables>) -> bool {
-    move |c: DSConstants, behavior: Seq<DSVariables>| temp_implies(temp_pred_a(c, behavior), temp_eventually(temp_pred_b(c, behavior)))
+pub open spec fn temp_eventually(temp_pred: TempPred) -> TempPred {
+    temp_not(temp_always(temp_not(temp_pred)))
+    // Set::new(|ex:Execution| exists |i:nat| i<ex.variables_seq.len() && #[trigger] temp_pred.contains(drop(ex, i)))
 }
 
-// temp_always(temp_leads(temp_always(configmapgenerator_exists_in_etcd()), two_configmaps_exist_in_etcd()))
+pub open spec fn temp_implies(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
+    temp_or(temp_not(temp_pred_a), temp_pred_b)
+}
 
+pub open spec fn temp_leads_to(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
+    temp_always(temp_implies(temp_pred_a, temp_eventually(temp_pred_b)))
+}
+
+// Verus will report the following error for the exists in enabled
+// error: Could not automatically infer triggers for this quantifer.  Use #[trigger] annotations to manually mark trigger terms instead.
+// And if we set action as trigger, it reports another error
+// error: trigger must be a function call, a field access, or a bitwise operator
+spec fn enabled(action: impl Fn(DSConstants, DSVariables, DSVariables) -> bool) -> TempPred {
+    lift_state(|constants: DSConstants, variables: DSVariables|
+        exists |new_variables: DSVariables| action(constants, variables, new_variables)
+    )
+}
+
+spec fn weak_fairness(action: impl Fn(DSConstants, DSVariables, DSVariables) -> bool) -> TempPred {
+    temp_leads_to(temp_always(enabled(action)), lift_action(action))
+    // temp_always(temp_implies(temp_always(enabled(action)), temp_eventually(lift_action(action))))
+}
+
+
+// Same error as enabled
+spec fn enabled2(action: impl Fn(DSConstants, DSVariables, DSVariables) -> bool, constants: DSConstants, variables: DSVariables) -> bool {
+    exists |new_variables: DSVariables| action(constants, variables, new_variables)
+}
+
+spec fn weak_fairness2(action: impl Fn(DSConstants, DSVariables, DSVariables) -> bool) -> TempPred {
+    temp_leads_to(temp_always(lift_state(|constants: DSConstants, variables: DSVariables| enabled2(action, constants, variables))),
+        lift_action(action))
+}
 
 }
