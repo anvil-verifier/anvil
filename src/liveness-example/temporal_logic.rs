a4d2ac6aa274e0bbac510b71cca5d612f3fa6688
Xudong Sun <xudongs1@vmware.com>
2022-09-29 16:54:30 -0500

    simplify Execution as Seq<SimpleState>
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/liveness-example/temporal_logic.rs
+++ src/liveness-example/temporal_logic.rs
@@ -16,28 +16,24 @@ use builtin_macros::*;
 
 verus! {
 
-pub struct Execution {
-    pub states: Seq<SimpleState>, // Pretend it is infinite
-}
+type Execution = Seq<SimpleState>;
 
 type TempPred = Set<Execution>;
 
 pub open spec fn lift_state(state_pred: impl Fn(SimpleState) -> bool) -> TempPred {
-    Set::new(|ex: Execution| state_pred(ex.states[0]))
+    Set::new(|ex: Execution| state_pred(ex[0]))
 }
 
 pub open spec fn lift_action(action_pred: impl Fn(SimpleState, SimpleState) -> bool) -> TempPred {
-    Set::new(|ex: Execution| action_pred(ex.states[0], ex.states[1]))
+    Set::new(|ex: Execution| action_pred(ex[0], ex[1]))
 }
 
 pub open spec fn drop(ex: Execution, idx: nat) -> Execution {
-    Execution {
-        states: ex.states.subrange(idx as int, ex.states.len() as int)
-    }
+    ex.subrange(idx as int, ex.len() as int)
 }
 
 pub open spec fn always(temp_pred: TempPred) -> TempPred {
-    Set::new(|ex:Execution| forall |i:nat| i<ex.states.len() && #[trigger] temp_pred.contains(drop(ex, i)))
+    Set::new(|ex:Execution| forall |i:nat| i<ex.len() && #[trigger] temp_pred.contains(drop(ex, i)))
 }
 
 pub open spec fn not(temp_pred: TempPred) -> TempPred {
@@ -54,7 +50,7 @@ pub open spec fn or(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
 
 pub open spec fn eventually(temp_pred: TempPred) -> TempPred {
     not(always(not(temp_pred)))
-    // Set::new(|ex:Execution| exists |i:nat| i<ex.states.len() && #[trigger] temp_pred.contains(drop(ex, i)))
+    // Set::new(|ex:Execution| exists |i:nat| i<ex.len() && #[trigger] temp_pred.contains(drop(ex, i)))
 }
 
 pub open spec fn implies(temp_pred_a: TempPred, temp_pred_b: TempPred) -> TempPred {
