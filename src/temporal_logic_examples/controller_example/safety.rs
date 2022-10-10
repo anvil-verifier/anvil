b540915e06b24946357ad8d10ceffb275d184c4f
Xudong Sun <xudongs1@vmware.com>
2022-10-10 14:50:32 -0500

    Finish safety proof
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal_logic_examples/controller_example/safety.rs
+++ src/temporal_logic_examples/controller_example/safety.rs
@@ -10,29 +10,73 @@ use builtin_macros::*;
 verus! {
 
 pub open spec fn msg_inv(s: CState) -> bool {
-    &&& s.sent_1_create ==> s.messages.contains(Message::CreateReq{id: 1})
-    &&& s.sent_2_create ==> s.messages.contains(Message::CreateReq{id: 2})
-    &&& s.messages.contains(Message::CreateReq{id: 1}) ==> s.sent_1_create
-    &&& s.messages.contains(Message::CreateReq{id: 2}) ==> s.sent_2_create
+    &&& s.sent_1_create <==> s.messages.contains(Message::CreateReq{id: 1})
+    &&& s.sent_2_create <==> s.messages.contains(Message::CreateReq{id: 2})
+}
+
+pub open spec fn obj1_inv(s: CState) -> bool {
+    &&& s.sent_2_create ==> s.obj_1_exists
+    &&& s.obj_1_exists ==> s.sent_1_create
+}
+
+pub open spec fn create_inv(s: CState) -> bool {
+    s.obj_2_exists ==> s.sent_2_create && s.sent_1_create && s.obj_1_exists
+}
+
+pub open spec fn safety(s: CState) -> bool {
+    s.obj_2_exists ==> s.obj_1_exists
+}
+
+pub open spec fn inductive_inv(s: CState) -> bool {
+    &&& msg_inv(s)
+    &&& obj1_inv(s)
+    &&& create_inv(s)
+    &&& safety(s)
 }
 
 pub open spec fn msg_inv_state_pred() -> StatePred<CState> {
     StatePred::new(|state: CState| msg_inv(state))
 }
 
-pub open spec fn msg_inv_always_holds() -> TempPred<CState> {
-    implies(
-        sm_spec(),
-        always(msg_inv_state_pred().lift())
-    )
+// pub open spec fn obj1_inv_state_pred() -> StatePred<CState> {
+//     StatePred::new(|state: CState| obj1_inv(state))
+// }
+
+// pub open spec fn create_inv_state_pred() -> StatePred<CState> {
+//     StatePred::new(|state: CState| create_inv(state))
+// }
+
+pub open spec fn safety_state_pred() -> StatePred<CState> {
+    StatePred::new(|state: CState| safety(state))
+}
+
+pub open spec fn inductive_inv_state_pred() -> StatePred<CState> {
+    StatePred::new(|state: CState| inductive_inv(state))
 }
 
 pub proof fn prove_msg_inv()
     ensures
-        valid(msg_inv_always_holds())
+        valid(implies(sm_spec(), always(msg_inv_state_pred().lift())))
 {
     apply_implies_auto::<CState>();
     init_invariant::<CState>(init_state_pred(), next_action_pred(), msg_inv_state_pred());
+}
+
+pub proof fn prove_inductive_inv()
+    ensures
+        valid(implies(sm_spec(), always(inductive_inv_state_pred().lift())))
+{
+    apply_implies_auto::<CState>();
+    init_invariant::<CState>(init_state_pred(), next_action_pred(), inductive_inv_state_pred());
+}
+
+pub proof fn prove_safety()
+    ensures
+        valid(implies(sm_spec(), always(safety_state_pred().lift())))
+{
+    apply_implies_auto::<CState>();
+    prove_inductive_inv();
+    implies_generalize::<CState>(inductive_inv_state_pred().lift(), safety_state_pred().lift());
 }
 
 }
