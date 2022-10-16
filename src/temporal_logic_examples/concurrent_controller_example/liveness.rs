8568c020bdf761f25d8d6e27200aa4b0e72e2e7c
Xudong Sun <xudongs1@vmware.com>
2022-10-16 12:02:59 -0500

    Prove liveness property: eventually pod1 exists, vol1 exists and attached
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal_logic_examples/concurrent_controller_example/liveness.rs
+++ src/temporal_logic_examples/concurrent_controller_example/liveness.rs
@@ -1,6 +1,7 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 #![allow(unused_imports)]
+use crate::concurrent_controller_example::safety::*;
 use crate::concurrent_controller_example::state_machine::*;
 use crate::pervasive::string::*;
 use crate::temporal_logic::*;
@@ -27,23 +28,22 @@ proof fn lemma_init_leads_to_pod1_exists()
             .implies(init_state_pred().lift()
                 .leads_to(pod1_exists_state_pred().lift()))),
 {
-    implies_apply_auto::<CState>();
     leads_to_eq_auto::<CState>(sm_spec());
 
     send_create_cr_enabled();
-    wf1::<CState>(next_action_pred(), send_create_cr_action_pred(), send_create_cr_pre_state_pred(), k8s_create_cr_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), send_create_cr_action_pred(), send_create_cr_pre_state_pred(), k8s_create_cr_pre_state_pred());
 
     k8s_create_cr_enabled();
-    wf1::<CState>(next_action_pred(), k8s_create_cr_action_pred(), k8s_create_cr_pre_state_pred(), send_create_sts_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_create_cr_action_pred(), k8s_create_cr_pre_state_pred(), send_create_sts_pre_state_pred());
 
     send_create_sts_enabled();
-    wf1::<CState>(next_action_pred(), send_create_sts_action_pred(), send_create_sts_pre_state_pred(), k8s_create_sts_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), send_create_sts_action_pred(), send_create_sts_pre_state_pred(), k8s_create_sts_pre_state_pred());
 
     k8s_create_sts_enabled();
-    wf1::<CState>(next_action_pred(), k8s_create_sts_action_pred(), k8s_create_sts_pre_state_pred(), k8s_create_pod_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_create_sts_action_pred(), k8s_create_sts_pre_state_pred(), k8s_create_pod_pre_state_pred());
 
     k8s_create_pod_enabled();
-    wf1::<CState>(next_action_pred(), k8s_create_pod_action_pred(), k8s_create_pod_pre_state_pred(), pod1_exists_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_create_pod_action_pred(), k8s_create_pod_pre_state_pred(), pod1_exists_state_pred());
 
     leads_to_trans::<CState>(sm_spec(), send_create_cr_pre_state_pred().lift(), k8s_create_cr_pre_state_pred().lift(), send_create_sts_pre_state_pred().lift());
     leads_to_trans::<CState>(sm_spec(), send_create_cr_pre_state_pred().lift(), send_create_sts_pre_state_pred().lift(), k8s_create_sts_pre_state_pred().lift());
@@ -58,20 +58,19 @@ proof fn lemma_init_leads_to_vol1_exists()
             .implies(init_state_pred().lift()
                 .leads_to(vol1_exists_state_pred().lift()))),
 {
-    implies_apply_auto::<CState>();
     leads_to_eq_auto::<CState>(sm_spec());
 
     send_create_cr_enabled();
-    wf1::<CState>(next_action_pred(), send_create_cr_action_pred(), send_create_cr_pre_state_pred(), k8s_create_cr_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), send_create_cr_action_pred(), send_create_cr_pre_state_pred(), k8s_create_cr_pre_state_pred());
 
     k8s_create_cr_enabled();
-    wf1::<CState>(next_action_pred(), k8s_create_cr_action_pred(), k8s_create_cr_pre_state_pred(), send_create_vol_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_create_cr_action_pred(), k8s_create_cr_pre_state_pred(), send_create_vol_pre_state_pred());
 
     send_create_vol_enabled();
-    wf1::<CState>(next_action_pred(), send_create_vol_action_pred(), send_create_vol_pre_state_pred(), k8s_create_vol_pre_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), send_create_vol_action_pred(), send_create_vol_pre_state_pred(), k8s_create_vol_pre_state_pred());
 
     k8s_create_vol_enabled();
-    wf1::<CState>(next_action_pred(), k8s_create_vol_action_pred(), k8s_create_vol_pre_state_pred(), vol1_exists_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_create_vol_action_pred(), k8s_create_vol_pre_state_pred(), vol1_exists_state_pred());
 
 
     leads_to_trans::<CState>(sm_spec(), send_create_cr_pre_state_pred().lift(), k8s_create_cr_pre_state_pred().lift(), send_create_vol_pre_state_pred().lift());
@@ -85,8 +84,6 @@ proof fn lemma_eventually_vol_attached()
         valid(sm_spec()
             .implies(eventually(vol_attached_state_pred().lift())))
 {
-    implies_apply_auto::<CState>();
-
     leads_to_weaken_auto::<CState>(sm_spec());
 
     lemma_init_leads_to_pod1_exists();
@@ -97,20 +94,32 @@ proof fn lemma_eventually_vol_attached()
 
     leads_to_always_combine::<CState>(sm_spec(), init_state_pred().lift(), pod1_exists_state_pred().lift(), vol1_exists_state_pred().lift());
 
-    always_weaken::<CState>(pod1_exists_state_pred().lift().and(vol1_exists_state_pred().lift()));
-
-    assert(valid(sm_spec()
-        .implies(init_state_pred().lift()
-            .leads_to(pod1_exists_state_pred().lift()
-                .and(vol1_exists_state_pred().lift())))));
+    leads_to_always_weaken::<CState>(sm_spec(), init_state_pred().lift(), pod1_exists_state_pred().lift().and(vol1_exists_state_pred().lift()));
 
     k8s_attach_vol_to_pod_enabled();
-    wf1::<CState>(next_action_pred(), k8s_attach_vol_to_pod_action_pred(), k8s_attach_vol_to_pod_pre_state_pred(), vol_attached_state_pred());
+    wf1::<CState>(sm_spec(), next_action_pred(), k8s_attach_vol_to_pod_action_pred(), k8s_attach_vol_to_pod_pre_state_pred(), vol_attached_state_pred());
 
     leads_to_trans::<CState>(sm_spec(), init_state_pred().lift(), k8s_attach_vol_to_pod_pre_state_pred().lift(), vol_attached_state_pred().lift());
 
     leads_to_apply::<CState>(sm_spec(), init_state_pred().lift(), vol_attached_state_pred().lift());
 }
 
+
+proof fn liveness()
+    ensures
+        valid(sm_spec()
+            .implies(eventually(vol_attached_state_pred().lift()
+                .and(pod1_exists_state_pred().lift())
+                    .and(vol1_exists_state_pred().lift())))),
+{
+    lemma_eventually_vol_attached();
+    lemma_attach_after_create_inv_state_pred();
+    always_and_eventually::<CState>(sm_spec(), attach_after_create_inv_state_pred().lift(), vol_attached_state_pred().lift());
+    eventually_weaken::<CState>(
+        sm_spec(),
+        attach_after_create_inv_state_pred().lift().and(vol_attached_state_pred().lift()),
+        vol_attached_state_pred().lift().and(pod1_exists_state_pred().lift()).and(vol1_exists_state_pred().lift())
+    );
+}
 
 }
