35d0c6cf959dfb3fa92a79d64e16c799d7911740
Xudong Sun <xudongs1@vmware.com>
2022-10-22 11:39:53 -0500

    Port abc state machine to the new StatePred/ActionPred
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal-logic-lib/examples/abc_state_machine/liveness.rs
+++ src/temporal-logic-lib/examples/abc_state_machine/liveness.rs
@@ -8,86 +8,44 @@ use builtin_macros::*;
 
 verus! {
 
-spec fn a_state_pred() -> StatePred<SimpleState> {
-    StatePred::new(|state: SimpleState| state.x === ABC::A)
-}
-
-spec fn b_state_pred() -> StatePred<SimpleState> {
-    StatePred::new(|state: SimpleState| state.x === ABC::B)
-}
-
-spec fn c_state_pred() -> StatePred<SimpleState> {
-    StatePred::new(|state: SimpleState| state.x === ABC::C)
-}
-
-spec fn a_temp_pred() -> TempPred<SimpleState> {
-    a_state_pred().lift()
-}
-
-spec fn b_temp_pred() -> TempPred<SimpleState> {
-    b_state_pred().lift()
-}
-
-spec fn c_temp_pred() -> TempPred<SimpleState> {
-    c_state_pred().lift()
-}
-
-proof fn a_b_enabled()
-    ensures forall |s: SimpleState| #[trigger] a_state_pred().satisfied_by(s) <==> enabled(a_b_action_pred()).satisfied_by(s)
-{
-    assert forall |s: SimpleState| #[trigger] a_state_pred().satisfied_by(s) implies enabled(a_b_action_pred()).satisfied_by(s) by {
-        let witness_action = Action {
-            state: s,
-            state_prime: SimpleState {
-                x: ABC::B,
-                happy: s.happy,
-            }
-        };
-        assert(a_b_action_pred().satisfied_by(witness_action));
-    };
-}
-
-proof fn b_c_enabled()
-    ensures forall |s: SimpleState| #[trigger] b_state_pred().satisfied_by(s) <==> enabled(b_c_action_pred()).satisfied_by(s)
-{
-    assert forall |s: SimpleState| #[trigger] b_state_pred().satisfied_by(s) implies enabled(b_c_action_pred()).satisfied_by(s) by {
-        let witness_action = Action {
-            state: s,
-            state_prime: SimpleState {
-                x: ABC::C,
-                happy: s.happy,
-            }
-        };
-        assert(b_c_action_pred().satisfied_by(witness_action));
-    }
-}
-
-proof fn prove_eventually_c()
+proof fn eventually_c()
     ensures
-        sm_spec().entails(eventually(c_temp_pred())),
+        sm_spec().entails(
+            eventually(lift_state(|s: SimpleState| s.x === ABC::C))
+        ),
 {
-    // a_b_enabled() gives a witness to convince Verus that x === a enables a_b()
+    // a_b_enabled() gives a witness to convince Verus that x === A enables a_b()
     a_b_enabled();
     // wf1 gives us a leads_to
-    wf1::<SimpleState>(sm_spec(), next_action_pred(), a_b_action_pred(), a_state_pred(), b_state_pred());
-    // Now we have:
-    // assert(sm_spec().entails(a_temp_pred().leads_to(b_temp_pred())));
+    wf1::<SimpleState>(sm_spec(),
+        next(),
+        a_b(),
+        |s: SimpleState| s.x === ABC::A,
+        |s: SimpleState| s.x === ABC::B
+    );
 
-    // a_b_enabled() gives a witness to convince Verus that x === b enables b_c()
+    // a_b_enabled() gives a witness to convince Verus that x === B enables b_c()
     b_c_enabled();
     // wf1 gives us another leads_to
-    wf1::<SimpleState>(sm_spec(), next_action_pred(), b_c_action_pred(), b_state_pred(), c_state_pred());
-    // Now we have:
-    // assert(sm_spec().entails(b_temp_pred().leads_to(c_temp_pred())));
+    wf1::<SimpleState>(sm_spec(),
+        next(),
+        b_c(),
+        |s: SimpleState| s.x === ABC::B,
+        |s: SimpleState| s.x === ABC::C,
+    );
 
     // leads_to_trans connects the two leads_to together
-    leads_to_trans::<SimpleState>(sm_spec(), a_state_pred(), b_state_pred(), c_state_pred());
-    // Now we have:
-    // assert(sm_spec().entails(a_temp_pred().leads_to(c_temp_pred())));
+    leads_to_trans::<SimpleState>(sm_spec(),
+        |s: SimpleState| s.x === ABC::A,
+        |s: SimpleState| s.x === ABC::B,
+        |s: SimpleState| s.x === ABC::C,
+    );
 
     // leads_to_apply gives us eventually from leads_to
-    // Note that init_state_pred(), as part of sm_spec(), implies a_temp_pred()
-    leads_to_apply::<SimpleState>(sm_spec(), a_state_pred(), c_state_pred());
+    leads_to_apply::<SimpleState>(sm_spec(),
+        |s: SimpleState| s.x === ABC::A,
+        |s: SimpleState| s.x === ABC::C,
+    );
 }
 
 }
