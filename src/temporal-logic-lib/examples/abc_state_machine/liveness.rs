7b9fc386f6f2ccf385a578e64577d36c0079248e
Xudong Sun <xudongs1@vmware.com>
2022-10-22 16:06:32 -0500

    Simplify abc liveness proof with helper functions
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal-logic-lib/examples/abc_state_machine/liveness.rs
+++ src/temporal-logic-lib/examples/abc_state_machine/liveness.rs
@@ -8,44 +8,39 @@ use builtin_macros::*;
 
 verus! {
 
+spec fn a() -> StatePred<SimpleState> {
+    |s: SimpleState| s.x === ABC::A
+}
+
+spec fn b() -> StatePred<SimpleState> {
+    |s: SimpleState| s.x === ABC::B
+}
+
+spec fn c() -> StatePred<SimpleState> {
+    |s: SimpleState| s.x === ABC::C
+}
+
 proof fn eventually_c()
     ensures
         sm_spec().entails(
-            eventually(lift_state(|s: SimpleState| s.x === ABC::C))
+            eventually(lift_state(c()))
         ),
 {
     // a_b_enabled() gives a witness to convince Verus that x === A enables a_b()
     a_b_enabled();
     // wf1 gives us a leads_to
-    wf1::<SimpleState>(sm_spec(),
-        next(),
-        a_b(),
-        |s: SimpleState| s.x === ABC::A,
-        |s: SimpleState| s.x === ABC::B
-    );
+    wf1::<SimpleState>(sm_spec(), next(), a_b(), a(), b());
 
     // a_b_enabled() gives a witness to convince Verus that x === B enables b_c()
     b_c_enabled();
     // wf1 gives us another leads_to
-    wf1::<SimpleState>(sm_spec(),
-        next(),
-        b_c(),
-        |s: SimpleState| s.x === ABC::B,
-        |s: SimpleState| s.x === ABC::C,
-    );
+    wf1::<SimpleState>(sm_spec(), next(), b_c(), b(), c());
 
     // leads_to_trans connects the two leads_to together
-    leads_to_trans::<SimpleState>(sm_spec(),
-        |s: SimpleState| s.x === ABC::A,
-        |s: SimpleState| s.x === ABC::B,
-        |s: SimpleState| s.x === ABC::C,
-    );
+    leads_to_trans::<SimpleState>(sm_spec(), a(), b(), c());
 
     // leads_to_apply gives us eventually from leads_to
-    leads_to_apply::<SimpleState>(sm_spec(),
-        |s: SimpleState| s.x === ABC::A,
-        |s: SimpleState| s.x === ABC::C,
-    );
+    leads_to_apply::<SimpleState>(sm_spec(), a(), c());
 }
 
 }
