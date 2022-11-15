4a4b6ef6e27e2e9ae760d54ba1904fd29160c1ce
Xudong Sun <xudongs1@vmware.com>
2022-11-15 11:31:29 -0600

    Add to the mod
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal-logic-lib/examples/mod.rs
+++ src/temporal-logic-lib/examples/mod.rs
@@ -1,5 +1,6 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 pub mod abc_state_machine;
+pub mod compound_state_machine;
 pub mod concurrent_controller;
 pub mod simple_controller;
