441ac3ebcc0deeaf3a20e4d7dd1cb381c8ca6fbb
Xudong Sun <xudongs1@vmware.com>
2022-12-02 20:47:28 -0600

    Simplify kubernetes api lemmas with lemma_msg_sent_leads_to_post_by_kubernetes_api
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal-logic-lib/examples/compound_state_machine/kubernetes_api_liveness.rs
+++ src/temporal-logic-lib/examples/compound_state_machine/kubernetes_api_liveness.rs
@@ -4,7 +4,10 @@
 use crate::examples::compound_state_machine::{
     common::*,
     distributed_system,
-    distributed_system::{message_sent, next, resource_exists, sm_spec, State},
+    distributed_system::{
+        kubernetes_api_action_pre, kubernetes_api_action_pre_implies_next_pre, kubernetes_api_next,
+        message_sent, next, resource_exists, sm_spec, State,
+    },
     kubernetes_api,
 };
 use crate::pervasive::option::*;
@@ -15,46 +18,45 @@ use builtin_macros::*;
 
 verus! {
 
+
+pub proof fn lemma_msg_sent_leads_to_post_by_kubernetes_api(msg: Message, action: kubernetes_api::KubernetesAPIAction, post: StatePred<State>)
+    requires
+        kubernetes_api::kubernetes_api().actions.contains(action),
+        forall |s, s_prime: State| kubernetes_api_next().pre(Option::Some(msg))(s) && action_pred_call(next(), s, s_prime) ==> kubernetes_api_next().pre(Option::Some(msg))(s_prime) || post(s_prime),
+        forall |s, s_prime: State| kubernetes_api_next().pre(Option::Some(msg))(s) && action_pred_call(next(), s, s_prime) && kubernetes_api_next().forward(Option::Some(msg))(s, s_prime) ==> post(s_prime),
+        forall |s: State| state_pred_call(message_sent(msg), s) ==> kubernetes_api_action_pre(action, Option::Some(msg))(s),
+    ensures
+        sm_spec().entails(lift_state(message_sent(msg)).leads_to(lift_state(post))),
+{
+    let recv = Option::Some(msg);
+    let pre = kubernetes_api_action_pre(kubernetes_api::handle_request(), recv);
+
+    leads_to_weaken_auto::<State>(sm_spec());
+    use_tla_forall::<State, Option<Message>>(sm_spec(), |r| weak_fairness(kubernetes_api_next().forward(r)), recv);
+
+    kubernetes_api_action_pre_implies_next_pre(action, recv);
+    kubernetes_api_next().wf1(recv, sm_spec(), next(), post);
+
+    assert(sm_spec().entails(lift_state(kubernetes_api_next().pre(recv)).leads_to(lift_state(post))));
+    assert(sm_spec().entails(lift_state(pre).leads_to(lift_state(post))));
+}
+
 pub proof fn lemma_create_req_leads_to_create_resp(msg: Message)
     requires
         msg.is_CreateRequest(),
     ensures
-        sm_spec().entails(
-            lift_state(message_sent(msg)).leads_to(lift_state(message_sent(create_resp_msg(msg.get_CreateRequest_0().obj.key))))
-        ),
+        sm_spec().entails(lift_state(message_sent(msg)).leads_to(lift_state(message_sent(create_resp_msg(msg.get_CreateRequest_0().obj.key))))),
 {
-    let recv = Option::Some(msg);
-    let pre = distributed_system::kubernetes_api_action_pre(kubernetes_api::handle_request(), recv);
-    let post = message_sent(create_resp_msg(msg.get_CreateRequest_0().obj.key));
-
-    leads_to_weaken_auto::<State>(sm_spec());
-    use_tla_forall::<State, Option<Message>>(sm_spec(), |r| weak_fairness(distributed_system::kubernetes_api_next().forward(r)), recv);
-
-    distributed_system::kubernetes_api_action_pre_implies_next_pre(kubernetes_api::handle_request(), recv);
-    distributed_system::kubernetes_api_next().wf1(recv, sm_spec(), next(), post);
-
-    assert(sm_spec().entails(lift_state(pre).leads_to(lift_state(post))));
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(msg, kubernetes_api::handle_request(), message_sent(create_resp_msg(msg.get_CreateRequest_0().obj.key)));
 }
 
 pub proof fn lemma_delete_req_leads_to_res_not_exists(msg: Message)
     requires
         msg.is_DeleteRequest(),
     ensures
-        sm_spec().entails(
-            lift_state(message_sent(msg)).leads_to(lift_state(|s| !resource_exists(msg.get_DeleteRequest_0().key)(s)))
-        ),
+        sm_spec().entails(lift_state(message_sent(msg)).leads_to(lift_state(|s| !resource_exists(msg.get_DeleteRequest_0().key)(s)))),
 {
-    let recv = Option::Some(msg);
-    let pre = distributed_system::kubernetes_api_action_pre(kubernetes_api::handle_request(), recv);
-    let post = |s| !resource_exists(msg.get_DeleteRequest_0().key)(s);
-
-    leads_to_weaken_auto::<State>(sm_spec());
-    use_tla_forall::<State, Option<Message>>(sm_spec(), |r| weak_fairness(distributed_system::kubernetes_api_next().forward(r)), recv);
-
-    distributed_system::kubernetes_api_action_pre_implies_next_pre(kubernetes_api::handle_request(), recv);
-    distributed_system::kubernetes_api_next().wf1(recv, sm_spec(), next(), post);
-
-    assert(sm_spec().entails(lift_state(pre).leads_to(lift_state(post))));
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(msg, kubernetes_api::handle_request(), |s| !resource_exists(msg.get_DeleteRequest_0().key)(s));
 }
 
 pub proof fn lemma_create_sts_req_sent_leads_to_pod_exists_and_vol_exists(msg: Message)
@@ -69,40 +71,15 @@ pub proof fn lemma_create_sts_req_sent_leads_to_pod_exists_and_vol_exists(msg: M
             .entails(lift_state(message_sent(msg))
                 .leads_to(lift_state(resource_exists(ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + vol_suffix(), kind: ResourceKind::VolumeKind})))),
 {
-    lemma_create_sts_req_sent_leads_to(msg, create_req_msg(ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + pod_suffix(), kind: ResourceKind::PodKind}));
-    lemma_create_sts_req_sent_leads_to(msg, create_req_msg(ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + vol_suffix(), kind: ResourceKind::VolumeKind}));
-}
+    let create_pod_req_msg = create_req_msg(ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + pod_suffix(), kind: ResourceKind::PodKind});
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(msg, kubernetes_api::handle_request(), message_sent(create_pod_req_msg));
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(create_pod_req_msg, kubernetes_api::handle_request(), resource_exists(create_pod_req_msg.get_CreateRequest_0().obj.key));
+    leads_to_trans::<State>(sm_spec(), message_sent(msg), message_sent(create_pod_req_msg), resource_exists(create_pod_req_msg.get_CreateRequest_0().obj.key));
 
-proof fn lemma_create_sts_req_sent_leads_to(msg: Message, sub_res_msg: Message)
-    requires
-        msg.is_CreateRequest(),
-        msg.get_CreateRequest_0().obj.key.kind.is_StatefulSetKind(),
-        sub_res_msg.is_CreateRequest(),
-        sub_res_msg.get_CreateRequest_0().obj.key === (ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + pod_suffix(), kind: ResourceKind::PodKind})
-        || sub_res_msg.get_CreateRequest_0().obj.key === (ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + vol_suffix(), kind: ResourceKind::VolumeKind}),
-    ensures
-        sm_spec()
-            .entails(lift_state(message_sent(msg))
-                .leads_to(lift_state(resource_exists(sub_res_msg.get_CreateRequest_0().obj.key)))),
-{
-    let recv_msg = Option::Some(msg);
-    let recv_sub_res_msg = Option::Some(sub_res_msg);
-    let pre = distributed_system::kubernetes_api_action_pre(kubernetes_api::handle_request(), recv_msg);
-    let post = resource_exists(sub_res_msg.get_CreateRequest_0().obj.key);
-
-    leads_to_weaken_auto::<State>(sm_spec());
-    use_tla_forall::<State, Option<Message>>(sm_spec(), |r| weak_fairness(distributed_system::kubernetes_api_next().forward(r)), recv_msg);
-    use_tla_forall::<State, Option<Message>>(sm_spec(), |r| weak_fairness(distributed_system::kubernetes_api_next().forward(r)), recv_sub_res_msg);
-
-    distributed_system::kubernetes_api_action_pre_implies_next_pre(kubernetes_api::handle_request(), recv_msg);
-    distributed_system::kubernetes_api_next().wf1(recv_msg, sm_spec(), next(), message_sent(sub_res_msg));
-    assert(sm_spec().entails(lift_state(pre).leads_to(lift_state(message_sent(sub_res_msg)))));
-
-    distributed_system::kubernetes_api_action_pre_implies_next_pre(kubernetes_api::handle_request(), recv_sub_res_msg);
-    distributed_system::kubernetes_api_next().wf1(recv_sub_res_msg, sm_spec(), next(), post);
-    assert(sm_spec().entails(lift_state(message_sent(sub_res_msg)).leads_to(lift_state(post))));
-
-    leads_to_trans::<State>(sm_spec(), pre, message_sent(sub_res_msg), post);
+    let create_vol_req_msg = create_req_msg(ResourceKey{name: msg.get_CreateRequest_0().obj.key.name + vol_suffix(), kind: ResourceKind::VolumeKind});
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(msg, kubernetes_api::handle_request(), message_sent(create_vol_req_msg));
+    lemma_msg_sent_leads_to_post_by_kubernetes_api(create_vol_req_msg, kubernetes_api::handle_request(), resource_exists(create_vol_req_msg.get_CreateRequest_0().obj.key));
+    leads_to_trans::<State>(sm_spec(), message_sent(msg), message_sent(create_vol_req_msg), resource_exists(create_vol_req_msg.get_CreateRequest_0().obj.key));
 }
 
 }
