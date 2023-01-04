9d181a553c6251fb7328d6a16aac8dc27f42cb12
Xudong Sun <xudongs1@vmware.com>
2023-01-03 20:09:25 -0600

    mod: Focus on the mini controller proof for now
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal-logic-lib/examples/mod.rs
+++ src/temporal-logic-lib/examples/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
-pub mod abc_state_machine;
-pub mod compound_state_machine;
-pub mod concurrent_controller;
-pub mod simple_controller;
+// pub mod abc_state_machine;
+// pub mod compound_state_machine;
+// pub mod concurrent_controller;
+pub mod kubernetes_cluster;
+// pub mod simple_controller;
