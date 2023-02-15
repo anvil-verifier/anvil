cd9df1c9e97c87cb545aed8b8a6ade6f31c396ad
Xudong Sun <xudongs1@vmware.com>
2023-02-14 22:23:55 -0600

    Write the simple controller in Anvil style
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

---
+++ src/simple_controller_impl/src/main.rs
@@ -0,0 +1,181 @@
+// Nightly clippy (0.1.64) considers Drop a side effect, see https://github.com/rust-lang/rust-clippy/issues/9608
+#![allow(clippy::unnecessary_lazy_evaluations)]
+
+use anyhow::Result;
+use futures::StreamExt;
+use k8s_openapi::api::core::v1::ConfigMap;
+use kube::{
+    api::{Api, ListParams, ObjectMeta, PostParams},
+    runtime::controller::{Action, Controller},
+    Client, CustomResource,
+};
+use kube_client;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use thiserror::Error;
+use tokio::time::Duration;
+use tracing::*;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("MissingObjectKey: {0}")]
+    MissingObjectKey(&'static str),
+}
+
+#[derive(CustomResource, Debug, Clone, Deserialize, Serialize, JsonSchema)]
+#[kube(group = "anvil.dev", version = "v1", kind = "SimpleCR")]
+#[kube(shortname = "cr", namespaced)]
+struct SimpleCRSpec {}
+
+struct SimpleReconcileState {
+    pub reconcile_pc: i32,
+}
+
+#[derive(Clone)]
+struct ResourceKey {
+    name: String,
+    namespace: String,
+    // kind: String,
+}
+
+enum APIRequest {
+    GetCRReq(ResourceKey),
+    CreateConfigMapReq(ConfigMap),
+}
+
+enum APIResponse {
+    GetCRResp(kube_client::Result<SimpleCR>),
+    CreateConfigMapResp(kube_client::Result<ConfigMap>),
+}
+
+fn reconcile_init_state() -> SimpleReconcileState {
+    SimpleReconcileState { reconcile_pc: 0 }
+}
+
+fn reconcile_done(state: &SimpleReconcileState) -> bool {
+    state.reconcile_pc != 0 && state.reconcile_pc != 1
+}
+
+fn reconcile_core(
+    cr_key: &ResourceKey,
+    resp_option: &Option<APIResponse>,
+    state: &SimpleReconcileState,
+) -> (SimpleReconcileState, Option<APIRequest>) {
+    let pc = state.reconcile_pc;
+    if pc == 0 {
+        let state_prime = SimpleReconcileState { reconcile_pc: 1 };
+        let req_option = Some(APIRequest::GetCRReq(cr_key.clone()));
+        info!("Send request to get CR");
+        (state_prime, req_option)
+    } else if pc == 1 {
+        let state_prime = SimpleReconcileState { reconcile_pc: 2 };
+        let cm = ConfigMap {
+            metadata: ObjectMeta {
+                name: Some(cr_key.name.clone() + "-cm"),
+                namespace: Some(cr_key.namespace.clone()),
+                ..ObjectMeta::default()
+            },
+            data: None,
+            ..Default::default()
+        };
+        let req_option = Some(APIRequest::CreateConfigMapReq(cm));
+        info!("Send request to create configmap");
+        (state_prime, req_option)
+    } else {
+        let state_prime = SimpleReconcileState {
+            reconcile_pc: state.reconcile_pc,
+        };
+        info!("Should not reach here");
+        (state_prime, None)
+    }
+}
+
+async fn reconcile(cr: Arc<SimpleCR>, ctx: Arc<Data>) -> Result<Action, Error> {
+    let client = &ctx.client;
+
+    let cr_name = cr
+        .metadata
+        .name
+        .as_ref()
+        .ok_or_else(|| Error::MissingObjectKey(".metadata.name"))?;
+    let cr_ns = cr
+        .metadata
+        .namespace
+        .as_ref()
+        .ok_or_else(|| Error::MissingObjectKey(".metadata.namespace"))?;
+
+    let cr_key = ResourceKey {
+        name: cr_name.clone(),
+        namespace: cr_ns.clone(),
+    };
+    let mut state = reconcile_init_state();
+    let mut resp_option: Option<APIResponse> = None;
+
+    loop {
+        if reconcile_done(&state) {
+            break;
+        }
+        let (state_prime, req_option) = reconcile_core(&cr_key, &resp_option, &state);
+        match req_option {
+            Some(req) => match req {
+                APIRequest::GetCRReq(key) => {
+                    let cr_api = Api::<SimpleCR>::namespaced(client.clone(), &key.namespace);
+                    resp_option = Some(APIResponse::GetCRResp(cr_api.get(&key.name).await));
+                }
+                APIRequest::CreateConfigMapReq(cm) => {
+                    let cm_api = Api::<ConfigMap>::namespaced(
+                        client.clone(),
+                        cm.metadata
+                            .namespace
+                            .as_ref()
+                            .ok_or_else(|| Error::MissingObjectKey(".metadata.namespace"))?,
+                    );
+                    let pp = PostParams::default();
+                    resp_option = Some(APIResponse::CreateConfigMapResp(
+                        cm_api.create(&pp, &cm).await,
+                    ));
+                }
+            },
+            _ => resp_option = None,
+        }
+        state = state_prime
+    }
+
+    Ok(Action::requeue(Duration::from_secs(100)))
+}
+
+/// The controller triggers this on reconcile errors
+fn error_policy(_object: Arc<SimpleCR>, _error: &Error, _ctx: Arc<Data>) -> Action {
+    Action::requeue(Duration::from_secs(1))
+}
+
+// Data we want access to in error/reconcile calls
+struct Data {
+    client: Client,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let client = Client::try_default().await?;
+
+    let crs = Api::<SimpleCR>::all(client.clone());
+    let cms = Api::<ConfigMap>::all(client.clone());
+
+    info!("starting simple-controller");
+
+    Controller::new(crs, ListParams::default())
+        .owns(cms, ListParams::default())
+        .shutdown_on_signal()
+        .run(reconcile, error_policy, Arc::new(Data { client }))
+        .for_each(|res| async move {
+            match res {
+                Ok(o) => info!("reconciled {:?}", o),
+                Err(e) => warn!("reconcile failed: {}", e),
+            }
+        })
+        .await;
+    info!("controller terminated");
+    Ok(())
+}
