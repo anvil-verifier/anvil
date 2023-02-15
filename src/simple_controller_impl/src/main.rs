9b526c914002bacef6b6c92f8ae9b14c31058bf7
Xudong Sun <xudongs1@vmware.com>
2023-02-15 11:15:39 -0600

    Add comments
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/simple_controller_impl/src/main.rs
+++ src/simple_controller_impl/src/main.rs
@@ -28,10 +28,14 @@ enum Error {
 #[kube(shortname = "cr", namespaced)]
 struct SimpleCRSpec {}
 
+// The local state used in each reconcile core invocation
+// It is very simple and only has a pc
 struct SimpleReconcileState {
     pub reconcile_pc: i32,
 }
 
+// This is the key of the custom resource object that the controller is reconciling for
+// TODO: add the type of the resource (i.e., kind) into it
 #[derive(Clone)]
 struct ResourceKey {
     name: String,
@@ -39,24 +43,37 @@ struct ResourceKey {
     // kind: String,
 }
 
+// This is the API request that reconcile_core returns to reconcile
+// I did not find a way to encode the type into the enum
+// so each request operation is hardcoded to one type
+// This is a big limitation and we should work on it
 enum APIRequest {
     GetCRReq(ResourceKey),
     CreateConfigMapReq(ConfigMap),
 }
 
+// The response that reconcile passes to reconcile_core
+// It has the same problem as APIRequest
 enum APIResponse {
     GetCRResp(kube_client::Result<SimpleCR>),
     CreateConfigMapResp(kube_client::Result<ConfigMap>),
 }
 
+// The initial state to start the reconcile_core loop in each reconcile invocation
 fn reconcile_init_state() -> SimpleReconcileState {
     SimpleReconcileState { reconcile_pc: 0 }
 }
 
+// It says there is no more work from reconcile_core and reconcile should return OK
+// TODO: we also need a function for returning Err
 fn reconcile_done(state: &SimpleReconcileState) -> bool {
     state.reconcile_pc != 0 && state.reconcile_pc != 1
 }
 
+// This is the untrusted reconcile logic
+// It takes (1) the key to the custom resource object, (2) the response of the previous request, and (3) the current reconcile state
+// and returns (1) a new reconcile state and (2) a request to send to the Kubernetes API
+// We start from simple and keep consistent with the simple reconcile spec by ignoring the response
 fn reconcile_core(
     cr_key: &ResourceKey,
     resp_option: &Option<APIResponse>,
@@ -91,6 +108,8 @@ fn reconcile_core(
     }
 }
 
+// The reconcile function is called by the kube-rs framework
+// All the reconcile logic is in the reconcile function
 async fn reconcile(cr: Arc<SimpleCR>, ctx: Arc<Data>) -> Result<Action, Error> {
     let client = &ctx.client;
 
@@ -105,6 +124,7 @@ async fn reconcile(cr: Arc<SimpleCR>, ctx: Arc<Data>) -> Result<Action, Error> {
         .as_ref()
         .ok_or_else(|| Error::MissingObjectKey(".metadata.namespace"))?;
 
+    // Prepare the input for calling reconcile_core
     let cr_key = ResourceKey {
         name: cr_name.clone(),
         namespace: cr_ns.clone(),
@@ -112,11 +132,15 @@ async fn reconcile(cr: Arc<SimpleCR>, ctx: Arc<Data>) -> Result<Action, Error> {
     let mut state = reconcile_init_state();
     let mut resp_option: Option<APIResponse> = None;
 
+    // Call reconcile_core in a loop
     loop {
+        // If reconcile core is done, then breaks the loop
         if reconcile_done(&state) {
             break;
         }
+        // Feed the current reconcile state and get the new state and the pending request
         let (state_prime, req_option) = reconcile_core(&cr_key, &resp_option, &state);
+        // Pattern match the request and send requests to the Kubernetes API via kube-rs methods
         match req_option {
             Some(req) => match req {
                 APIRequest::GetCRReq(key) => {
@@ -145,7 +169,7 @@ async fn reconcile(cr: Arc<SimpleCR>, ctx: Arc<Data>) -> Result<Action, Error> {
     Ok(Action::requeue(Duration::from_secs(100)))
 }
 
-/// The controller triggers this on reconcile errors
+// The controller triggers this on reconcile errors
 fn error_policy(_object: Arc<SimpleCR>, _error: &Error, _ctx: Arc<Data>) -> Action {
     Action::requeue(Duration::from_secs(1))
 }
@@ -155,20 +179,19 @@ struct Data {
     client: Client,
 }
 
+// The main function registers the (1) the reconcile function and (2) the trigger of reconcile
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
 
     let crs = Api::<SimpleCR>::all(client.clone());
-    let cms = Api::<ConfigMap>::all(client.clone());
 
     info!("starting simple-controller");
 
-    Controller::new(crs, ListParams::default())
-        .owns(cms, ListParams::default())
+    Controller::new(crs, ListParams::default()) // The controller's reconcile is triggered when a CR is created/updated
         .shutdown_on_signal()
-        .run(reconcile, error_policy, Arc::new(Data { client }))
+        .run(reconcile, error_policy, Arc::new(Data { client })) // The reconcile function is registered
         .for_each(|res| async move {
             match res {
                 Ok(o) => info!("reconciled {:?}", o),
