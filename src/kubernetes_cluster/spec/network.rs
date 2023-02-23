0ab6318e6c6c080c4d91e098c262254ac173d1a2
Xudong Sun <xudongs1@vmware.com>
2023-02-22 21:08:45 -0600

    Revise network model: at most once delivery
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/kubernetes_cluster/spec/network.rs
+++ src/kubernetes_cluster/spec/network.rs
@@ -1,9 +1,9 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 #![allow(unused_imports)]
-use crate::state_machine::action::*;
 use crate::kubernetes_cluster::spec::common::*;
 use crate::pervasive::{option::*, set::*};
+use crate::state_machine::action::*;
 use crate::state_machine::state_machine::*;
 use crate::temporal_logic::defs::*;
 use builtin::*;
@@ -21,9 +21,17 @@ pub open spec fn deliver() -> Action<NetworkState, MessageOps<Message>, ()> {
             msg_ops.recv.is_Some() ==> s.sent_messages.contains(msg_ops.recv.get_Some_0())
         },
         transition: |msg_ops: MessageOps<Message>, s: NetworkState| {
-            (NetworkState {
-                sent_messages: s.sent_messages + msg_ops.send
-            }, ())
+            if msg_ops.recv.is_Some() {
+                let s_prime = NetworkState {
+                    sent_messages: s.sent_messages.remove(msg_ops.recv.get_Some_0()) + msg_ops.send
+                };
+                (s_prime, ())
+            } else {
+                let s_prime = NetworkState {
+                    sent_messages: s.sent_messages + msg_ops.send
+                };
+                (s_prime, ())
+            }
         },
     }
 }
