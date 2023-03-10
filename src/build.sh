6f6b40731337fd7cc7a9bb2aab1491ff460f8ce2
Xudong Sun <xudongs1@vmware.com>
2023-03-09 21:22:19 -0600

    Add build script
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

---
+++ src/build.sh
@@ -0,0 +1,5 @@
+rv=$VERUS_DIR/source/tools/rust-verify.sh
+cd deps_hack; bash build.sh
+cd ..
+k8s_openapi_rlib=$(find deps_hack/target/debug/deps/ -name 'libk8s_openapi-*.rlib')
+$rv -L dependency=deps_hack/target/debug/deps --extern=k8s_openapi=$k8s_openapi_rlib --cfg erasure_macro_todo --erasure macro --compile main.rs
