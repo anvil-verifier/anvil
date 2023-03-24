47e7aa1127bf079c7637e5d720e823a8473f28fd
Xudong Sun <xudongs1@vmware.com>
2023-03-23 22:25:32 -0500

    Implement KubeObjectRef
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/kubernetes_api_objects/common.rs
+++ src/kubernetes_api_objects/common.rs
@@ -1,6 +1,8 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 use crate::pervasive::prelude::*;
+use crate::pervasive::string::*;
+use crate::pervasive_ext::string_view::*;
 
 verus! {
 
@@ -16,8 +18,24 @@ pub enum Kind {
 
 pub struct ObjectRef {
     pub kind: Kind,
-    pub name: Seq<char>,
-    pub namespace: Seq<char>,
+    pub name: StringView,
+    pub namespace: StringView,
+}
+
+pub struct KubeObjectRef {
+    pub kind: Kind,
+    pub name: String,
+    pub namespace: String,
+}
+
+impl KubeObjectRef {
+    pub open spec fn to_view(&self) -> ObjectRef {
+        ObjectRef {
+            kind: self.kind,
+            name: self.name@,
+            namespace: self.namespace@,
+        }
+    }
 }
 
 }
