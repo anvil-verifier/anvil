cb7967d7750a7af1687b1a1fc7f492fc3884d279
Xudong Sun <xudongs1@vmware.com>
2023-03-29 13:21:30 -0500

    Document ChannelManager
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/kubernetes_cluster/spec/channel.rs
+++ src/kubernetes_cluster/spec/channel.rs
@@ -7,6 +7,10 @@ use builtin_macros::*;
 
 verus! {
 
+/// ChannelManager allocates new channel id for each request sent by the controller.
+/// The response will carry the same channel id.
+
+// TODO: the concept of channel should be modeled inside the network state machine
 pub struct ChannelManager {
     pub cur_chan_id: nat,
 }
@@ -17,6 +21,8 @@ impl ChannelManager {
             cur_chan_id: 0,
         }
     }
+
+    /// allocate returns cur_chan_id and another ChannelManager with an incremented cur_chan_id
 
     pub open spec fn allocate(self) -> (Self, nat) {
         (ChannelManager {
