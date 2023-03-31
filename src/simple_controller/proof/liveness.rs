d78872d6977aaa4df88c93859177cfc8fa9d3fa1
Wenjie Ma <rebekah1368@gmail.com>
2023-03-31 17:21:28 +0800

    Fix the lemmas: spec |= init_pc ~> []cm_exists, spec |= []cr_exists ~> reconcile_scheduled
    Signed-off-by: Wenjie Ma <rebekah1368@gmail.com>

--- src/simple_controller/proof/liveness.rs
+++ src/simple_controller/proof/liveness.rs
@@ -58,6 +58,22 @@ proof fn liveness_proof(cr: CustomResourceView)
         ),
 {}
 
+proof fn lemma_cr_always_exists_leads_to_reconcile_scheduled(cr: CustomResourceView)
+    ensures
+        sm_spec(simple_reconciler()).entails(
+            always(cr_exists(cr)).leads_to(lift_state(|s: State<SimpleReconcileState>| s.reconcile_scheduled_for(cr.object_ref())))
+        ),
+{
+    let scheduled = lift_state(|s: State<SimpleReconcileState>| s.reconcile_scheduled_for(cr.object_ref()));
+    let cr_key_exists = lift_state(|s: State<SimpleReconcileState>| s.resource_key_exists(cr.object_ref()));
+    valid_stable_sm_partial_spec::<SimpleReconcileState>(simple_reconciler());
+    controller_runtime_liveness::lemma_true_leads_to_reconcile_scheduled_by_assumption::<SimpleReconcileState>(simple_reconciler(), cr.object_ref());
+    unpack_assumption_from_spec::<State<SimpleReconcileState>>(lift_state(init(simple_reconciler())), sm_partial_spec(simple_reconciler()), always(cr_key_exists), scheduled);
+    implies_preserved_by_always_temp::<State<SimpleReconcileState>>(cr_exists(cr), cr_key_exists);
+    valid_implies_implies_leads_to::<State<SimpleReconcileState>>(sm_spec(simple_reconciler()), always(cr_exists(cr)), always(cr_key_exists));
+    leads_to_trans_auto(sm_spec(simple_reconciler()));
+}
+
 #[verifier(external_body)]
 proof fn lemma_reconcile_not_ongoing_leads_to_cm_always_exists(cr: CustomResourceView)
     ensures
@@ -74,16 +90,25 @@ proof fn lemma_reconcile_ongoing_leads_to_cm_always_exists(cr: CustomResourceVie
             lift_state(|s: State<SimpleReconcileState>| s.reconcile_state_contains(cr.object_ref()))
                 .leads_to(always(lift_state(cm_exists(cr))))
         ),
-{}
+{
+    lemma_init_pc_leads_to_cm_always_exists(cr);
+    lemma_after_get_cr_pc_leads_to_cm_always_exists(cr);
+    lemma_after_create_cm_pc_leads_to_cm_always_exists(cr);
+}
 
-#[verifier(external_body)]
 proof fn lemma_init_pc_leads_to_cm_always_exists(cr: CustomResourceView)
     ensures
         sm_spec(simple_reconciler()).entails(
             lift_state(reconciler_at_init_pc(cr))
             .leads_to(always(lift_state(cm_exists(cr))))
         ),
-{}
+{
+    safety::lemma_always_reconcile_init_implies_no_pending_req(cr);
+    lemma_init_pc_leads_to_after_get_cr_pc(cr);
+    lemma_after_get_cr_pc_leads_to_cm_always_exists(cr);
+    leads_to_trans_auto::<State<SimpleReconcileState>>(sm_spec(simple_reconciler()));
+    leads_to_weaken_auto::<State<SimpleReconcileState>>(sm_spec(simple_reconciler()));
+}
 
 proof fn lemma_after_get_cr_pc_leads_to_cm_always_exists(cr: CustomResourceView)
     ensures
