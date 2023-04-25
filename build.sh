d07b231e5866ab0ddfd471c8b0fc37965f36cb98
Xudong Sun <xudongs1@vmware.com>
2023-04-25 09:44:56 -0500

    Import kube-rs
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- build.sh
+++ build.sh
@@ -15,8 +15,22 @@ cd deps_hack
 cargo build
 cd ..
 k8s_openapi_rlib="$(find deps_hack/target/debug/deps -name 'libk8s_openapi-*.rlib')"
+kube_rlib="$(find deps_hack/target/debug/deps -name 'libkube-*.rlib')"
+kube_client_rlib="$(find deps_hack/target/debug/deps -name 'libkube_client-*.rlib')"
+kube_core_rlib="$(find deps_hack/target/debug/deps -name 'libkube_core-*.rlib')"
+kube_runtime_rlib="$(find deps_hack/target/debug/deps -name 'libkube_runtime-*.rlib')"
+serde_rlib="$(find deps_hack/target/debug/deps -name 'libserde-*.rlib' | head -n 1)"
+serde_json_rlib="$(find deps_hack/target/debug/deps -name 'libserde_json-*.rlib' | head -n 1)"
+schemars_rlib="$(find deps_hack/target/debug/deps -name 'libschemars-*.rlib')"
 "$rv" -L dependency=deps_hack/target/debug/deps \
   --extern=k8s_openapi="$k8s_openapi_rlib" \
+  --extern=kube="$kube_rlib" \
+  --extern=kube_client="$kube_client_rlib" \
+  --extern=kube_core="$kube_core_rlib" \
+  --extern=kube_runtime="$kube_runtime_rlib" \
+  --extern=serde="$serde_rlib" \
+  --extern=serde_json="$serde_json_rlib" \
+  --extern=schemars="$schemars_rlib" \
   --expand-errors \
   --compile \
   --time \
--- src/deps_hack/src/lib.rs
+++ src/deps_hack/src/lib.rs
@@ -1,1 +1,5 @@
 pub use k8s_openapi;
+pub use kube;
+pub use schemars;
+pub use serde;
+pub use serde_json;
