647e81746c92de364d2c54f0bb4c97bae5367f68
Xudong Sun <xudongs1@vmware.com>
2023-04-26 20:45:43 -0500

    Add more dependencies
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- build.sh
+++ build.sh
@@ -15,13 +15,18 @@ cd deps_hack
 cargo build
 cd ..
 k8s_openapi_rlib="$(find deps_hack/target/debug/deps -name 'libk8s_openapi-*.rlib')"
-kube_rlib="$(find deps_hack/target/debug/deps -name 'libkube-*.rlib')"
+kube_rlib="$(find deps_hack/target/debug/deps -name 'libkube-*.rlib' | head -n 1)"
 kube_client_rlib="$(find deps_hack/target/debug/deps -name 'libkube_client-*.rlib')"
 kube_core_rlib="$(find deps_hack/target/debug/deps -name 'libkube_core-*.rlib')"
 kube_runtime_rlib="$(find deps_hack/target/debug/deps -name 'libkube_runtime-*.rlib')"
 serde_rlib="$(find deps_hack/target/debug/deps -name 'libserde-*.rlib' | head -n 1)"
 serde_json_rlib="$(find deps_hack/target/debug/deps -name 'libserde_json-*.rlib' | head -n 1)"
+serde_yaml_rlib="$(find deps_hack/target/debug/deps -name 'libserde_yaml-*.rlib' | head -n 1)"
 schemars_rlib="$(find deps_hack/target/debug/deps -name 'libschemars-*.rlib')"
+tokio_rlib="$(find deps_hack/target/debug/deps -name 'libtokio-*.rlib' | head -n 1)"
+tracing_rlib="$(find deps_hack/target/debug/deps -name 'libtracing-*.rlib' | head -n 1)"
+anyhow_rlib="$(find deps_hack/target/debug/deps -name 'libanyhow-*.rlib' | head -n 1)"
+futures_rlib="$(find deps_hack/target/debug/deps -name 'libfutures-*.rlib' | head -n 1)"
 "$rv" -L dependency=deps_hack/target/debug/deps \
   --extern=k8s_openapi="$k8s_openapi_rlib" \
   --extern=kube="$kube_rlib" \
@@ -30,7 +35,13 @@ schemars_rlib="$(find deps_hack/target/debug/deps -name 'libschemars-*.rlib')"
   --extern=kube_runtime="$kube_runtime_rlib" \
   --extern=serde="$serde_rlib" \
   --extern=serde_json="$serde_json_rlib" \
+  --extern=serde_yaml="$serde_yaml_rlib" \
   --extern=schemars="$schemars_rlib" \
+  --extern=tokio="$tokio_rlib" \
+  --extern=tracing="$tracing_rlib" \
+  --extern=anyhow="$anyhow_rlib" \
+  --extern=futures="$futures_rlib" \
+  --extern=deps_hack="deps_hack/target/debug/libdeps_hack.rlib" \
   --expand-errors \
   --compile \
   --time \
