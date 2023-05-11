c6dee6ace196c91e3fd33c1f5405f9b78ce15594
Xudong Sun <xudongs1@vmware.com>
2023-05-11 13:23:56 -0500

    Clean up TLA examples
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/temporal_logic_examples.rs
+++ src/temporal_logic_examples.rs
@@ -1,11 +1,11 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 
-pub mod pervasive;
+#![allow(unused_imports)]
+
 pub mod pervasive_ext;
 pub mod state_machine;
 pub mod temporal_logic;
 pub mod tla_examples;
 
-#[verifier(external_body)]
 fn main() {}
--- src/tla_examples/mod.rs
+++ src/tla_examples/mod.rs
@@ -1,6 +1,5 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 pub mod abc;
-pub mod compound;
 pub mod concurrent;
 pub mod sequential;
