da144c838a6b28f095e98bede028b5958b5a8c6f
Xudong Sun <xudongs1@vmware.com>
2023-05-14 21:45:37 -0500

    Document the ResourceView trait
    Signed-off-by: Xudong Sun <xudongs1@vmware.com>

--- src/kubernetes_api_objects/resource.rs
+++ src/kubernetes_api_objects/resource.rs
@@ -11,12 +11,21 @@ use kube::api::DynamicObject as K8SDynamicObject;
 
 verus! {
 
-
+/// This trait defines the methods that each ghost type of Kubernetes resource object should implement
 pub trait ResourceView: Sized {
     // TODO: make metadata() a trait method
+    // TODO: implement this trait for all k8s resource types
+
+    /// Get the reference of the object
     open spec fn object_ref(self) -> ObjectRef;
+
+    /// Convert the object to a dynamic object
     open spec fn to_dynamic_object(self) -> DynamicObjectView;
+
+    /// Convert back from a dynamic object
     open spec fn from_dynamic_object(obj: DynamicObjectView) -> Self;
+
+    /// Check if the data integrity is preserved after converting to and back from dynamic object
     proof fn integrity_check()
         ensures forall |o: Self| o == Self::from_dynamic_object(#[trigger] o.to_dynamic_object());
 }
