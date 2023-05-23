ba883706ef4d78d810ad46ef76241e95f8c3ed01
Xudong Sun <xudongs3@illinois.edu>
2023-05-22 19:49:00 -0500

    WIP: implement statefulset types
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/kubernetes_api_objects/stateful_set.rs
+++ src/kubernetes_api_objects/stateful_set.rs
@@ -1,26 +1,31 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
+use crate::kubernetes_api_objects::api_resource::*;
 use crate::kubernetes_api_objects::common::*;
 use crate::kubernetes_api_objects::dynamic::*;
 use crate::kubernetes_api_objects::object_meta::*;
 use crate::kubernetes_api_objects::resource::*;
 use vstd::prelude::*;
 
-use k8s_openapi::api::apps::v1::StatefulSet as K8SStatefulSet;
-use k8s_openapi::api::apps::v1::StatefulSetSpec as K8SStatefulSetSpec;
-use k8s_openapi::api::apps::v1::StatefulSetStatus as K8SStatefulSetStatus;
-
 verus! {
 
 #[verifier(external_body)]
 pub struct StatefulSet {
-    inner: K8SStatefulSet,
+    inner: k8s_openapi::api::apps::v1::StatefulSet,
+}
+
+#[verifier(external_body)]
+pub struct StatefulSetSpec {
+    inner: k8s_openapi::api::apps::v1::StatefulSetSpec,
 }
 
 pub struct StatefulSetView {
     pub metadata: ObjectMetaView,
-    // pub spec: Option<StatefulSetSpecView>,
-    // pub status: Option<StatefulSetStatusView>,
+    pub spec: Option<StatefulSetSpecView>,
+}
+
+pub struct StatefulSetSpecView {
+    pub replicas: Option<int>,
 }
 
 impl StatefulSet {
@@ -32,7 +37,7 @@ impl StatefulSet {
             stateful_set@ == StatefulSetView::default(),
     {
         StatefulSet {
-            inner: K8SStatefulSet::default(),
+            inner: k8s_openapi::api::apps::v1::StatefulSet::default(),
         }
     }
 
@@ -44,33 +49,59 @@ impl StatefulSet {
         todo!()
     }
 
-    // #[verifier(external_body)]
-    // pub fn spec(&self) -> (spec: Option<StatefulSetSpec>)
-    //     ensures
-    //         self@.spec.is_Some() == spec.is_Some(),
-    //         spec.is_Some() ==> spec.get_Some_0()@ == self@.spec.get_Some_0(),
-    // {
-    //     todo!()
-    // }
+    #[verifier(external_body)]
+    pub fn spec(&self) -> (spec: Option<StatefulSetSpec>)
+        ensures
+            self@.spec.is_Some() == spec.is_Some(),
+            spec.is_Some() ==> spec.get_Some_0()@ == self@.spec.get_Some_0(),
+    {
+        todo!()
+    }
 
-    // #[verifier(external_body)]
-    // pub fn status(&self) -> (status: Option<StatefulSetStatus>)
-    //     ensures
-    //         self@.status.is_Some() == status.is_Some(),
-    //         status.is_Some() ==> status.get_Some_0()@ == self@.status.get_Some_0(),
-    // {
-    //     todo!()
-    // }
+    #[verifier(external)]
+    pub fn into_kube_obj(self) -> k8s_openapi::api::apps::v1::StatefulSet {
+        self.inner
+    }
+
+    #[verifier(external_body)]
+    pub fn api_resource() -> (res: ApiResource)
+        ensures
+            res@.kind == Kind::CustomResourceKind,
+    {
+        ApiResource::from_kube_api_resource(kube::api::ApiResource::erase::<k8s_openapi::api::apps::v1::StatefulSet>(&()))
+    }
+
+    // NOTE: This function assumes serde_json::to_string won't fail!
+    #[verifier(external_body)]
+    pub fn to_dynamic_object(self) -> (obj: DynamicObject)
+        ensures
+            obj@ == self@.to_dynamic_object(),
+    {
+        DynamicObject::from_kube_obj(
+            k8s_openapi::serde_json::from_str(&k8s_openapi::serde_json::to_string(&self.inner).unwrap()).unwrap()
+        )
+    }
+
+    /// Convert a DynamicObject to a ConfigMap
+    // NOTE: This function assumes try_parse won't fail!
+    #[verifier(external_body)]
+    pub fn from_dynamic_object(obj: DynamicObject) -> (sts: StatefulSet)
+        ensures
+            sts@ == StatefulSetView::from_dynamic_object(obj@),
+    {
+        StatefulSet { inner: obj.into_kube_obj().try_parse::<k8s_openapi::api::apps::v1::StatefulSet>().unwrap() }
+    }
 }
 
 impl StatefulSetView {
     pub open spec fn default() -> StatefulSetView {
         StatefulSetView {
             metadata: ObjectMetaView::default(),
-            // spec: Option::None,
-            // status: Option::None,
+            spec: Option::None,
         }
     }
+
+    pub open spec fn spec_field() -> nat {0}
 }
 
 impl ResourceView for StatefulSetView {
@@ -94,26 +125,23 @@ impl ResourceView for StatefulSetView {
         DynamicObjectView {
             kind: self.kind(),
             metadata: self.metadata,
-            data: Value::Object(Map::empty()),
+            data: Value::Object(Map::empty()
+                                    .insert(Self::spec_field(), if self.spec.is_None() {Value::Null} else {
+                                        self.spec.get_Some_0().marshal()
+                                    })),
         }
     }
 
     open spec fn from_dynamic_object(obj: DynamicObjectView) -> StatefulSetView {
         StatefulSetView {
             metadata: obj.metadata,
+            spec: if obj.data.get_Object_0()[Self::spec_field()].is_Null() {Option::None} else {
+                Option::Some(StatefulSetSpecView::unmarshal(obj.data.get_Object_0()[Self::spec_field()]))
+            },
         }
     }
 
     proof fn integrity_check() {}
-}
-
-#[verifier(external_body)]
-pub struct StatefulSetSpec {
-    inner: K8SStatefulSetSpec,
-}
-
-pub struct StatefulSetSpecView {
-    // A lot more fields to specify...
 }
 
 impl StatefulSetSpec {
@@ -125,44 +153,40 @@ impl StatefulSetSpec {
             stateful_set_spec@ == StatefulSetSpecView::default(),
     {
         StatefulSetSpec {
-            inner: K8SStatefulSetSpec::default(),
+            inner: k8s_openapi::api::apps::v1::StatefulSetSpec::default(),
         }
     }
 }
 
 impl StatefulSetSpecView {
     pub open spec fn default() -> StatefulSetSpecView {
-       StatefulSetSpecView {}
-    }
-}
-
-#[verifier(external_body)]
-pub struct StatefulSetStatus {
-    inner: K8SStatefulSetStatus,
-}
-
-pub struct StatefulSetStatusView {
-    // A lot more fields to specify...
-}
-
-impl StatefulSetStatus {
-    pub spec fn view(&self) -> StatefulSetStatusView;
-
-    #[verifier(external_body)]
-    pub fn default() -> (stateful_set_status: StatefulSetStatus)
-        ensures
-            stateful_set_status@ == StatefulSetStatusView::default(),
-    {
-        StatefulSetStatus {
-            inner: K8SStatefulSetStatus::default(),
+        StatefulSetSpecView {
+            replicas: Option::None,
         }
     }
-}
 
-impl StatefulSetStatusView {
-    pub open spec fn default() -> StatefulSetStatusView {
-       StatefulSetStatusView {}
+    pub open spec fn marshal(self) -> Value {
+        Value::Object(
+            Map::empty()
+                .insert(Self::replicas_field(), if self.replicas.is_None() {Value::Null} else {
+                    Value::Int(self.replicas.get_Some_0())
+                })
+        )
     }
+
+    pub open spec fn unmarshal(value: Value) -> Self {
+        StatefulSetSpecView {
+            replicas: if value.get_Object_0()[Self::replicas_field()].is_Null() {Option::None} else {
+                Option::Some(value.get_Object_0()[Self::replicas_field()].get_Int_0())
+            },
+        }
+    }
+
+    proof fn integrity_check()
+        ensures forall |o: Self| o == Self::unmarshal(#[trigger] o.marshal()),
+    {}
+
+    pub open spec fn replicas_field() -> nat {0}
 }
 
 }
