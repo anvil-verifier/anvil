9ec600c12499bb237dc7660f8bc4af83a3fc4d77
Xudong Sun <xudongs3@illinois.edu>
2023-05-23 22:19:58 -0500

    WIP: Implement PersistentVolumeClaim
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/kubernetes_api_objects/persistent_volume_claim.rs
+++ src/kubernetes_api_objects/persistent_volume_claim.rs
@@ -5,23 +5,15 @@ use crate::kubernetes_api_objects::dynamic::*;
 use crate::kubernetes_api_objects::marshal::*;
 use crate::kubernetes_api_objects::object_meta::*;
 use crate::kubernetes_api_objects::resource::*;
+use crate::pervasive_ext::string_view::*;
 use vstd::prelude::*;
-
-use k8s_openapi::api::core::v1::PersistentVolumeClaim as K8SPersistentVolumeClaim;
-use k8s_openapi::api::core::v1::PersistentVolumeClaimSpec as K8SPersistentVolumeClaimSpec;
-use k8s_openapi::api::core::v1::PersistentVolumeClaimStatus as K8SPersistentVolumeClaimStatus;
+use vstd::seq_lib::*;
 
 verus! {
 
 #[verifier(external_body)]
 pub struct PersistentVolumeClaim {
-    inner: K8SPersistentVolumeClaim,
-}
-
-pub struct PersistentVolumeClaimView {
-    pub metadata: ObjectMetaView,
-    // pub spec: Option<PersistentVolumeClaimSpecView>,
-    // pub status: Option<PersistentVolumeClaimStatusView>,
+    inner: k8s_openapi::api::core::v1::PersistentVolumeClaim,
 }
 
 impl PersistentVolumeClaim {
@@ -33,7 +25,7 @@ impl PersistentVolumeClaim {
             pvc@ == PersistentVolumeClaimView::default(),
     {
         PersistentVolumeClaim {
-            inner: K8SPersistentVolumeClaim::default(),
+            inner: k8s_openapi::api::core::v1::PersistentVolumeClaim::default(),
         }
     }
 
@@ -45,33 +37,49 @@ impl PersistentVolumeClaim {
         todo!()
     }
 
-    // #[verifier(external_body)]
-    // pub fn spec(&self) -> (spec: Option<PersistentVolumeClaimSpec>)
-    //     ensures
-    //         self@.spec.is_Some() == spec.is_Some(),
-    //         spec.is_Some() ==> spec.get_Some_0()@ == self@.spec.get_Some_0(),
-    // {
-    //     todo!()
-    // }
+    #[verifier(external_body)]
+    pub fn spec(&self) -> (spec: Option<PersistentVolumeClaimSpec>)
+        ensures
+            self@.spec.is_Some() == spec.is_Some(),
+            spec.is_Some() ==> spec.get_Some_0()@ == self@.spec.get_Some_0(),
+    {
+        todo!()
+    }
+}
 
-    // #[verifier(external_body)]
-    // pub fn status(&self) -> (status: Option<PersistentVolumeClaimStatus>)
-    //     ensures
-    //         self@.status.is_Some() == status.is_Some(),
-    //         status.is_Some() ==> status.get_Some_0()@ == self@.status.get_Some_0(),
-    // {
-    //     todo!()
-    // }
+#[verifier(external_body)]
+pub struct PersistentVolumeClaimSpec {
+    inner: k8s_openapi::api::core::v1::PersistentVolumeClaimSpec,
+}
+
+impl PersistentVolumeClaimSpec {
+    pub spec fn view(&self) -> PersistentVolumeClaimSpecView;
+
+    #[verifier(external_body)]
+    pub fn default() -> (pvc_spec: PersistentVolumeClaimSpec)
+        ensures
+            pvc_spec@ == PersistentVolumeClaimSpecView::default(),
+    {
+        PersistentVolumeClaimSpec {
+            inner: k8s_openapi::api::core::v1::PersistentVolumeClaimSpec::default(),
+        }
+    }
+}
+
+pub struct PersistentVolumeClaimView {
+    pub metadata: ObjectMetaView,
+    pub spec: Option<PersistentVolumeClaimSpecView>,
 }
 
 impl PersistentVolumeClaimView {
     pub open spec fn default() -> PersistentVolumeClaimView {
         PersistentVolumeClaimView {
             metadata: ObjectMetaView::default(),
-            // spec: Option::None,
-            // status: Option::None,
+            spec: Option::None,
         }
     }
+
+    pub open spec fn spec_field() -> nat {0}
 }
 
 impl ResourceView for PersistentVolumeClaimView {
@@ -95,75 +103,80 @@ impl ResourceView for PersistentVolumeClaimView {
         DynamicObjectView {
             kind: self.kind(),
             metadata: self.metadata,
-            data: Value::Object(Map::empty()),
+            data: Value::Object(Map::empty()
+                                    .insert(Self::spec_field(), if self.spec.is_None() {Value::Null} else {
+                                        self.spec.get_Some_0().marshal()
+                                    })),
         }
     }
 
     open spec fn from_dynamic_object(obj: DynamicObjectView) -> PersistentVolumeClaimView {
         PersistentVolumeClaimView {
             metadata: obj.metadata,
+            spec: if obj.data.get_Object_0()[Self::spec_field()].is_Null() {Option::None} else {
+                Option::Some(PersistentVolumeClaimSpecView::unmarshal(obj.data.get_Object_0()[Self::spec_field()]))
+            },
         }
     }
 
-    proof fn integrity_check() {}
-}
-
-#[verifier(external_body)]
-pub struct PersistentVolumeClaimSpec {
-    inner: K8SPersistentVolumeClaimSpec,
+    proof fn integrity_check() {
+        assert forall |o: Self| o == Self::from_dynamic_object(#[trigger] o.to_dynamic_object()) by {
+            if o.spec.is_Some() && o.spec.get_Some_0().access_modes.is_Some() {
+                assert_seqs_equal!(
+                    o.spec.get_Some_0().access_modes.get_Some_0(),
+                    Self::from_dynamic_object(o.to_dynamic_object()).spec.get_Some_0().access_modes.get_Some_0()
+                );
+            }
+        }
+    }
 }
 
 pub struct PersistentVolumeClaimSpecView {
-    // A lot more fields to specify...
-}
-
-impl PersistentVolumeClaimSpec {
-    pub spec fn view(&self) -> PersistentVolumeClaimSpecView;
-
-    #[verifier(external_body)]
-    pub fn default() -> (pvc_spec: PersistentVolumeClaimSpec)
-        ensures
-            pvc_spec@ == PersistentVolumeClaimSpecView::default(),
-    {
-        PersistentVolumeClaimSpec {
-            inner: K8SPersistentVolumeClaimSpec::default(),
-        }
-    }
+    pub access_modes: Option<Seq<StringView>>,
 }
 
 impl PersistentVolumeClaimSpecView {
     pub open spec fn default() -> PersistentVolumeClaimSpecView {
-       PersistentVolumeClaimSpecView {}
-    }
-}
-
-#[verifier(external_body)]
-pub struct PersistentVolumeClaimStatus {
-    inner: K8SPersistentVolumeClaimStatus,
-}
-
-pub struct PersistentVolumeClaimStatusView {
-    // A lot more fields to specify...
-}
-
-impl PersistentVolumeClaimStatus {
-    pub spec fn view(&self) -> PersistentVolumeClaimStatusView;
-
-    #[verifier(external_body)]
-    pub fn default() -> (pvc_status: PersistentVolumeClaimStatus)
-        ensures
-            pvc_status@ == PersistentVolumeClaimStatusView::default(),
-    {
-        PersistentVolumeClaimStatus {
-            inner: K8SPersistentVolumeClaimStatus::default(),
+        PersistentVolumeClaimSpecView {
+            access_modes: Option::None,
         }
     }
-}
 
-impl PersistentVolumeClaimStatusView {
-    pub open spec fn default() -> PersistentVolumeClaimStatusView {
-       PersistentVolumeClaimStatusView {}
+    pub open spec fn set_access_modes(self, access_modes: Seq<StringView>) -> PersistentVolumeClaimSpecView {
+        PersistentVolumeClaimSpecView {
+            access_modes: Option::Some(access_modes),
+            ..self
+        }
     }
+
+    pub open spec fn marshal(self) -> Value {
+        Value::Object(
+            Map::empty()
+                .insert(Self::access_modes_field(), if self.access_modes.is_None() {Value::Null} else {
+                    Value::Array(self.access_modes.get_Some_0().map_values(|mode: StringView| Value::String(mode)))
+                })
+        )
+    }
+
+    pub open spec fn unmarshal(value: Value) -> Self {
+        PersistentVolumeClaimSpecView {
+            access_modes: if value.get_Object_0()[Self::access_modes_field()].is_Null() {Option::None} else {
+                Option::Some(value.get_Object_0()[Self::access_modes_field()].get_Array_0().map_values(|v: Value| v.get_String_0()))
+            },
+        }
+    }
+
+    proof fn integrity_check()
+        ensures forall |o: Self| o == Self::unmarshal(#[trigger] o.marshal())
+    {
+        assert forall |o: Self| o == Self::unmarshal(#[trigger] o.marshal()) by {
+            if o.access_modes.is_Some() {
+                assert_seqs_equal!(o.access_modes.get_Some_0(), Self::unmarshal(o.marshal()).access_modes.get_Some_0());
+            }
+        }
+    }
+
+    pub open spec fn access_modes_field() -> nat {0}
 }
 
 }
