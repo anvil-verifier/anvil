04931f036ecdd1acf0fb491b831e5c5d76a45669
Xudong Sun <xudongs3@illinois.edu>
2023-05-28 22:42:04 -0500

    Update spec reconcile_core to take Object
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/reconciler/spec/mod.rs
+++ src/reconciler/spec/mod.rs
@@ -22,7 +22,7 @@ pub struct Reconciler<#[verifier(maybe_negative)] K: ResourceView, #[verifier(ma
     // reconcile_core describes the logic of reconcile function and is the key logic we want to verify.
     // Each reconcile_core should take the local state and a response of the previous request (if any) as input
     // and outputs the next local state and the request to send to Kubernetes API (if any).
-    pub reconcile_core: ReconcileCore<T>,
+    pub reconcile_core: ReconcileCore<K, T>,
 
     // reconcile_done is used to tell the controller_runtime whether this reconcile round is done.
     // If it is true, controller_runtime will probably requeue the reconcile.
@@ -31,16 +31,11 @@ pub struct Reconciler<#[verifier(maybe_negative)] K: ResourceView, #[verifier(ma
     // reconcile_error is used to tell the controller_runtime whether this reconcile round returns with error.
     // If it is true, controller_runtime will requeue the reconcile.
     pub reconcile_error: ReconcileError<T>,
-
-    // use K so that Rust no longer complains about "parameter `K` is never used"
-    pub consume_kubernetes_resource_type: FnSpec(K) -> K,
 }
 
 pub type ReconcileInitState<T> = FnSpec() -> T;
 
-pub type ReconcileTrigger = FnSpec(Message) -> Option<ObjectRef>;
-
-pub type ReconcileCore<T> = FnSpec(ObjectRef, Option<APIResponse>, T) -> (T, Option<APIRequest>);
+pub type ReconcileCore<K, T> = FnSpec(K, Option<APIResponse>, T) -> (T, Option<APIRequest>);
 
 pub type ReconcileDone<T> = FnSpec(T) -> bool;
 
