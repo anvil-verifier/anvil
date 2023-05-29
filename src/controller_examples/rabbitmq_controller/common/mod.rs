b8c23028f88325b610a3ef37edde399fa033edfc
ZichengMa <zma17@illinois.edu>
2023-05-29 17:17:01 -0500

    add service account and rabbitmq steps
    Signed-off-by: ZichengMa <zma17@illinois.edu>

---
+++ src/controller_examples/rabbitmq_controller/common/mod.rs
@@ -0,0 +1,26 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+#![allow(unused_imports)]
+use builtin::*;
+use builtin_macros::*;
+
+verus! {
+
+#[is_variant]
+pub enum RabbitmqReconcileStep {
+    Init,
+    AfterGetRBMQ,
+    AfterCreateHeadlessService,
+    AfterCreateService,
+    AfterCreateErlangCookieSecret,
+    AfterCreateDefaultUserSecret,
+    AfterCreatePluginsConfigMap,
+    AfterCreateServerConfigMap,
+    AfterCreateServiceAccount,
+    AfterCreateRole,
+    AfterCreateRoleBinding,
+    Done,
+    Error,
+}
+
+}
