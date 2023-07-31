a20812b1f81935a865c23fdf31e25530a590bf8b
Xudong Sun <xudongs3@illinois.edu>
2023-07-30 21:17:02 -0500

    Add Dockerfile, deploy scripts and CI job for the fluent controller
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

---
+++ src/fluent_controller.rs
@@ -0,0 +1,15 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+
+#![allow(unused_imports)]
+
+pub mod kubernetes_api_objects;
+pub mod kubernetes_cluster;
+pub mod pervasive_ext;
+pub mod reconciler;
+pub mod shim_layer;
+pub mod state_machine;
+pub mod temporal_logic;
+
+// TODO(xudong): implement verified fluent-controller
+fn main() {}
