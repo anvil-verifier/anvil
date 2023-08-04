cb68ad5de32fb36f733b99cd262bce2044cf03d3
Wenjie Ma <rebekah1368@gmail.com>
2023-08-03 22:46:03 -0500

    Document the Cluster type
    Signed-off-by: Wenjie Ma <rebekah1368@gmail.com>

--- src/kubernetes_cluster/mod.rs
+++ src/kubernetes_cluster/mod.rs
@@ -5,6 +5,16 @@ pub mod spec;
 
 use std::marker::PhantomData;
 
+/// The Cluster struct is a container for all the specifications and lemmas regarding the Kubernetes cluster, including
+/// those seperate state machines and the compound state machine. It take three generics, which should be essentially one:
+/// R is the type of Reconciler and K and E are the two generics fed to R. We don't bound K, E or R here but will do that
+/// when implementing methods for it.
+/// 
+/// By using such a struct, we don't have to let all the functions carry the generics; and therefore we don't need to
+/// specify the generics whenever calling those spec or proof functions.
+/// 
+/// Three markers (PhantomData) are used here to make the three generics not "unused". Unused generics will trigger
+/// compilation errors.
 pub struct Cluster<K, E, R> {
     _marker1: PhantomData<K>,
     _marker2: PhantomData<E>,
