5f46d705b83c86cc7ee781e7a04d49c79caca293
Xudong Sun <xudongs3@illinois.edu>
2023-08-19 11:48:22 -0500

    Add options to deploy.sh
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- deploy.sh
+++ deploy.sh
@@ -4,47 +4,37 @@
 ##
 ## Requires a running Kubernetes cluster and kubectl to be installed.
 
-set -eu
+set -xeu
 
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-if [ "$1" = "simple" ]; then
-    if cd deploy/simple && kubectl apply -f crd.yaml && kubectl apply -f deploy.yaml; then
-        echo ""
-        echo -e "${GREEN}Simple controller is deployed in your Kubernetes cluster in namespace \"simple\"."
-        echo -e "Run \"kubectl get pod -n simple\" to check the controller pod."
-        echo -e "Run \"kubectl apply -f deploy/simple/simplecr.yaml\" to deploy a simple custom resource.${NC}"
-    else
-        echo ""
-        echo -e "${RED}Cannot deploy the controller."
-        echo -e "${YELLOW}Please ensure kubectl can connect to a Kubernetes cluster.${NC}"
-    fi
-elif [ "$1" = "zookeeper" ]; then
-    if cd deploy/zookeeper && kubectl apply -f crd.yaml && kubectl apply -f deploy.yaml; then
-        echo ""
-        echo -e "${GREEN}Zookeeper controller is deployed in your Kubernetes cluster in namespace \"zookeeper\"."
-        echo -e "Run \"kubectl get pod -n zookeeper\" to check the controller pod."
-        echo -e "Run \"kubectl apply -f deploy/zookeeper/zookeeper.yaml\" to deploy a zookeeper cluster custom resource.${NC}"
-    else
-        echo ""
-        echo -e "${RED}Cannot deploy the controller."
-        echo -e "${YELLOW}Please ensure kubectl can connect to a Kubernetes cluster.${NC}"
-    fi
-elif [ "$1" = "rabbitmq" ]; then
-    if cd deploy/rabbitmq && kubectl apply -f crd.yaml && kubectl apply -f deploy.yaml; then
-        echo ""
-        echo -e "${GREEN}Rabbitmq controller is deployed in your Kubernetes cluster in namespace \"rabbitmq\"."
-        echo -e "Run \"kubectl get pod -n rabbitmq\" to check the controller pod."
-        echo -e "Run \"kubectl apply -f deploy/rabbitmq/rabbitmq.yaml\" to deploy a rabbitmq cluster custom resource.${NC}"
-    else
-        echo ""
-        echo -e "${RED}Cannot deploy the controller."
-        echo -e "${YELLOW}Please ensure kubectl can connect to a Kubernetes cluster.${NC}"
-    fi
+app=$1
+registry=$2
+
+if [ "$app" != "zookeeper" ] && [ "$app" != "rabbitmq" ] && [ "$app" != "fluent" ]; then
+    echo -e "${RED}The first argument has to be one of: zookeeper, rabbitmq, fluent.${NC}"
+    exit 1    
+fi
+
+if [ "$registry" != "remote" ] && [ "$registry" != "local" ]; then
+    echo -e "${RED}The second argument has to be one of: remote, local.${NC}"
+    exit 2
+fi
+
+if ["$registry" = "local"]; then
+    kind load docker-image local/$app-controller:v0.1.0
+fi
+
+if cd deploy/$1 && kubectl apply -f crd.yaml && kubectl apply -f deploy_$registry.yaml; then
+    echo ""
+    echo -e "${GREEN}The $app controller is deployed in your Kubernetes cluster in namespace \"$app\"."
+    echo -e "Run \"kubectl get pod -n $app\" to check the controller pod."
+    echo -e "Run \"kubectl apply -f deploy/$app/$app.yaml\" to deploy the cluster custom resource(s).${NC}"
 else
-    echo -e "${RED}Unsupported app name."
-    echo -e "${YELLOW}Currently supported app: simple.${NC}"
+    echo ""
+    echo -e "${RED}Cannot deploy the controller."
+    echo -e "${YELLOW}Please ensure kubectl can connect to a Kubernetes cluster.${NC}"
 fi
