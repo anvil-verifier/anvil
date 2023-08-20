3c0c7c4e33d811cff7dcfa53f856bd674a7ae96d
Xudong Sun <xudongs3@illinois.edu>
2023-08-19 19:58:21 -0500

    Add comments
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/controller_examples/zookeeper_controller/spec/zookeeper_api.rs
+++ src/controller_examples/zookeeper_controller/spec/zookeeper_api.rs
@@ -53,18 +53,34 @@ impl ExternalAPI for ZKAPI {
     }
 }
 
+// The exec version of this method sets up a connection to the zookeeper cluster,
+// creates the node ("/zookeeper-operator/{zk_name}") (if not exists),
+// and sets its data to the replicas number.
+// The behavior can be abstracted as writing the replicas number to an "address"
+// identified by the uri of the zookeeper cluster and the node path (uri, zk_name).
+// Note that the uri is decided by zk_name and zk_namespace, that is,
+// {zk_name}-client.{zk_namespace}.svc.cluster.local:2181 (here we hardcode the port to 2181).
+// Assuming the exec version correctly constructs the uri, we can further simplify the "address"
+// as (zk_name, zk_namespace). That's why the key of the map is an ObjectRef (the kind is useless here.).
+//
+// NOTE: we assume the parent node ("/zookeeper-operator") already exists or successfully gets created
+// by the exec version so we don't model it in the spec version.
+// We also omit the "CLUSTER_SIZE=" prefix since it is always written with the replicas.
+//
+// TODO: the address should probably also include the uid of the stateful set.
+// If the stateful set (with the same name) gets deleted and recrated,
+// the controller, when connecting to the same uri, essentially talks to a different zookeeper cluster.
+// Using uid in the address can help to distinguish different zookeeper clusters (with the same name).
+//
+// TODO: the result of this zk api should also depend on the cluster state, for example
+// whether the client service object and the zookeeper stateful set object exist.
+// Specifying such dependency would require us to pass the cluster state into this function.
+//
+// TODO: specify version number of zookeeper node to reason about potential race between the current API call and
+// the old API call from the previously crashed controller.
 pub open spec fn set_zk_node(
     zk_name: StringView, zk_namespace: StringView, replicas: StringView, state: ZooKeeperState
 ) -> (Option<ZKAPIOutputView>, ZooKeeperState) {
-    // Here we assume the parent node ("/zookeeper-operator") already exists so we don't model it.
-    // We also omit the "CLUSTER_SIZE=" prefix since it is always written with the replicas.
-    //
-    // TODO: the result of this zk api should also depend on the cluster state, for example
-    // whether the client service object and the zookeeper stateful set object exist.
-    // Specifying such dependency would require us to pass the cluster state (the map) into this function.
-    //
-    // TODO: specify version number to reason about potential race between the current API call and
-    // the old API call from the previously crashed controller.
     let key = ObjectRef {
         kind: Kind::CustomResourceKind,
         namespace: zk_namespace,
