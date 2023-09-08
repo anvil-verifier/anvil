94c627605fea92a0f7b376b6ff9495418deeac1d
Xudong Sun <xudongs3@illinois.edu>
2023-09-08 11:56:53 -0500

    Fix unit tests
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/unit_tests/object_meta.rs
+++ src/unit_tests/object_meta.rs
@@ -8,13 +8,6 @@ use vstd::string::*;
 
 verus! {
 
-// #[verifier(external)]
-// pub fn test() {
-//     println!("Running unit tests for kubernetes_api_objects::object_meta...");
-//     test_set_name();
-//     test_set_namespace();
-// }
-
 #[test]
 #[verifier(external)]
 pub fn test_set_name() {
@@ -27,7 +20,6 @@ pub fn test_set_name() {
 #[test]
 #[verifier(external)]
 pub fn test_set_namespace() {
-    println!("Testing set_namespace()...");
     let mut object_meta = ObjectMeta::default();
     object_meta.set_namespace(new_strlit("namespace").to_string());
     assert_eq!("namespace".to_string(), object_meta.into_kube().namespace.unwrap());
@@ -36,7 +28,6 @@ pub fn test_set_namespace() {
 #[test]
 #[verifier(external)]
 pub fn test_set_generate_name() {
-    println!("Testing set_generate_name()...");
     let mut object_meta = ObjectMeta::default();
     object_meta.set_generate_name(new_strlit("generate_name").to_string());
     assert_eq!("generate_name".to_string(), object_meta.into_kube().generate_name.unwrap());
@@ -45,51 +36,36 @@ pub fn test_set_generate_name() {
 #[test]
 #[verifier(external)]
 pub fn test_set_labels() {
-    println!("Testing set_labels()...");
     let mut object_meta = ObjectMeta::default();
     let mut labels = StringMap::new();
     labels.insert(new_strlit("key").to_string(), new_strlit("value").to_string());
-    object_meta.set_labels(labels);
-    assert_eq!(1, object_meta.into_kube().labels.unwrap().len());
-    // assert_eq!("value".to_string(), object_meta.into_kube().labels.unwrap().get("key").unwrap().to_string());
+    object_meta.set_labels(labels.clone());
+    assert_eq!(labels.into_rust_map(), object_meta.into_kube().labels.unwrap());
 }
 
 #[test]
 #[verifier(external)]
-pub fn test_set_add_annotations() {
-    println!("Testing set_annotations()...");
+pub fn test_set_annotations() {
     let mut object_meta = ObjectMeta::default();
     let mut annotations = StringMap::new();
     annotations.insert(new_strlit("key").to_string(), new_strlit("value").to_string());
-    object_meta.set_annotations(annotations);
-    assert_eq!(1, object_meta.into_kube().annotations.unwrap().len());
-    // assert_eq!("value".to_string(), object_meta.into_kube().annotations.unwrap().get("key").unwrap().to_string());
-    // println!("Testing add_annotations()...");
-    // object_meta.add_annotation(new_strlit("key2").to_string(), new_strlit("value2").to_string());
-    // assert_eq!(2, object_meta.into_kube().annotations.unwrap().len());
-    // assert_eq!("value2".to_string(), object_meta.into_kube().annotations.unwrap().get("key2").unwrap().to_string());
-}
-
-#[test]
-#[verifier(external)]
-pub fn test_set_owner_references() {
-  //@TODO  Need some discussion about this because we do not have set functions to ownerreference
+    object_meta.set_annotations(annotations.clone());
+    assert_eq!(annotations.into_rust_map(), object_meta.into_kube().annotations.unwrap());
 }
 
 #[test]
 #[verifier(external)]
 pub fn test_set_finalizers() {
-    println!("Testing set_finalizers()...");
     let mut object_meta = ObjectMeta::default();
-    let mut finalizers = Vec::new();
-    finalizers.push(new_strlit("finalizer").to_string());
-    object_meta.set_finalizers(finalizers);
-    assert_eq!(1, object_meta.into_kube().finalizers.unwrap().len());
-    // assert_eq!("finalizer".to_string(), object_meta.into_kube().finalizers.unwrap().get(0).unwrap().to_string());
-    // println!("Testing unset_finalizers()...");
-    // object_meta.unset_finalizers();
-    // assert_eq!( None, object_meta.into_kube().finalizers);
-
+    // We use a closure to generate the Vec<vstd::string::String>
+    // because we cannot use clone() of it.
+    let finalizers_gen = || {
+        let mut finalizers = Vec::new();
+        finalizers.push(new_strlit("finalizer").to_string());
+        finalizers
+    };
+    object_meta.set_finalizers(finalizers_gen());
+    assert_eq!(finalizers_gen().into_iter().map(|s: String| s.into_rust_string()).collect::<Vec<_>>(), object_meta.into_kube().finalizers.unwrap());
 }
 
 
