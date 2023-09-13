dd1712cd40c66d1f34b498be99330acd47924faf
Xudong Sun <xudongs3@illinois.edu>
2023-09-13 15:16:13 -0500

    Add unit test to cover extend
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

---
+++ src/unit_tests/vstd_ext/string_map.rs
@@ -0,0 +1,28 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+use crate::pervasive_ext::string_map::*;
+use vstd::prelude::*;
+use vstd::string::*;
+
+verus! {
+
+#[test]
+#[verifier(external)]
+pub fn test_extend() {
+    let mut m = StringMap::empty();
+    m.insert(new_strlit("key1").to_string(), new_strlit("val1").to_string());
+    m.insert(new_strlit("key2").to_string(), new_strlit("val2").to_string());
+
+    let mut m2 = StringMap::empty();
+    m.insert(new_strlit("key1").to_string(), new_strlit("new_val1").to_string());
+    m.insert(new_strlit("key3").to_string(), new_strlit("val3").to_string());
+
+    m.extend(m2);
+
+    let rust_map = m.into_rust_map();
+    assert_eq!(rust_map.get(&"key1".to_string()), Some(&"new_val1".to_string()));
+    assert_eq!(rust_map.get(&"key2".to_string()), Some(&"val2".to_string()));
+    assert_eq!(rust_map.get(&"key3".to_string()), Some(&"val3".to_string()));
+}
+
+}
