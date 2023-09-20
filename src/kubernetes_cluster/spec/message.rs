4f10abfb08244c0eb814296b4082e6a6bed423dd
Xudong Sun <xudongs3@illinois.edu>
2023-09-20 15:33:14 -0500

    Revert changes to avoid conflicts
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/kubernetes_cluster/spec/message.rs
+++ src/kubernetes_cluster/spec/message.rs
@@ -258,7 +258,7 @@ pub open spec fn form_matched_resp_msg(req_msg: Message<I, O>, result: Result<Dy
 {
     match req_msg.content.get_APIRequest_0() {
         APIRequest::GetRequest(_) => Self::form_get_resp_msg(req_msg, result),
-        APIRequest::ListRequest(_) => Self::form_list_resp_msg(req_msg, Err(APIError::Invalid)), // TODO: support list
+        APIRequest::ListRequest(_) => Self::form_list_resp_msg(req_msg, Err(APIError::Invalid)),
         APIRequest::CreateRequest(_) => Self::form_create_resp_msg(req_msg, result),
         APIRequest::DeleteRequest(_) => Self::form_delete_resp_msg(req_msg, result),
         APIRequest::UpdateRequest(_) => Self::form_update_resp_msg(req_msg, result),
