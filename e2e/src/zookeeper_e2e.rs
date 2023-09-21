5f6dfd4fd556a511cad3b06ae47926102b19fa68
Xudong Sun <xudongs3@illinois.edu>
2023-09-21 00:00:52 -0500

    Simplify ephemeral test again
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- e2e/src/zookeeper_e2e.rs
+++ e2e/src/zookeeper_e2e.rs
@@ -915,8 +915,6 @@ pub async fn zookeeper_ephemeral_e2e_test() -> Result<(), Error> {
     let zk_name = apply(zookeeper_cluster_ephemeral(), client.clone(), &discovery).await?;
 
     desired_state_test(client.clone(), zk_name.clone()).await?;
-    reconfiguration_test(client.clone(), zk_name.clone()).await?;
-    upgrading_test(client.clone(), zk_name.clone()).await?;
     scaling_test(client.clone(), zk_name.clone()).await?;
     zk_workload_test(client.clone(), zk_name.clone()).await?;
 
