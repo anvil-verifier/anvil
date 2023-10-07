2af7463d827e83b4f258d3b1f0d7ea1928d145f2
ZichengMa <zma17@illinois.edu>
2023-10-07 07:14:11 -0500

    add tests for volume
    Signed-off-by: ZichengMa <zma17@illinois.edu>

--- src/unit_tests/kubernetes_api_objects/mod.rs
+++ src/unit_tests/kubernetes_api_objects/mod.rs
@@ -1,10 +1,14 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 pub mod config_map;
+pub mod configmap_volume_source;
 pub mod container;
+pub mod downwardapi_volume_source;
+pub mod empty_dir_volume_source;
 pub mod env_var;
 pub mod env_var_source;
 pub mod execaction;
+pub mod hostpath_volume_source;
 pub mod lifecycle;
 pub mod lifecycle_handler;
 pub mod object_field_selector;
@@ -13,6 +17,9 @@ pub mod pod;
 pub mod pod_spec;
 pub mod pod_template_spec;
 pub mod probe;
+pub mod projected_volume_source;
 pub mod resource_requirements;
+pub mod secret_volume_source;
 pub mod tcp_socket_action;
+pub mod volume;
 pub mod volume_mount;
