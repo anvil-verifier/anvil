b2c84c43d4b95a4eb737ded7995a69910ba6df9e
Xudong Sun <xudongs3@illinois.edu>
2023-10-17 14:34:16 -0500

    Remove redundant lemma
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/vstd_ext/map_lib.rs
+++ src/vstd_ext/map_lib.rs
@@ -25,37 +25,4 @@ pub proof fn union_prefer_right_self_changes_nothing<K, V>()
             map.union_prefer_right(map) =~= map,
 {}
 
-// // TODO: We will use the Verus native lemma_values_finite once we update to the most recent Verus.
-// pub proof fn lemma_values_finite<K, V>(m: Map<K, V>)
-//     requires m.dom().finite(),
-//     ensures m.values().finite(),
-//     decreases m.len(),
-// {
-//     if m.len() > 0 {
-//         let k = m.dom().choose();
-//         let v = m[k];
-//         let m1 = m.remove(k);
-//         assert(m.contains_key(k));
-//         assert(m.contains_value(v));
-//         assert_sets_equal!(m.values() == m1.values().insert(v), v0 => {
-//             if m.values().contains(v0) {
-//                 assert(m1.values().insert(v).contains(v0));
-//             }
-//             if m1.values().insert(v).contains(v0) {
-//                 if v0 == v {
-//                     assert(m.contains_value(v));
-//                     assert(m.values().contains(v0));
-//                 } else {
-//                     assert(m.values().contains(v0));
-//                 }
-//             }
-//         });
-//         assert(m1.len() < m.len());
-//         lemma_values_finite(m1);
-//         axiom_set_insert_finite(m1.values(), v);
-//     } else {
-//         assert(m.values() =~= Set::<V>::empty());
-//     }
-// }
-
 }
