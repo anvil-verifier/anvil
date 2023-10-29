8e82cfe49e90678004c589a9ea1d8e7c38e4c841
marshtompsxd <xudongs3@illinois.edu>
2023-10-28 22:34:04 -0500

    Remove unused and unverified lemmas from map lib (#395)
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/vstd_ext/map_lib.rs
+++ src/vstd_ext/map_lib.rs
@@ -9,20 +9,15 @@ verus! {
 // TODO: implement the spec function and prove the lemma
 pub closed spec fn select_map_to_seq<K, V>(m: Map<K, V>, f: FnSpec(K) -> bool) -> Seq<V>;
 
-#[verifier(external_body)]
-pub proof fn lemma_select_map_to_seq_membership<K, V>(m: Map<K, V>, f: FnSpec(K) -> bool)
-    ensures
-        // all the values whose keys satisfy f are in the sequence
-        forall |k: K| #[trigger] m.contains_key(k) && f(k) ==> select_map_to_seq(m, f).contains(m[k]),
-        // and the length of the filtered dom of m is the same as the length of the sequence
-        // which means the sequence does not have any other values
-        m.dom().filter(f).len() == select_map_to_seq(m, f).len(),
-{}
+// #[verifier(external_body)]
+// pub proof fn lemma_select_map_to_seq_membership<K, V>(m: Map<K, V>, f: FnSpec(K) -> bool)
+//     ensures
+//         // all the values whose keys satisfy f are in the sequence
+//         forall |k: K| #[trigger] m.contains_key(k) && f(k) ==> select_map_to_seq(m, f).contains(m[k]),
+//         // and the length of the filtered dom of m is the same as the length of the sequence
+//         // which means the sequence does not have any other values
+//         m.dom().filter(f).len() == select_map_to_seq(m, f).len(),
+// {}
 
-pub proof fn union_prefer_right_self_changes_nothing<K, V>()
-    ensures
-        forall |map: Map<K, V>|
-            map.union_prefer_right(map) =~= map,
-{}
 
 }
