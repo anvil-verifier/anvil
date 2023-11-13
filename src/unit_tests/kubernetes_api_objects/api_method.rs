4a503c721f9be8ecd17c768819bf4cf7b9101d8b
ZichengMa <zma17@illinois.edu>
2023-11-13 04:58:33 -0600

    add key and fmt tests
    Signed-off-by: ZichengMa <zma17@illinois.edu>

---
+++ src/unit_tests/kubernetes_api_objects/api_method.rs
@@ -0,0 +1,195 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+use crate::kubernetes_api_objects::api_method::*;
+use crate::kubernetes_api_objects::api_resource::*;
+use crate::kubernetes_api_objects::dynamic::*;
+use crate::kubernetes_api_objects::object_meta::*;
+use crate::kubernetes_api_objects::resource::*;
+use crate::vstd_ext::string_map::*;
+use deps_hack::chrono::{DateTime, Utc};
+use deps_hack::k8s_openapi::apimachinery::pkg::apis::meta::v1::Time;
+use vstd::prelude::*;
+use vstd::string::*;
+
+verus! {
+// Tests for api method
+#[test]
+#[verifier(external)]
+pub fn test_getrequest_key() {
+    let api_method = KubeGetRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        name: new_strlit("name").to_string(),
+        namespace: new_strlit("namespace").to_string(),
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace/name"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_listrequest_key() {
+    let api_method = KubeListRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        namespace: new_strlit("namespace").to_string(),
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_createquest_key() {
+    let api_method = KubeCreateRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        namespace: new_strlit("namespace").to_string(),
+        obj:  DynamicObject::from_kube(
+                deps_hack::kube::api::DynamicObject {
+                metadata: deps_hack::kube::api::ObjectMeta {
+                    name: Some("dyn_name".to_string()),
+                    namespace: Some("namespace".to_string()),
+                    ..Default::default()
+                },
+                types: Some(deps_hack::kube::api::TypeMeta {
+                    api_version: "api_version".to_string(),
+                    kind: "kind".to_string(),
+                }),
+                data: deps_hack::serde_json::json!({
+                    "key": "value",
+                }),
+            },
+        )
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace/dyn_name"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_deleterequest_key() {
+    let api_method = KubeDeleteRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        name: new_strlit("name").to_string(),
+        namespace: new_strlit("namespace").to_string(),
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace/name"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_updaterequest_key() {
+    let api_method = KubeUpdateRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        name: new_strlit("name").to_string(),
+        namespace: new_strlit("namespace").to_string(),
+        obj:  DynamicObject::from_kube(
+                deps_hack::kube::api::DynamicObject {
+                metadata: deps_hack::kube::api::ObjectMeta {
+                    name: Some("dyn_name".to_string()),
+                    namespace: Some("namespace".to_string()),
+                    ..Default::default()
+                },
+                types: Some(deps_hack::kube::api::TypeMeta {
+                    api_version: "api_version".to_string(),
+                    kind: "kind".to_string(),
+                }),
+                data: deps_hack::serde_json::json!({
+                    "key": "value",
+                }),
+            },
+        )
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace/name"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_updatestatusrequest_key() {
+    let api_method = KubeUpdateStatusRequest {
+        api_resource: ApiResource::from_kube(
+            deps_hack::kube::api::ApiResource {
+                group: "group".to_string(),
+                version: "version".to_string(),
+                kind: "kind".to_string(),
+                api_version: "api_version".to_string(),
+                plural: "plural".to_string(),
+            },
+        ),
+        name: new_strlit("name").to_string(),
+        namespace: new_strlit("namespace").to_string(),
+        obj:  DynamicObject::from_kube(
+                deps_hack::kube::api::DynamicObject {
+                metadata: deps_hack::kube::api::ObjectMeta {
+                    name: Some("dyn_name".to_string()),
+                    namespace: Some("namespace".to_string()),
+                    ..Default::default()
+                },
+                types: Some(deps_hack::kube::api::TypeMeta {
+                    api_version: "api_version".to_string(),
+                    kind: "kind".to_string(),
+                }),
+                data: deps_hack::serde_json::json!({
+                    "key": "value",
+                }),
+            },
+        )
+    };
+    assert_eq!(
+        api_method.key(),
+        "kind/namespace/name"
+    );
+}
+
+
+}
---
+++ src/unit_tests/kubernetes_api_objects/error.rs
@@ -0,0 +1,99 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+use crate::kubernetes_api_objects::dynamic::*;
+use crate::kubernetes_api_objects::error::*;
+use crate::kubernetes_api_objects::object_meta::*;
+use crate::kubernetes_api_objects::resource::*;
+use crate::vstd_ext::string_map::*;
+use deps_hack::chrono::{DateTime, Utc};
+use deps_hack::k8s_openapi::apimachinery::pkg::apis::meta::v1::Time;
+use vstd::prelude::*;
+use vstd::string::*;
+
+verus! {
+// Tests for error
+#[test]
+#[verifier(external)]
+pub fn test_apierror_fmt() {
+    let error = APIError::BadRequest;
+    assert_eq!(
+        format!("{:?}", error),
+        "BadRequest"
+    );
+    let error = APIError::Conflict;
+    assert_eq!(
+        format!("{:?}", error),
+        "Conflict"
+    );
+    let error = APIError::Forbidden;
+    assert_eq!(
+        format!("{:?}", error),
+        "Forbidden"
+    );
+    let error = APIError::Invalid;
+    assert_eq!(
+        format!("{:?}", error),
+        "Invalid"
+    );
+    let error = APIError::ObjectNotFound;
+    assert_eq!(
+        format!("{:?}", error),
+        "ObjectNotFound"
+    );
+    let error = APIError::ObjectAlreadyExists;
+    assert_eq!(
+        format!("{:?}", error),
+        "ObjectAlreadyExists"
+    );
+    let error = APIError::NotSupported;
+    assert_eq!(
+        format!("{:?}", error),
+        "NotSupported"
+    );
+    let error = APIError::InternalError;
+    assert_eq!(
+        format!("{:?}", error),
+        "InternalError"
+    );
+    let error = APIError::Timeout;
+    assert_eq!(
+        format!("{:?}", error),
+        "Timeout"
+    );
+    let error = APIError::ServerTimeout;
+    assert_eq!(
+        format!("{:?}", error),
+        "ServerTimeout"
+    );
+    let error = APIError::Other;
+    assert_eq!(
+        format!("{:?}", error),
+        "Other"
+    );
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_parse_dyn_error_fmt() {
+    let error = ParseDynamicObjectError::MissingField;
+    assert_eq!(
+        format!("{:?}", error),
+        "MissingField"
+    );
+    let error = ParseDynamicObjectError::UnexpectedType;
+    assert_eq!(
+        format!("{:?}", error),
+        "UnexpectedType"
+    );
+    let error = ParseDynamicObjectError::UnmarshalError;
+    assert_eq!(
+        format!("{:?}", error),
+        "UnmarshalError"
+    );
+    let error = ParseDynamicObjectError::ExecError;
+    assert_eq!(
+        format!("{:?}", error),
+        "ExecError"
+    );
+}
+}
