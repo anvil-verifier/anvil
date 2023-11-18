7e32250833c0075adaba7584b954ba1c16eb9abc
ZichengMa <zma17@illinois.edu>
2023-11-18 00:04:10 -0600

    update the newest version
    Signed-off-by: ZichengMa <zma17@illinois.edu>

--- tools/count-tests.py
+++ tools/count-tests.py
@@ -27,13 +27,21 @@ def count_functions(file_path):
     # Returning the counts
     return count_external_body, count_external_pub_fn, count_external_fn
 
-def count_test_functions(file_path):
+def count_test_functions(test_filename, test_directory_path='src/unit_tests/kubernetes_api_objects'):
     """Count test functions in the given file."""
-    with open(file_path, 'r') as file:
-        file_contents = file.read()
-
-    test_function_pattern = re.compile(r'fn test\w*\(')
-    return len(test_function_pattern.findall(file_contents))
+    test_file_path = os.path.join(test_directory_path, test_filename)
+    count = 0
+    test_function_pattern = re.compile(r"pub fn test_")
+    special_functions = {"test_marshal", "test_kube"}
+    if os.path.isfile(test_file_path):
+        with open(test_file_path, 'r') as file:
+            content = file.read()
+            matches = test_function_pattern.findall(content)
+            count = len(matches)
+            for special_function in special_functions:
+                if special_function in content:
+                    count += 1
+    return count
 
 def find_structs(file_path):
     """Find all struct names in the given file."""
@@ -55,7 +63,7 @@ def camel_to_snake(name):
 def main():
     api_directory_path = 'src/kubernetes_api_objects'  # Update this path if needed
     test_directory_path = 'src/unit_tests/kubernetes_api_objects'
-    for filename in sorted(os.listdir(api_directory_path)):
+    for filename in sorted(os.listdir(api_directory_path)): # loop through all files in api_directory_path
         if filename.endswith('.rs'):
             file_path = os.path.join(api_directory_path, filename)
             structs = find_structs(file_path)
@@ -63,17 +71,30 @@ def main():
                 structs = ['ApiMethod']
             if filename == 'error.rs':
                 structs = ['Error']
-            # print(f"\n{filename}: {structs}")
-            for struct_name in structs:
+            test_count = 0
+            struct_count = {}
+            for struct_name in structs: # count every struct's test functions
                 test_filename = f"{camel_to_snake(struct_name)}.rs"
                 test_file_path = os.path.join(test_directory_path, test_filename)
+                temp = count_test_functions(test_filename)
+                test_count += temp
+                struct_count[struct_name] = temp
             count_external_body, count_external_pub_fn, count_external_fn = count_functions(file_path)
             total_count = count_external_body + count_external_pub_fn + count_external_fn
-            if not os.path.exists(test_file_path) and total_count > 0:
-                # test_count = count_test_functions(test_file_path)
-                print(f"\nThis file can not be found:{test_filename}\n")
-
-            # print(f"{filename}: Total Count = {total_count}")
-
+            
+            if total_count != test_count:
+                print(f"{filename}: Total Count = {total_count}")
+                print("Test Count = ", test_count)
+                print("ERROR: Test Count != Total Count")
+                print(struct_count)
+                print("-------------------------------------------")
+            elif total_count == 0:
+                print(f"{filename}: Total Count = {total_count}")
+                print("-------------------------------------------")
+            else:
+                print(f"{filename}: Total Count = {total_count}")
+                print("Test Count = ", test_count)
+                print("Test coverage:", round(test_count / total_count * 100, 2), "%")
+                print("-------------------------------------------")
 if __name__ == "__main__":
     main()
