6cb8d2a24df1003f0af776bed89c2377907176a7
ZichengMa <zma17@illinois.edu>
2023-12-02 04:36:53 -0600

    update path for script and cover new functions
    Signed-off-by: ZichengMa <zma17@illinois.edu>

--- src/unit_tests/kubernetes_api_objects/container_port.rs
+++ src/unit_tests/kubernetes_api_objects/container_port.rs
@@ -36,6 +36,45 @@ pub fn test_set_name() {
 
 #[test]
 #[verifier(external)]
+pub fn test_name() {
+    let mut container_port = ContainerPort::default();
+    let temp = container_port.name();
+    if !temp.is_none() {
+        panic!("name should be none");
+    }
+    container_port.set_name(new_strlit("name").to_string());
+    assert_eq!("name".to_string(), container_port.name().unwrap().into_rust_string());
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_container_port() {
+    let mut container_port = ContainerPort::default();
+    container_port.set_container_port(8080);
+    assert_eq!(8080, container_port.container_port());
+}
+
+#[test]
+#[verifier(external)]
+pub fn test_protocol() {
+    let container_port = ContainerPort::default();
+    let temp = container_port.protocol();
+    if !temp.is_none() {
+        panic!("protocol should be none");
+    }
+    let container_port = ContainerPort::from_kube(deps_hack::k8s_openapi::api::core::v1::ContainerPort {
+        container_port: 8080,
+        host_ip: Some("host_ip".to_string()),
+        host_port: Some(8080),
+        name: Some("name".to_string()),
+        protocol: Some("protocol".to_string()),
+        ..Default::default()
+    });
+    assert_eq!("protocol".to_string(), container_port.protocol().unwrap().into_rust_string());
+}
+
+#[test]
+#[verifier(external)]
 pub fn test_kube() {
     let kube_container_port = deps_hack::k8s_openapi::api::core::v1::ContainerPort {
         container_port: 8080,
