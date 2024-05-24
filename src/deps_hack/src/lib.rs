290f285dbb382d9c5d60e6a99e5ca003f0afc83e
Cody Rivera <codyjr3@illinois.edu>
2024-05-24 15:05:50 -0500

    Start definition of VStatefulSet in Anvil

--- src/deps_hack/src/lib.rs
+++ src/deps_hack/src/lib.rs
@@ -267,3 +267,52 @@ pub struct FluentBitConfigSpec {
     #[serde(rename = "parsersConfig")]
     pub parsers_config: String,
 }
+
+#[derive(
+    kube::CustomResource, Debug, Clone, serde::Deserialize, serde::Serialize, schemars::JsonSchema,
+)]
+#[kube(group = "anvil.dev", version = "v1", kind = "VStatefulSet")]
+#[kube(shortname = "vsts", namespaced)]
+#[kube(status = "VStatefulSetStatus")]
+pub struct VStatefulSetSpec {
+    #[serde(rename = "serviceName")]
+    pub service_name: String,
+    // pub selector: k8s_openapi::apimachinery::pkg::apis::meta::v1::LabelSelector,
+    // pub template: k8s_openapi::api::core::v1::PodTemplateSpec,
+    pub replicas: i32,
+    // #[serde(rename = "updateStrategy")]
+    // pub update_strategy: Option<k8s_openapi::api::apps::v1::StatefulSetUpdateStrategy>,
+    #[serde(rename = "podManagementPolicy")]
+    pub pod_management_policy: String,
+    #[serde(rename = "revisionHistoryLimit")]
+    pub revision_history_limit: i32,
+    // #[serde(rename = "volumeClaimTemplates")]
+    // pub volume_claim_templates: Option<Vec<k8s_openapi::api::core::v1::PersistentVolumeClaim>>,
+    #[serde(rename = "minReadySeconds")]
+    pub min_ready_seconds: i32,
+    // #[serde(rename = "persistentVolumeClaimRetentionPolicy")]
+    // pub persistent_volume_claim_retention_policy: Option<k8s_openapi::api::apps::v1::StatefulSetPersistentVolumeClaimRetentionPolicy>,
+    // pub ordinals: Option<k8s_openapi::api::apps::v1::StatefulSetOrdinals>,
+}
+
+#[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize, schemars::JsonSchema)]
+pub struct VStatefulSetStatus {
+    pub replicas: i32,
+    #[serde(rename = "readyReplicas")]
+    pub ready_replicas: i32,
+    #[serde(rename = "currentReplicas")]
+    pub current_replicas: i32,
+    #[serde(rename = "updatedReplicas")]
+    pub updated_replicas: i32,
+    #[serde(rename = "availableReplicas")]
+    pub available_replicas: i32,
+    #[serde(rename = "collisionCount")]
+    pub collision_count: i32,
+    // pub conditions: Option<Vec<k8s_openapi::api::apps::v1::StatefulSetCondition>>,
+    #[serde(rename = "currentRevision")]
+    pub current_revision: String,
+    #[serde(rename = "updateRevision")]
+    pub update_revision: String,
+    #[serde(rename = "observedGeneration")]
+    pub observed_generation: i64,
+}
