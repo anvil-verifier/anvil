91db7d17e6012f59700c5d10ab6036fc14acbbc6
Xudong Sun <xudongs3@illinois.edu>
2024-08-05 10:41:08 -0500

    Move everything into proof.rs
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/soundness/compositional_liveness_proof/mod.rs
+++ src/soundness/compositional_liveness_proof/mod.rs
@@ -1,6 +1,4 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
-pub mod assumptions;
 pub mod proof;
-pub mod properties;
 pub mod state_machine;
--- src/soundness/compositional_liveness_proof/proof.rs
+++ src/soundness/compositional_liveness_proof/proof.rs
@@ -1,13 +1,15 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
 #![allow(unused_imports)]
-use crate::soundness::compositional_liveness_proof::{
-    assumptions::*, properties::*, state_machine::*,
-};
+use crate::soundness::compositional_liveness_proof::state_machine::*;
 use crate::temporal_logic::defs::*;
 use vstd::prelude::*;
 
 verus! {
+
+closed spec fn a_property<S>() -> TempPred<S>;
+
+closed spec fn b_property<S>() -> TempPred<S>;
 
 proof fn b_property_holds_globally<S, I>(spec: TempPred<S>)
     requires
@@ -108,5 +110,66 @@ proof fn next_does_nothing_beyond_next_with_b_only<S, I>(s: S, s_prime: S)
         }
     }
 }
+
+closed spec fn a_inv<S>() -> StatePred<S>;
+
+closed spec fn b_inv<S>() -> StatePred<S>;
+
+#[verifier(external_body)]
+proof fn b_inv_holds_locally<S, I>(spec: TempPred<S>)
+    requires
+        spec.entails(lift_state(init::<S>())),
+        spec.entails(always(lift_action(next_with_b_only::<S, I>()))),
+    ensures
+        spec.entails(always(lift_state(b_inv::<S>()))),
+{}
+
+#[verifier(external_body)]
+proof fn a_does_nothing_beyond_what_env_does<S, I>(input: I) -> (env_input: I)
+    ensures forall |s, s_prime: S| #[trigger] controller_a_next(input)(s, s_prime)
+            ==> environment_next(env_input)(s, s_prime)
+{
+    arbitrary()
+}
+
+#[verifier(external_body)]
+proof fn b_property_holds_locally<S, I>(spec: TempPred<S>)
+    requires
+        spec.entails(lift_state(init::<S>())),
+        spec.entails(always(lift_action(next_with_b_only::<S, I>()))),
+        spec.entails(fairness::<S>()),
+        spec.entails(always(lift_state(a_inv::<S>()))),
+    ensures
+        spec.entails(b_property::<S>()),
+{}
+
+#[verifier(external_body)]
+proof fn a_inv_holds_locally<S, I>(spec: TempPred<S>)
+    requires
+        spec.entails(lift_state(init::<S>())),
+        spec.entails(always(lift_action(next_with_a_only::<S, I>()))),
+    ensures
+        spec.entails(always(lift_state(a_inv::<S>()))),
+{}
+
+#[verifier(external_body)]
+proof fn b_does_nothing_beyond_what_env_does<S, I>(input: I) -> (env_input: I)
+    ensures forall |s, s_prime: S| #[trigger] controller_b_next(input)(s, s_prime)
+            ==> environment_next(env_input)(s, s_prime)
+{
+    arbitrary()
+}
+
+#[verifier(external_body)]
+proof fn a_property_holds_locally<S, I>(spec: TempPred<S>)
+    requires
+        spec.entails(lift_state(init::<S>())),
+        spec.entails(always(lift_action(next_with_a_only::<S, I>()))),
+        spec.entails(fairness::<S>()),
+        spec.entails(always(lift_state(b_inv::<S>()))),
+        spec.entails(b_property::<S>()),
+    ensures
+        spec.entails(a_property::<S>()),
+{}
 
 }
