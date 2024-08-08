15a7b3ed7145a68e3686a9728e083ae72967400f
Xudong Sun <xudongs3@illinois.edu>
2024-08-08 10:50:13 -0500

    Doc
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/soundness/compositionality/proof.rs
+++ src/soundness/compositionality/proof.rs
@@ -5,11 +5,6 @@ use crate::soundness::compositionality::state_machine::*;
 use crate::temporal_logic::defs::*;
 use vstd::prelude::*;
 
-// A is consumer and B is producer (A relies on B)
-// step 1: prove that B is correct (the property) in an env (shape shifter) that does not interfere with B (B + shapeshifter)
-// step 2: prove that A is correct assuming B is correct (A + B)
-// step 3: prove that A does not interfere with B (A + B)
-
 verus! {
 
 // The top level property of the consumer controller (e.g., ESR)
@@ -21,15 +16,22 @@ spec fn producer_property<S>() -> TempPred<S>;
 // The inv saying that no one interferes with the producer's reconcile
 spec fn no_one_interferes_producer<S>() -> StatePred<S>;
 
-// Behaviors of consumer are covered by behaviors of the shape shifter
-#[verifier(external_body)]
-proof fn consumer_does_nothing_beyond_what_shape_shifter_does<S, I>(input: I) -> (ss_input: I)
-    ensures forall |s, s_prime: S| #[trigger] ConsumerController::reconcile(input)(s, s_prime)
-            ==> ShapeShifter::reconcile(ss_input)(s, s_prime)
-{
-    arbitrary()
-}
+// Our goal is to prove that both producer and consumer are correct
+//     requires
+//         spec.entails(lift_state(ConsumerAndProducer::<S, I>::init())),
+//         spec.entails(always(lift_action(ConsumerAndProducer::<S, I>::next()))),
+//         spec.entails(producer_fairness::<S, I>()),
+//         spec.entails(consumer_fairness::<S, I>()),
+//     ensures
+//         spec.entails(producer_property::<S>()),
+//         spec.entails(consumer_property::<S>()),
+//
+// To do so, there are three proof obligations:
 
+// Proof obligation 1:
+// Producer is correct when running with the shape shifter assuming no interference.
+// In fact, this theorem is all you need if you only care about the producer, not the
+// consumer.
 #[verifier(external_body)]
 proof fn producer_property_holds_if_no_interference<S, I>(spec: TempPred<S>)
     requires
@@ -41,6 +43,8 @@ proof fn producer_property_holds_if_no_interference<S, I>(spec: TempPred<S>)
         spec.entails(producer_property::<S>()),
 {}
 
+// Proof obligation 2:
+// Consumer is correct when running with the producer assuming the producer is correct.
 #[verifier(external_body)]
 proof fn consumer_property_holds_if_producer_property_holds<S, I>(spec: TempPred<S>)
     requires
@@ -53,6 +57,8 @@ proof fn consumer_property_holds_if_producer_property_holds<S, I>(spec: TempPred
         spec.entails(consumer_property::<S>()),
 {}
 
+// Proof obligation 3:
+// Consumer does not interfere with the producer.
 #[verifier(external_body)]
 proof fn consumer_does_not_interfere_with_the_producer<S, I>(spec: TempPred<S>)
     requires
@@ -62,6 +68,7 @@ proof fn consumer_does_not_interfere_with_the_producer<S, I>(spec: TempPred<S>)
         spec.entails(always(lift_state(no_one_interferes_producer::<S>()))),
 {}
 
+// Now we can draw the final conclusion with the lemmas above.
 proof fn consumer_property_holds<S, I>(spec: TempPred<S>)
     requires
         spec.entails(lift_state(ConsumerAndProducer::<S, I>::init())),
@@ -69,6 +76,7 @@ proof fn consumer_property_holds<S, I>(spec: TempPred<S>)
         spec.entails(producer_fairness::<S, I>()),
         spec.entails(consumer_fairness::<S, I>()),
     ensures
+        spec.entails(producer_property::<S>()),
         spec.entails(consumer_property::<S>()),
 {
     assert forall |ex| #[trigger] spec.satisfied_by(ex)
--- src/soundness/compositionality/state_machine.rs
+++ src/soundness/compositionality/state_machine.rs
@@ -6,10 +6,43 @@ use vstd::prelude::*;
 
 verus! {
 
+// This abstract state machine example is used to illustrate how to do compositional
+// liveness proof for controllers. There are two controllers: producer and consumer.
+// To make the proof conclusion general, the producer and consumer remain abstract
+// in this example (spec functions without body).
+//
+// The producer realizes its desired state without relying on any other controller.
+// The consumer relies on the producer to realize its desired state. For example,
+// during the consumer's state reconciliation, it might create a desired state
+// description of the producer, wait for the producer to realize the producer's
+// desired state, and then continue to work based on the producer's desired state.
+//
+// One concrete example could be:
+// A consumer desired state description is created -> the consumer gets triggered
+// and then creates a producer desired state description -> the producer gets triggered
+// and then creates a pod (which realizes the producer's desired state) -> the consumer
+// waits until the pod is successfully created, and then takes the next step
+//
+// Note that this example differs from the classic producer-consumer model where the
+// consumer does not generate anything as the producer's input.
+//
+// Our end goal is to prove the correctness (ESR) of both the producer and consumer
+// controller in a compositional way. That is, to first prove the correctness of the
+// producer (without the consumer involved), and then prove the correctness of the
+// consumer using the previous correctness conclusion, while minimizing the effort
+// to reason about the interactions between the two controllers.
+//
+// The key of the proof is to reason about:
+// (1) how the consumer uses the producer to realize its own desired state, and
+// (2) how the consumer and producer do NOT interfere with each other.
+
+// A helper trait that allows us to plug any controller logic into the state machine
 pub trait Controller<S, I> {
     spec fn reconcile(input: I) -> ActionPred<S>;
 }
 
+// We define a struct for the abstract state machine. The dummy variables are not
+// used anywhere but just to shut up checks on unused generic types.
 pub struct Cluster<S, I, TargetController, AnotherController>
 where TargetController: Controller<S, I>, AnotherController: Controller<S, I>
 {
@@ -19,17 +52,7 @@ where TargetController: Controller<S, I>, AnotherController: Controller<S, I>
     dummy_ac: std::marker::PhantomData<AnotherController>,
 }
 
-pub enum Step<I> {
-    OtherComponentsStep(I),
-    TargetControllerStep(I),
-    AnotherControllerStep(I),
-    StutterStep(),
-}
-
-pub spec fn init<S>() -> StatePred<S>;
-
-pub spec fn other_components_next<S, I>(input: I) -> ActionPred<S>;
-
+// The methods below define the initial state and next transitions of the state machine.
 impl<S, I, TargetController, AnotherController> Cluster<S, I, TargetController, AnotherController>
 where TargetController: Controller<S, I>, AnotherController: Controller<S, I>
 {
@@ -67,28 +90,59 @@ where TargetController: Controller<S, I>, AnotherController: Controller<S, I>
     }
 }
 
+pub spec fn init<S>() -> StatePred<S>;
+
+pub spec fn other_components_next<S, I>(input: I) -> ActionPred<S>;
+
+pub enum Step<I> {
+    OtherComponentsStep(I),
+    TargetControllerStep(I),
+    AnotherControllerStep(I),
+    StutterStep(),
+}
+
+// The controller logic of the producer controller.
 pub struct ProducerController {}
 
 impl<S, I> Controller<S, I> for ProducerController {
     spec fn reconcile(input: I) -> ActionPred<S>;
 }
 
+// The controller logic of the consumer controller.
 pub struct ConsumerController {}
 
 impl<S, I> Controller<S, I> for ConsumerController {
     spec fn reconcile(input: I) -> ActionPred<S>;
 }
 
+// The controller logic of the shape shifter. The shape shifter is not any
+// concrete controller. It's used to represent *any* possible controller
+// that will interact with the target controller.
+// If we write down the body of the reconcile, it will be:
+// randomly choose a state object to create, update or delete.
 pub struct ShapeShifter {}
 
 impl<S, I> Controller<S, I> for ShapeShifter {
     spec fn reconcile(input: I) -> ActionPred<S>;
 }
 
+// Here is an example: the shape shifter can simulate the behavior of the
+// consumer. For any possible transition taken by the consumer, the shape
+// shifter can take the same transition.
+#[verifier(external_body)]
+pub proof fn consumer_does_nothing_beyond_what_shape_shifter_does<S, I>(input: I) -> (ss_input: I)
+    ensures forall |s, s_prime: S| #[trigger] ConsumerController::reconcile(input)(s, s_prime)
+            ==> ShapeShifter::reconcile(ss_input)(s, s_prime)
+{
+    arbitrary()
+}
+
+// Fairness conditions used for proving liveness
 pub spec fn producer_fairness<S, I>() -> TempPred<S>;
 
 pub spec fn consumer_fairness<S, I>() -> TempPred<S>;
 
+// We "instantiate" the Cluster by plugging different controller logics into different positions.
 pub type ConsumerAndProducer<S, I> = Cluster<S, I, ConsumerController, ProducerController>;
 
 pub type ConsumerAndShapeShifter<S, I> = Cluster<S, I, ConsumerController, ShapeShifter>;
