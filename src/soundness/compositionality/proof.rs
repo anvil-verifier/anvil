e98deaaebd5a8ecccfc04541ec833d7babc0542f
Xudong Sun <xudongs3@illinois.edu>
2024-08-12 14:00:25 -0500

    Extend the abstract state machine as an array of controllers (#510)
    This PR makes two major changes to the abstract state machine used for
    demonstrating compositional proof:
    (1) the abstract state machine is no longer hardcoded to two
    controllers, but takes a sequence of controllers
    (2) the first proof obligation no longer constrains *what the cluster
    is*. Instead, its precondition constrains *what the cluster does* (the
    producer runs in the cluster and there is no interference).
    
    ---------
    
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/soundness/compositionality/proof.rs
+++ src/soundness/compositionality/proof.rs
@@ -11,36 +11,60 @@ verus! {
 spec fn consumer_property<S>() -> TempPred<S>;
 
 // The top level property of the producer controller (e.g., ESR)
-spec fn producer_property<S>() -> TempPred<S>;
+spec fn producer_property<S>(p_index: int) -> TempPred<S>;
 
 // The inv saying that no one interferes with the producer's reconcile
-spec fn no_one_interferes_producer<S, I>(any: Controller<S, I>) -> StatePred<S>;
+spec fn no_one_interferes_producer<S, I>(cluster: Cluster<S, I>, p_index: int) -> StatePred<S>;
 
-// Our goal is to prove that both producer and consumer are correct
-//    requires
-//        spec.entails(lift_state(consumer_and_producer::<S, I>().init())),
-//        spec.entails(always(lift_action(consumer_and_producer::<S, I>().next()))),
-//        spec.entails(producer_fairness::<S, I>()),
-//        spec.entails(consumer_fairness::<S, I>()),
-//    ensures
-//        spec.entails(producer_property::<S>()),
-//        spec.entails(consumer_property::<S>()),
-//
-// To do so, there are three proof obligations:
+// This is our end-goal theorem: in a cluster with all the producers and the consumer,
+// all controllers are correct.
+proof fn consumer_property_holds<S, I>(spec: TempPred<S>)
+    requires
+        spec.entails(lift_state(consumer_and_producers::<S, I>().init())),
+        spec.entails(always(lift_action(consumer_and_producers::<S, I>().next()))),
+        forall |p_index: int| 0 <= p_index < producers::<S, I>().len() ==> #[trigger] spec.entails(producer_fairness::<S, I>(p_index)),
+        spec.entails(consumer_fairness::<S, I>()),
+    ensures
+        forall |p_index: int| 0 <= p_index < producers::<S, I>().len() ==> #[trigger] spec.entails(producer_property::<S>(p_index)),
+        spec.entails(consumer_property::<S>()),
+{
+    let cluster = consumer_and_producers::<S, I>();
+
+    assert forall |p_index| 0 <= p_index < producers::<S, I>().len() implies #[trigger] spec.entails(producer_property::<S>(p_index)) by {
+        assert forall |s| cluster.init()(s) implies #[trigger] producers::<S, I>()[p_index].init()(s) by {
+            assert forall |i| 0 <= i < cluster.controllers.len() implies #[trigger] cluster.controllers[i].init()(s) by {}
+            assert(producers::<S, I>()[p_index] =~= cluster.controllers[p_index]);
+        }
+
+        assert forall |input, s, s_prime| #[trigger] producers::<S, I>()[p_index].next(input)(s, s_prime) implies cluster.next()(s, s_prime) by {
+            let step = Step::ControllerStep(p_index, input);
+            assert(cluster.next_step(s, s_prime, step));
+        }
+
+        consumer_does_not_interfere_with_the_producer::<S, I>(spec, p_index);
+        producer_property_holds_if_no_interference::<S, I>(spec, cluster, p_index);
+    }
+
+    consumer_property_holds_if_producer_property_holds::<S, I>(spec);
+}
+
+// To prove the above theorem, there are three proof obligations.
 
 // Proof obligation 1:
-// Producer is correct when running with the shape shifter assuming no interference.
-// In fact, this theorem is all you need if you only care about the producer, not the
-// consumer.
+// Producer is correct when running in any cluster where there is no interference.
+// This theorem is all you need if you only care about the producer, not the consumer.
 #[verifier(external_body)]
-proof fn producer_property_holds_if_no_interference<S, I>(spec: TempPred<S>, any: Controller<S, I>)
+proof fn producer_property_holds_if_no_interference<S, I>(spec: TempPred<S>, cluster: Cluster<S, I>, p_index: int)
     requires
-        spec.entails(lift_state(producer_and_any::<S, I>(any).init())),
-        spec.entails(always(lift_action(producer_and_any::<S, I>(any).next()))),
-        spec.entails(producer_fairness::<S, I>()),
-        spec.entails(always(lift_state(no_one_interferes_producer::<S, I>(any)))),
+        0 <= p_index < producers::<S, I>().len(),
+        spec.entails(lift_state(cluster.init())),
+        forall |s| cluster.init()(s) ==> #[trigger] producers::<S, I>()[p_index].init()(s),
+        spec.entails(always(lift_action(cluster.next()))),
+        forall |input, s, s_prime| #[trigger] producers::<S, I>()[p_index].next(input)(s, s_prime) ==> cluster.next()(s, s_prime),
+        spec.entails(producer_fairness::<S, I>(p_index)),
+        spec.entails(always(lift_state(no_one_interferes_producer::<S, I>(cluster, p_index)))),
     ensures
-        spec.entails(producer_property::<S>()),
+        spec.entails(producer_property::<S>(p_index)),
 {}
 
 // Proof obligation 2:
@@ -48,11 +72,11 @@ proof fn producer_property_holds_if_no_interference<S, I>(spec: TempPred<S>, any
 #[verifier(external_body)]
 proof fn consumer_property_holds_if_producer_property_holds<S, I>(spec: TempPred<S>)
     requires
-        spec.entails(lift_state(consumer_and_producer::<S, I>().init())),
-        spec.entails(always(lift_action(consumer_and_producer::<S, I>().next()))),
-        spec.entails(producer_fairness::<S, I>()),
+        spec.entails(lift_state(consumer_and_producers::<S, I>().init())),
+        spec.entails(always(lift_action(consumer_and_producers::<S, I>().next()))),
+        forall |p_index: int| 0 <= p_index < producers::<S, I>().len() ==> #[trigger] spec.entails(producer_fairness::<S, I>(p_index)),
         spec.entails(consumer_fairness::<S, I>()),
-        spec.entails(producer_property::<S>()),
+        forall |p_index: int| 0 <= p_index < producers::<S, I>().len() ==> #[trigger] spec.entails(producer_property::<S>(p_index)),
     ensures
         spec.entails(consumer_property::<S>()),
 {}
@@ -60,63 +84,13 @@ proof fn consumer_property_holds_if_producer_property_holds<S, I>(spec: TempPred
 // Proof obligation 3:
 // Consumer does not interfere with the producer.
 #[verifier(external_body)]
-proof fn consumer_does_not_interfere_with_the_producer<S, I>(spec: TempPred<S>)
+proof fn consumer_does_not_interfere_with_the_producer<S, I>(spec: TempPred<S>, p_index: int)
     requires
-        spec.entails(lift_state(consumer_and_producer::<S, I>().init())),
-        spec.entails(always(lift_action(consumer_and_producer::<S, I>().next()))),
+        0 <= p_index < producers::<S, I>().len(),
+        spec.entails(lift_state(consumer_and_producers::<S, I>().init())),
+        spec.entails(always(lift_action(consumer_and_producers::<S, I>().next()))),
     ensures
-        spec.entails(always(lift_state(no_one_interferes_producer::<S, I>(consumer())))),
+        spec.entails(always(lift_state(no_one_interferes_producer::<S, I>(consumer_and_producers::<S, I>(), p_index)))),
 {}
-
-// Now we can draw the final conclusion with the lemmas above.
-proof fn consumer_property_holds<S, I>(spec: TempPred<S>)
-    requires
-        spec.entails(lift_state(consumer_and_producer::<S, I>().init())),
-        spec.entails(always(lift_action(consumer_and_producer::<S, I>().next()))),
-        spec.entails(producer_fairness::<S, I>()),
-        spec.entails(consumer_fairness::<S, I>()),
-    ensures
-        spec.entails(producer_property::<S>()),
-        spec.entails(consumer_property::<S>()),
-{
-    assert forall |ex| #[trigger] spec.satisfied_by(ex)
-    implies lift_state(producer_and_any::<S, I>(consumer()).init()).satisfied_by(ex) by {
-        assert(spec.implies(lift_state(consumer_and_producer::<S, I>().init())).satisfied_by(ex));
-    }
-
-    assert forall |ex| #[trigger] spec.satisfied_by(ex)
-    implies always(lift_action(producer_and_any::<S, I>(consumer()).next())).satisfied_by(ex) by {
-        assert(spec.implies(always(lift_action(consumer_and_producer::<S, I>().next()))).satisfied_by(ex));
-        assert forall |i| #[trigger] lift_action(producer_and_any::<S, I>(consumer()).next()).satisfied_by(ex.suffix(i)) by {
-            assert(lift_action(consumer_and_producer::<S, I>().next()).satisfied_by(ex.suffix(i)));
-            state_machine_simulation::<S, I>(ex.suffix(i).head(), ex.suffix(i).head_next());
-        }
-    }
-
-    consumer_does_not_interfere_with_the_producer::<S, I>(spec);
-    producer_property_holds_if_no_interference::<S, I>(spec, consumer());
-    consumer_property_holds_if_producer_property_holds::<S, I>(spec);
-}
-
-proof fn state_machine_simulation<S, I>(s: S, s_prime: S)
-    requires
-        consumer_and_producer::<S, I>().next()(s, s_prime),
-    ensures
-        producer_and_any::<S, I>(consumer()).next()(s, s_prime),
-{
-    let step = choose |step: Step<I>| consumer_and_producer::<S, I>().next_step(s, s_prime, step);
-    assert(consumer_and_producer::<S, I>().next_step(s, s_prime, step));
-    match step {
-        Step::TargetControllerStep(input) => {
-            assert(producer_and_any::<S, I>(consumer()).next_step(s, s_prime, Step::AnotherControllerStep(input)));
-        }
-        Step::AnotherControllerStep(input) => {
-            assert(producer_and_any::<S, I>(consumer()).next_step(s, s_prime, Step::TargetControllerStep(input)));
-        }
-        _ => {
-            assert(producer_and_any::<S, I>(consumer()).next_step(s, s_prime, step));
-        }
-    }
-}
 
 }
--- src/soundness/compositionality/state_machine.rs
+++ src/soundness/compositionality/state_machine.rs
@@ -10,15 +10,9 @@ verus! {
 // other controllers to be correct?
 //
 // This abstract state machine example is used to illustrate how to do compositional
-// liveness proof for controllers. There are two controllers: producer and consumer.
-// To make the proof conclusion general, the producer and consumer remain abstract
-// in this example (spec functions without body).
-//
-// The producer realizes its desired state without relying on any other controller.
-// The consumer relies on the producer to realize its desired state. For example,
-// during the consumer's state reconciliation, it might create a desired state
-// description of the producer, wait for the producer to realize the producer's
-// desired state, and then continue to work based on the producer's desired state.
+// liveness proof for controllers. There are two types controllers: (1) a consumer
+// and (2) a sequence of producers that the consumer depends on. To make the proof
+// conclusion general, the producer and consumer remain abstract in this example.
 //
 // One concrete example could be:
 // A consumer desired state description is created -> the consumer gets triggered
@@ -29,47 +23,56 @@ verus! {
 // Note that this example differs from the classic producer-consumer model where the
 // consumer does not generate anything as the producer's input.
 //
-// Our end goal is to prove the correctness (ESR) of both the producer and consumer
-// controller in a compositional way. That is, to first prove the correctness of the
+// Our end goal is to prove the correctness (ESR) of all the producer and consumer
+// controllers in a compositional way. That is, to first prove the correctness of each
 // producer (without the consumer involved), and then prove the correctness of the
-// consumer using the previous correctness conclusion, while minimizing the effort
-// to reason about the interactions between the two controllers.
+// consumer using the previous correctness conclusions, while minimizing the effort
+// to reason about the interactions between different controllers.
 //
 // The key of the proof is to reason about:
-// (1) how the consumer uses the producer to realize its own desired state, and
-// (2) how the consumer and producer do NOT interfere with each other.
+// (1) how the consumer uses the producers to realize its own desired state, and
+// (2) how the consumer and producers do NOT interfere with each other.
 
 #[verifier::reject_recursive_types(S)]
 #[verifier::reject_recursive_types(I)]
 pub struct Controller<S, I> {
-    pub init: StatePred<S>,
-    pub next: spec_fn(I) -> ActionPred<S>,
+    pub init_pred: StatePred<S>,
+    pub next_pred: spec_fn(I) -> ActionPred<S>,
+}
+
+impl<S, I> Controller<S, I> {
+    pub open spec fn init(self) -> StatePred<S> {
+        self.init_pred
+    }
+
+    pub open spec fn next(self, input: I) -> ActionPred<S> {
+        |s, s_prime| {
+            &&& (self.next_pred)(input)(s, s_prime)
+            &&& network_next(input)(s, s_prime)
+        }
+    }
 }
 
 #[verifier::reject_recursive_types(S)]
 #[verifier::reject_recursive_types(I)]
 pub struct Cluster<S, I> {
-    pub target_controller: Controller<S, I>,
-    pub another_controller: Controller<S, I>,
+    pub controllers: Seq<Controller<S, I>>,
 }
 
 // The methods below define the initial state and next transitions of the state machine.
 impl<S, I> Cluster<S, I> {
     pub open spec fn init(self) -> StatePred<S> {
         |s| {
-            &&& (self.target_controller.init)(s)
-            &&& (self.another_controller.init)(s)
+            &&& forall |i| 0 <= i < self.controllers.len() ==> #[trigger] self.controllers[i].init()(s)
             &&& other_init()(s)
         }
     }
 
-    pub open spec fn target_controller_next(self, input: I) -> ActionPred<S> {
-        (self.target_controller.next)(input)
-    }
-
-    // In a more complex example, there could be an array of "another controller"s.
-    pub open spec fn another_controller_next(self, input: I) -> ActionPred<S> {
-        (self.another_controller.next)(input)
+    pub open spec fn controller_next(self, index: int, input: I) -> ActionPred<S> {
+        |s, s_prime| {
+            &&& 0 <= index < self.controllers.len()
+            &&& self.controllers[index].next(input)(s, s_prime)
+        }
     }
 
     pub open spec fn other_components_next(self, input: I) -> ActionPred<S> {
@@ -86,8 +89,7 @@ impl<S, I> Cluster<S, I> {
 
     pub open spec fn next_step(self, s: S, s_prime: S, step: Step<I>) -> bool {
         match step {
-            Step::TargetControllerStep(input) => self.target_controller_next(input)(s, s_prime),
-            Step::AnotherControllerStep(input) => self.another_controller_next(input)(s, s_prime),
+            Step::ControllerStep(index, input) => self.controller_next(index, input)(s, s_prime),
             Step::OtherComponentsStep(input) => self.other_components_next(input)(s, s_prime),
             Step::StutterStep() => self.stutter()(s, s_prime),
         }
@@ -96,34 +98,31 @@ impl<S, I> Cluster<S, I> {
 
 pub spec fn other_init<S>() -> StatePred<S>;
 
+pub spec fn network_next<S, I>(input: I) -> ActionPred<S>;
+
 pub spec fn other_components_next<S, I>(input: I) -> ActionPred<S>;
 
 pub enum Step<I> {
     OtherComponentsStep(I),
-    TargetControllerStep(I),
-    AnotherControllerStep(I),
+    ControllerStep(int, I),
     StutterStep(),
 }
 
-pub spec fn producer<S, I>() -> Controller<S, I>;
+pub spec fn producers<S, I>() -> Seq<Controller<S, I>>;
 
 pub spec fn consumer<S, I>() -> Controller<S, I>;
 
-pub spec fn producer_fairness<S, I>() -> TempPred<S>;
-
-pub spec fn consumer_fairness<S, I>() -> TempPred<S>;
-
-pub open spec fn consumer_and_producer<S, I>() -> Cluster<S, I> {
-    Cluster {
-        target_controller: consumer::<S, I>(),
-        another_controller: producer::<S, I>(),
-    }
+pub open spec fn producer_fairness<S, I>(p_index: int) -> TempPred<S> {
+    tla_forall(|input: I| weak_fairness(producers()[p_index].next(input)))
 }
 
-pub open spec fn producer_and_any<S, I>(any: Controller<S, I>) -> Cluster<S, I> {
+pub open spec fn consumer_fairness<S, I>() -> TempPred<S> {
+    tla_forall(|input: I| weak_fairness(consumer().next(input)))
+}
+
+pub open spec fn consumer_and_producers<S, I>() -> Cluster<S, I> {
     Cluster {
-        target_controller: producer::<S, I>(),
-        another_controller: any,
+        controllers: producers::<S, I>().push(consumer::<S, I>()),
     }
 }
 
