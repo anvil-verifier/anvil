6eab6b5246b66d366b9b5769bfe30033c385049a
Xudong Sun <xudongs3@illinois.edu>
2024-08-16 22:23:30 -0500

    Group the spec functions for each controller (#514)
    This PR introduces `ControllerSpecGroup` which includes the controller,
    its key in the `cluster.controllers` map, its temporal property, and its
    non-interference invariant. This struct helps to simplify the
    compositional theorems, as we no longer need to carry the
    `consumer_id`/`producer_ids` along the way.
    
    ---------
    
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/soundness/compositionality/examples.rs
+++ src/soundness/compositionality/examples.rs
@@ -10,70 +10,108 @@ verus! {
 
 // This is a use case of consumer_and_producers_are_correct:
 // We can apply consumer_and_producers_are_correct to a concrete cluster that includes
-// only a consumer and a sequence of producers.
-pub spec fn producers<S, I>() -> Seq<Controller<S, I>>;
+// only a waiter and a sequence of cooks.
+pub spec fn cooks<S, I>() -> Seq<Controller<S, I>>;
 
-pub spec fn consumer<S, I>() -> Controller<S, I>;
+pub spec fn waiter<S, I>() -> Controller<S, I>;
 
-// A concrete cluster with only producers and consumer
-pub open spec fn consumer_and_producers<S, I>() -> Cluster<S, I> {
-    Cluster {
-        controllers: Map::new(|k| 0 <= k < producers::<S, I>().len(), |k| producers::<S, I>()[k])
-            .insert(producers::<S, I>().len() as int, consumer::<S, I>())
+pub open spec fn cook_spec_groups<S, I>() -> Seq<ControllerSpecGroup<S, I>> {
+    cooks().map(|i, v| ControllerSpecGroup {
+        controller: v,
+        key: i,
+        property: arbitrary(),
+        not_interfered_by: arbitrary(),
+    })
+}
+
+pub open spec fn waiter_spec_group<S, I>() -> ControllerSpecGroup<S, I> {
+    ControllerSpecGroup::<S, I> {
+        controller: waiter(),
+        key: cook_spec_groups::<S, I>().len() as int,
+        property: arbitrary(),
+        not_interfered_by: arbitrary(),
     }
 }
 
-proof fn consumer_and_producers_are_correct_in_a_concrete_cluster<S, I>(spec: TempPred<S>)
+// A concrete cluster with only cooks and waiter_spec_group
+pub open spec fn waiter_and_cooks<S, I>() -> Cluster<S, I> {
+    Cluster {
+        controllers: Map::new(|k| 0 <= k < cooks::<S, I>().len(), |k| cooks::<S, I>()[k])
+            .insert(cooks::<S, I>().len() as int, waiter::<S, I>())
+    }
+}
+
+proof fn waiter_and_cooks_are_correct<S, I>(spec: TempPred<S>)
     requires
-        spec.entails(lift_state(consumer_and_producers::<S, I>().init())),
-        spec.entails(always(lift_action(consumer_and_producers::<S, I>().next()))),
-        forall |p_index: int| 0 <= p_index < producers::<S, I>().len()
-            ==> #[trigger] spec.entails(controller_fairness::<S, I>(producers()[p_index])),
-        spec.entails(controller_fairness::<S, I>(consumer())),
+        spec.entails(lift_state(waiter_and_cooks::<S, I>().init())),
+        spec.entails(always(lift_action(waiter_and_cooks::<S, I>().next()))),
+        forall |p_index: int| 0 <= p_index < cook_spec_groups::<S, I>().len()
+            ==> #[trigger] spec.entails(controller_fairness::<S, I>(cook_spec_groups()[p_index].controller)),
+        spec.entails(controller_fairness::<S, I>(waiter_spec_group().controller)),
     ensures
-        forall |p_index: int| 0 <= p_index < producers::<S, I>().len()
-            ==> #[trigger] spec.entails(property::<S, I>(producers()[p_index])),
-        spec.entails(property::<S, I>(consumer())),
+        forall |p_index: int| 0 <= p_index < cook_spec_groups::<S, I>().len()
+            ==> #[trigger] spec.entails(cook_spec_groups::<S, I>()[p_index].property),
+        spec.entails(waiter_spec_group::<S, I>().property),
 {
-    let cluster = consumer_and_producers::<S, I>();
+    let cluster = waiter_and_cooks::<S, I>();
 
-    let producer_ids = Map::new(|k: int| 0 <= k < producers::<S, I>().len(), |k: int| k);
-    let consumer_id = producers::<S, I>().len() as int;
-
-    assert forall |s| #[trigger] cluster.init()(s) implies consumer::<S, I>().init()(s) by {
+    assert forall |s| #[trigger] cluster.init()(s) implies waiter_spec_group::<S, I>().controller.init()(s) by {
         assert forall |i| #[trigger] cluster.controllers.contains_key(i) implies cluster.controllers[i].init()(s) by {}
-        assert(cluster.controllers.contains_key(consumer_id));
+        assert(cluster.controllers.contains_key(waiter_spec_group::<S, I>().key));
     }
 
-    assert forall |p_index| #![trigger producers::<S, I>()[p_index]] 0 <= p_index < producers::<S, I>().len()
-    implies forall |s| #[trigger] cluster.init()(s) ==> producers::<S, I>()[p_index].init()(s) by {
-        assert forall |s| #[trigger] cluster.init()(s) implies producers::<S, I>()[p_index].init()(s) by {
+    assert forall |p_index| #![trigger cook_spec_groups::<S, I>()[p_index]] 0 <= p_index < cook_spec_groups::<S, I>().len()
+    implies forall |s| #[trigger] cluster.init()(s) ==> cook_spec_groups::<S, I>()[p_index].controller.init()(s) by {
+        assert forall |s| #[trigger] cluster.init()(s) implies cook_spec_groups::<S, I>()[p_index].controller.init()(s) by {
             assert forall |i| #[trigger] cluster.controllers.contains_key(i) implies cluster.controllers[i].init()(s) by {}
             assert(cluster.controllers.contains_key(p_index));
         }
     }
 
-    // Due to our cluster construct, you won't find a good_citizen_id that is neither the consumer nor any producer.
+    let cook_keys = cook_spec_groups().map_values(|p: ControllerSpecGroup<S, I>| p.key);
+    let cook_key_set = cook_keys.to_set();
+
+    // Due to our cluster construct, you won't find a good_citizen_id that is neither the waiter_spec_group nor any cook.
     // So we prove the following assertion by contradiction.
     assert forall |good_citizen_id: int|
-        #[trigger] cluster.controllers.remove(consumer_id).remove_keys(producer_ids.values()).contains_key(good_citizen_id)
+        cluster.controllers.remove(waiter_spec_group::<S, I>().key).remove_keys(cook_key_set).contains_key(good_citizen_id)
     implies
-        spec.entails(always(lift_state(one_does_not_interfere_with_this_controller::<S, I>(cluster, good_citizen_id, consumer()))))
-        && forall |p_index: int| 0 <= p_index < producers::<S, I>().len()
-            ==> spec.entails(always(lift_state(#[trigger] one_does_not_interfere_with_this_controller::<S, I>(cluster, good_citizen_id, producers()[p_index]))))
+        #[trigger] spec.entails(always(lift_state((waiter_spec_group::<S, I>().not_interfered_by)(good_citizen_id))))
     by {
         assert forall |controller_id| #[trigger] cluster.controllers.contains_key(controller_id)
-        implies controller_id == consumer_id || producer_ids.values().contains(controller_id) by {
-            if controller_id == producers::<S, I>().len() as int {
-                assert(controller_id == consumer_id);
+        implies controller_id == waiter_spec_group::<S, I>().key || cook_key_set.contains(controller_id) by {
+            if controller_id == cook_spec_groups::<S, I>().len() as int {
+                assert(controller_id == waiter_spec_group::<S, I>().key);
             } else {
-                assert(producer_ids.dom().contains(controller_id) && producer_ids[controller_id] == controller_id);
+                assert(0 <= controller_id < cook_keys.len() && cook_keys[controller_id] == controller_id);
             }
         }
-        assert(!cluster.controllers.remove(consumer_id).remove_keys(producer_ids.values()).contains_key(good_citizen_id));
+        assert(!cluster.controllers.remove(waiter_spec_group::<S, I>().key).remove_keys(cook_key_set).contains_key(good_citizen_id));
     }
 
-    consumer_and_producers_are_correct(spec, cluster, consumer(), consumer_id, producers(), producer_ids);
+    assert forall |p_index| #![trigger cook_spec_groups::<S, I>()[p_index]] 0 <= p_index < cook_spec_groups::<S, I>().len()
+    implies
+        (forall |good_citizen_id| cluster.controllers.remove(waiter_spec_group::<S, I>().key).remove_keys(cook_key_set).contains_key(good_citizen_id)
+            ==> spec.entails(always(lift_state(#[trigger] (cook_spec_groups::<S, I>()[p_index].not_interfered_by)(good_citizen_id)))))
+    by {
+        assert forall |good_citizen_id|
+            cluster.controllers.remove(waiter_spec_group::<S, I>().key).remove_keys(cook_key_set).contains_key(good_citizen_id)
+        implies
+            #[trigger] spec.entails(always(lift_state(#[trigger] (cook_spec_groups::<S, I>()[p_index].not_interfered_by)(good_citizen_id))))
+        by {
+            assert forall |controller_id| #[trigger] cluster.controllers.contains_key(controller_id)
+            implies controller_id == waiter_spec_group::<S, I>().key || cook_key_set.contains(controller_id) by {
+                if controller_id == cook_spec_groups::<S, I>().len() as int {
+                    assert(controller_id == waiter_spec_group::<S, I>().key);
+                } else {
+                    assert(0 <= controller_id < cook_keys.len() && cook_keys[controller_id] == controller_id);
+                }
+            }
+            assert(!cluster.controllers.remove(waiter_spec_group::<S, I>().key).remove_keys(cook_key_set).contains_key(good_citizen_id));
+        }
+    }
+
+    consumer_and_producers_are_correct(spec, cluster, waiter_spec_group(), cook_spec_groups());
 }
 
 }
