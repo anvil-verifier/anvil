e2f2103b050f39017fea1fdfa7a89aaf81d69673
Xudong Sun <xudongs3@illinois.edu>
2024-09-15 11:24:08 -0500

    Merge wf1 lemmas into wf1_helpers
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/v2/kubernetes_cluster/proof/mod.rs
+++ src/v2/kubernetes_cluster/proof/mod.rs
@@ -1,23 +1,7 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: MIT
-// pub mod api_server_liveness;
-// pub mod api_server_safety;
-// pub mod builtin_controllers;
-// pub mod cluster;
-// pub mod cluster_safety;
-// pub mod controller_runtime;
-// pub mod controller_runtime_eventual_safety;
-// pub mod controller_runtime_liveness;
-// pub mod controller_runtime_safety;
-// pub mod daemon_set_controller;
-// pub mod external_api_liveness;
-// pub mod message;
-// pub mod stateful_set_controller;
-// pub mod validation_rule;
-pub mod api_server_liveness;
 pub mod cluster;
 pub mod compositionality;
-pub mod external_liveness;
 pub mod failures_liveness;
 pub mod network;
 pub mod network_liveness;
--- src/v2/kubernetes_cluster/proof/wf1_helpers.rs
+++ src/v2/kubernetes_cluster/proof/wf1_helpers.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 #![allow(unused_imports)]
 use crate::kubernetes_api_objects::spec::prelude::*;
+use crate::state_machine::{action::*, state_machine::*};
+use crate::temporal_logic::{defs::*, rules::*};
 use crate::v2::kubernetes_cluster::spec::{
     api_server::state_machine::api_server,
     api_server::types::*,
@@ -16,13 +18,55 @@ use crate::v2::kubernetes_cluster::spec::{
     network::state_machine::network,
     network::types::*,
 };
-use crate::state_machine::{action::*, state_machine::*};
-use crate::temporal_logic::defs::*;
 use vstd::prelude::*;
 
 verus! {
 
 impl Cluster {
+
+pub proof fn lemma_pre_leads_to_post_by_api_server(
+    self, spec: TempPred<ClusterState>, input: Option<Message>, next: ActionPred<ClusterState>,
+    step: APIServerStep, pre: StatePred<ClusterState>, post: StatePred<ClusterState>
+)
+    requires
+        forall |s, s_prime| pre(s) && #[trigger] next(s, s_prime) ==> pre(s_prime) || post(s_prime),
+        forall |s, s_prime| pre(s) && #[trigger] next(s, s_prime) && self.api_server_next().forward(input)(s, s_prime) ==> post(s_prime),
+        forall |s| #[trigger] pre(s) ==> self.api_server_action_pre(step, input)(s),
+        spec.entails(always(lift_action(next))),
+        spec.entails(tla_forall(|i| self.api_server_next().weak_fairness(i))),
+    ensures spec.entails(lift_state(pre).leads_to(lift_state(post))),
+{
+    use_tla_forall::<ClusterState, Option<Message>>(spec, |i| self.api_server_next().weak_fairness(i), input);
+    self.api_server_action_pre_implies_next_pre(step, input);
+    valid_implies_trans::<ClusterState>(
+        lift_state(pre),
+        lift_state(self.api_server_action_pre(step, input)),
+        lift_state(self.api_server_next().pre(input))
+    );
+    self.api_server_next().wf1(input, spec, next, pre, post);
+}
+
+pub proof fn lemma_pre_leads_to_post_by_external(
+    self, spec: TempPred<ClusterState>, controller_id: int, input: Option<Message>, next: ActionPred<ClusterState>,
+    step: ExternalStep, pre: StatePred<ClusterState>, post: StatePred<ClusterState>
+)
+    requires
+        forall |s, s_prime| pre(s) && #[trigger] next(s, s_prime) ==> pre(s_prime) || post(s_prime),
+        forall |s, s_prime| pre(s) && #[trigger] next(s, s_prime) && self.external_next().forward((controller_id, input))(s, s_prime) ==> post(s_prime),
+        forall |s| #[trigger] pre(s) ==> self.external_action_pre(step, (controller_id, input))(s),
+        spec.entails(always(lift_action(next))),
+        spec.entails(tla_forall(|i| self.external_next().weak_fairness((controller_id, i)))),
+    ensures spec.entails(lift_state(pre).leads_to(lift_state(post))),
+{
+    use_tla_forall::<ClusterState, Option<Message>>(spec, |i| self.external_next().weak_fairness((controller_id, i)), input);
+    self.external_action_pre_implies_next_pre(step, (controller_id, input));
+    valid_implies_trans::<ClusterState>(
+        lift_state(pre),
+        lift_state(self.external_action_pre(step, (controller_id, input))),
+        lift_state(self.external_next().pre((controller_id, input)))
+    );
+    self.external_next().wf1((controller_id, input), spec, next, pre, post);
+}
 
 pub proof fn api_server_action_pre_implies_next_pre(self, step: APIServerStep, input: Option<Message>)
     ensures valid(lift_state(self.api_server_action_pre(step, input)).implies(lift_state(self.api_server_next().pre(input)))),
