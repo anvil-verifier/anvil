8a2fc115ec6cc00e668704789c103c24335941b0
Xudong Sun <xudongs3@illinois.edu>
2024-09-29 13:46:46 -0500

    Set up the ESR proof obligation for v2 VRS controller (#546)
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

---
+++ src/v2/controllers/vreplicaset_controller/proof/liveness/proof.rs
@@ -0,0 +1,39 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+#![allow(unused_imports)]
+use crate::kubernetes_api_objects::spec::prelude::*;
+use crate::kubernetes_cluster::spec::{cluster::*, message::*};
+use crate::temporal_logic::{defs::*, rules::*};
+use crate::vreplicaset_controller::{
+    model::{install::*, reconciler::*},
+    trusted::{liveness_theorem::*, spec_types::*, step::*},
+};
+use vstd::prelude::*;
+
+verus! {
+
+#[verifier(external_body)]
+proof fn eventually_stable_reconciliation_holds(spec: TempPred<ClusterState>, cluster: Cluster, controller_id: int)
+    requires
+        spec.entails(lift_state(cluster.init())),
+        spec.entails(always(lift_action(cluster.next()))),
+        // The vrs type is installed in the cluster.
+        cluster.type_is_installed_in_cluster::<VReplicaSetView>(),
+        // The vrs controller runs in the cluster.
+        cluster.controller_models.contains_pair(controller_id, vrs_controller_model()),
+        // The fairness condition of the controller.
+        spec.entails(tla_forall(|i: (Option<Message>, Option<ObjectRef>)| cluster.controller_next().weak_fairness((controller_id, i.0, i.1)))),
+        // The fairness condition of the API server.
+        spec.entails(tla_forall(|i| cluster.api_server_next().weak_fairness(i))),
+        // The fairness condition of the built-in controllers.
+        spec.entails(tla_forall(|i| cluster.builtin_controllers_next().weak_fairness(i))),
+        // The fairness condition of scheduling controller reconcile.
+        spec.entails(tla_forall(|i| cluster.schedule_controller_reconcile().weak_fairness((controller_id, i)))),
+        // No other controllers interfere with the vrs controller.
+        forall |other_id| cluster.controller_models.remove(controller_id).contains_key(other_id)
+            ==> spec.entails(always(lift_state(#[trigger] vrs_not_interfered_by(other_id)))),
+    ensures
+        spec.entails(vrs_eventually_stable_reconciliation()),
+{}
+
+}
--- src/v2/kubernetes_cluster/spec/mod.rs
+++ src/v2/kubernetes_cluster/spec/mod.rs
@@ -4,6 +4,7 @@ pub mod api_server;
 pub mod builtin_controllers;
 pub mod cluster;
 pub mod controller;
+pub mod esr;
 pub mod external;
 pub mod install_helpers;
 pub mod message;
