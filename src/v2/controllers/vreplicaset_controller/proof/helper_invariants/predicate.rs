f549e51d5cb35c96bc28a247fb20ac6968f97d85
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-01-15 15:12:53 -0600

    skip the proof by stating the invariant directly hold

--- src/v2/controllers/vreplicaset_controller/proof/helper_invariants/predicate.rs
+++ src/v2/controllers/vreplicaset_controller/proof/helper_invariants/predicate.rs
@@ -328,18 +328,18 @@ pub open spec fn every_delete_request_from_vrs_has_rv_precondition_that_is_less_
         }
     }
 }
+
+pub open spec fn vrs_does_not_have_deletion_timestamp (
+    vrs: VReplicaSetView, controller_id: int,
+) -> StatePred<ClusterState> {
+    |s: ClusterState| {
+        &&& s.resources()[vrs.object_ref()].metadata.deletion_timestamp.is_None()
+        &&& s.ongoing_reconciles(controller_id)[vrs.object_ref()].triggering_cr.metadata.deletion_timestamp.is_None()
+    }
+}
 //
 // TODO: Prove this.
 //
 // Every delete request must be on an object with a resource version less than the resource version counter.
 //
-
-pub open spec fn vrs_does_not_have_deletion_timestamp (
-    vrs: VReplicaSetView
-) -> StatePred<ClusterState> {
-    |s: ClusterState| {
-        let vrs_obj = s.resources()[vrs.object_ref()];
-        vrs_obj.metadata.deletion_timestamp.is_None()
-    }
-}
 }
