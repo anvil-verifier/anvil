b00be7df5fb48bec5ff4769f4565d0da9a23c4f1
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-01-22 16:27:30 -0600

    ensure code style consistency

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -208,7 +208,7 @@ pub proof fn seq_filter_is_a_subset_of_original_seq<A>(s: Seq<A>, pred: spec_fn(
     reveal(Seq::filter);
     if s.filter(pred).len() != 0 {
         let subseq = s.drop_last();
-        seq_filter_is_a_subset_of_original_seq(s.drop_last(), pred);
+        seq_filter_is_a_subset_of_original_seq(subseq, pred);
         assert(forall |i: int| 0 <= i < subseq.filter(pred).len() ==> subseq.contains(#[trigger] subseq.filter(pred)[i]));
         // assert(forall |i: int| 0 <= i < s.filter(pred).len() ==> s.contains(#[trigger] s.filter(pred)[i]));
         // assert(forall |e: A| s.filter(pred).contains(e) ==> #[trigger] s.contains(e));
