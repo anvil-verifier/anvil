f5eea87f400d791e9d01b4676e16fc57a449091e
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-01-22 02:59:49 -0600

    prove seq_filter_contains_implies_seq_contains and seq_filter_preserves_no_duplicates, ensure style consistency

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -128,7 +128,6 @@ proof fn seq_pred_false_on_all_elements_implies_empty_filter<A>(s: Seq<A>, pred:
     if s.len() != 0 {
         let subseq = s.drop_last();
         // prove precondition for subseq and recursive call
-<<<<<<< HEAD
         assert(forall |e: A| subseq.contains(e) ==> !pred(e)) by {
             assert(forall |i: int| 0 <= i < subseq.len() ==> s.contains(#[trigger] s[i]) ==> !pred(subseq[i]));
         }
@@ -168,20 +167,9 @@ proof fn empty_filter_implies_seq_pred_false_on_all_elements<A>(s: Seq<A>, pred:
             assert(forall |i: int| 0 <= i < subseq.len() ==> (subseq.contains(#[trigger] subseq[i]) ==> !pred(subseq[i])));
             assert(forall |i: int| 0 <= i < subseq.len() ==> s[i] == subseq[i]);
             // assert(!pred(s.last()) && s.contains(s.last()));
-=======
-        assert (forall |e: A| subseq.contains(e) ==> !pred(e)) by {
-            assert (forall |i: int| 0 <= i < subseq.len() ==> s.contains(#[trigger] s[i]) ==> !pred(subseq[i]));
->>>>>>> d4e96dd (ensure code style consistency)
         }
-        seq_pred_false_on_all_elements_implies_empty_filter(subseq, pred);
-        assert (subseq.filter(pred) == s.filter(pred)) by {
-            assert (!pred(s.last())) by {
-                assert (s.contains(s.last()) ==> !pred(s.last()));
-            };
-        } // s.filter(pred) == subseq.filter(pred) == ... == Seq::empty()
     }
 }
-<<<<<<< HEAD
 
 // useful theorem to prove the following 2
 proof fn seq_filter_is_a_subset_of_original_seq<A>(s: Seq<A>, pred: spec_fn(A) -> bool)
@@ -199,8 +187,6 @@ proof fn seq_filter_is_a_subset_of_original_seq<A>(s: Seq<A>, pred: spec_fn(A) -
         // assert(forall |e: A| s.filter(pred).contains(e) ==> #[trigger] s.contains(e));
     }
 }
-=======
->>>>>>> d4e96dd (ensure code style consistency)
 
 pub proof fn seq_filter_preserves_no_duplicates<A>(s: Seq<A>, pred: spec_fn(A) -> bool)
     requires s.no_duplicates(),
@@ -221,5 +207,20 @@ pub proof fn seq_filter_contains_implies_seq_contains<A>(s: Seq<A>, pred: spec_f
     ensures s.contains(elt)
 {
     seq_filter_is_a_subset_of_original_seq(s, pred);
+}
+}
+
+pub proof fn seq_filter_preserves_no_duplicates<A>(s: Seq<A>, pred: spec_fn(A) -> bool)
+    requires s.no_duplicates(),
+    ensures s.filter(pred).no_duplicates()
+    decreases s.len()
+{
+    reveal(Seq::filter);
+    if s.len() != 0 {
+        seq_filter_preserves_no_duplicates(s.drop_last(), pred);
+        if pred(s.last()) {
+            seq_filter_is_a_subset_of_original_seq(s.drop_last(), pred);
+        }
+    }
 }
 }
