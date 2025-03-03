63c56c41f0d7b9735c88dbde6f8ed8266e388248
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-03-03 16:48:36 -0600

    prove push_to_set_eq_to_set_insert

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -243,9 +243,32 @@ pub proof fn true_pred_on_all_element_equal_to_pred_on_all_index<A>(s: Seq<A>, p
     }
 }
 
-#[verifier(external_body)]
-pub proof fn push_to_set_eq_to_set_insert<A>(s: Seq<A>, elt: A)
-    ensures s.push(elt).to_set() == s.to_set().insert(elt);
+pub proof fn push_to_set_eq_to_set_insert<A>(s: Seq<A>, e: A)
+    ensures s.push(e).to_set() == s.to_set().insert(e)
+{
+    assert(s.push(e).to_set() =~= s.to_set().insert(e)) by {
+        assert forall |obj: A| s.push(e).to_set().contains(obj) implies #[trigger] s.to_set().insert(e).contains(obj) by {
+            assert(s.push(e).contains(obj));
+            if obj == e {
+                assert(s.to_set().insert(e).contains(e));
+            } else {
+                assert(s.contains(obj));
+                assert(s.to_set().contains(obj));
+                assert(s.to_set().insert(e).contains(obj));
+            }
+        }
+        assert forall |obj: A| s.to_set().insert(e).contains(obj) implies #[trigger] s.push(e).to_set().contains(obj) by {
+            if obj == e {
+                assert(s.push(e).last() == e); // why this trivial line is required
+                assert(s.push(e).contains(e));
+            } else {
+                assert(s.to_set().contains(obj));
+                assert(s.contains(obj));
+                assert(s == s.push(e).drop_last());
+            }
+        }
+    }
+}
 
 #[verifier(external_body)]
 pub proof fn map_values_to_set_eq_to_set_mk_map_values<A, B>(s: Seq<A>, map: spec_fn(A) -> B)
