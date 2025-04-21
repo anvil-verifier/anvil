f99885e870456cf63f7c2385a755e397185d9720
Xudong Sun <xudongs3@illinois.edu>
2025-04-20 22:01:28 -0500

    Delete deprecated VRS controller
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -3,8 +3,8 @@
 pub mod common;
 pub mod fluent_e2e;
 pub mod rabbitmq_e2e;
-pub mod vreplicaset_e2e;
 pub mod v2_vreplicaset_e2e;
+pub mod vreplicaset_e2e;
 pub mod zookeeper_e2e;
 
 use common::Error;
@@ -13,8 +13,8 @@ use rabbitmq_e2e::{rabbitmq_e2e_test, rabbitmq_ephemeral_e2e_test, rabbitmq_scal
 use std::str::FromStr;
 use std::{env, sync::Arc};
 use tracing::*;
-use vreplicaset_e2e::vreplicaset_e2e_test;
 use v2_vreplicaset_e2e::v2_vreplicaset_e2e_test;
+use vreplicaset_e2e::vreplicaset_e2e_test;
 use zookeeper_e2e::{zookeeper_e2e_test, zookeeper_ephemeral_e2e_test, zookeeper_scaling_e2e_test};
 
 #[tokio::main]
@@ -51,10 +51,6 @@ async fn main() -> Result<(), Error> {
         "fluent" => {
             info!("Running fluent end-to-end test");
             return fluent_e2e_test().await;
-        }
-        "vreplicaset" => {
-            info!("Running vreplicaset end-to-end test");
-            return vreplicaset_e2e_test().await;
         }
         "v2-vreplicaset" => {
             info!("Running v2-vreplicaset end-to-end test");
--- src/controller_examples/v_replica_set_controller/proof/helper_invariants/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 VMware, Inc.
-// SPDX-License-Identifier: MIT
-pub mod predicate;
-pub mod proof;
-
-pub use predicate::*;
-pub use proof::*;
-
