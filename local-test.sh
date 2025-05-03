203d6bb950a2c2e538087333c40d59456f71cd3f
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-05-02 19:07:01 -0500

    do not use docker builder

--- local-test.sh
+++ local-test.sh
@@ -9,10 +9,26 @@ set -xeu
 app=$(echo "$1" | tr '_' '-')
 app_filename=$(echo "$app" | tr '-' '_')
 
+rv="${VERUS_DIR}/source/target-verus/release/verus"
+
+if [ ! -f "$rv" ]; then
+  echo "Verus not found. Please set VERUS_DIR correct"
+  exit 1
+fi
+
+if [ $# -gt 1 ] && [ "$2" == "--build" ]; then
+    shift 2
+    ./build.sh "${app_filename}_controller.rs" "--no-verify" $@
+fi
+
+if [ ! -f "src/${app_filename}_controller" ]; then
+    echo "Controller ${app_filename} not found. Please build the controller manually or use the --build flag"
+    exit 1
+fi
+
 # Copy the Dockerfile and build the docker image of the controller
-cp docker/controller/Dockerfile .
-docker build -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
-rm Dockerfile
+docker build -f docker/controller/Dockerfile -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
+
 # Set up the kind cluster and load the image into the cluster
 kind create cluster --config deploy/kind.yaml
 kind load docker-image local/$app-controller:v0.1.0
