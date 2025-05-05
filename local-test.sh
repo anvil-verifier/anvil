52e2607fbafb6761a53c38a595db62fa2ed01124
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-05-04 20:46:20 -0500

    add local/remote build support, remote builder should be used when host has different env (libc, etc.)

--- local-test.sh
+++ local-test.sh
@@ -8,23 +8,35 @@ set -xeu
 
 app=$(echo "$1" | tr '_' '-')
 app_filename=$(echo "$app" | tr '-' '_')
+build_controller="no"
 
-if [ $# -gt 1 ] && [ "$2" == "--build" ]; then
-    if [ ! -f "${VERUS_DIR}/source/target-verus/release/verus" ]; then
-        echo "Verus not found. Please set VERUS_DIR correct"
-        exit 1
+if [ $# -gt 1 ]; then
+    if  [ "$2" == "--build" ]; then # chain build.sh
+        if [ ! -f "${VERUS_DIR}/source/target-verus/release/verus" ]; then
+            echo "Verus not found. Please set VERUS_DIR correct"
+            exit 1
+        fi
+        build_controller="local"
+    elif [ "$2" == "--build-remote" ]; then
+        build_controller="remote"
     fi
-    shift 2
-    ./build.sh "${app_filename}_controller.rs" "--no-verify" $@
 fi
 
-if [ ! -f "src/${app_filename}_controller" ]; then
-    echo "Controller ${app_filename} not found. Please build the controller manually or use the --build flag"
-    exit 1
-fi
+case "$build_controller" in
+    local)
+        shift 2
+        ./build.sh "${app_filename}_controller.rs" "--no-verify" $@
+        docker build -f docker/controller/Dockerfile.local -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
+        ;;
+    remote)
+        docker build -f docker/controller/Dockerfile.remote -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
+        ;;
+    no)
+        shift 1
+        docker build -f docker/controller/Dockerfile.local -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
+        ;;
+esac
 
-# Copy the Dockerfile and build the docker image of the controller
-docker build -f docker/controller/Dockerfile -t local/$app-controller:v0.1.0 --build-arg APP=$app_filename .
 
 # Set up the kind cluster and load the image into the cluster
 kind create cluster --config deploy/kind.yaml
