eeba23e9a182fedd5c1966332ef8f062be36ad4b
Xudong Sun <xudongs3@illinois.edu>
2025-05-10 14:17:22 -0500

    Add lib.rs back
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub mod conformance_tests;
+pub mod executable_model;
+#[path = "v2/external_shim_layer/mod.rs"]
+pub mod external_shim_layer;
+pub mod kubernetes_api_objects;
+#[path = "v2/kubernetes_cluster/mod.rs"]
+pub mod kubernetes_cluster;
+#[path = "v2/reconciler/mod.rs"]
+pub mod reconciler;
+#[path = "v2/shim_layer/mod.rs"]
+pub mod shim_layer;
+pub mod state_machine;
+pub mod temporal_logic;
+pub mod unit_tests;
+pub mod vstd_ext;
+
+use vstd::prelude::*;
