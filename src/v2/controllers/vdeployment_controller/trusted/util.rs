b33cf0eb219a7f42a4521df0c3d065b191a80a88
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-06-20 03:17:12 -0500

    Fix VDeployment reconciler bug (#648)
    Fix a bug when there are multiple VReplicaset matching VDeployment's
    template, VDeployment controller can not reconcile the cluster state to
    be desired state by scaling down all but one of them.
    
    ### Bug description
    
    Previously the check on managed ReplicaSets was
    
    ```rust
    if new_vrs_list.len() == 0 {
        // create the new vrs
        create_new_vrs(state, vd)
    } else {
        let new_vrs = new_vrs_list.last();
        let state = VDeploymentReconcileState {
            new_vrs: Some(new_vrs),
            ..state
        };
        // ..
    ```
    
    Because when replicating the logic of k8s ReplicaSet(RS) controller,
    there was one
    [comment](https://github.com/kubernetes/kubernetes/blob/73014647f9bdd7186b5fe121326b961d6521c162/pkg/controller/deployment/util/deployment_util.go#L633-L636):
    ```rust
     // In rare cases, such as after cluster upgrades, Deployment may end up with 
     // having more than one new ReplicaSets that have the same template as its template, 
     // see https://github.com/kubernetes/kubernetes/issues/40415 
     // We deterministically choose the oldest new ReplicaSet.
    ```
    In kubernetes, when there are multiple RS with the same template as a
    deployment's, only one will be marked as new(updated) replicaset. Back
    that time I don't think our controller will allow multiple vrs to
    co-exist under the same template, but that is possible under the model
    of pod monkey. In this case, the deployment controller will not be able
    to reconcile cluster state with multiple RS matching deployment CR's
    template to desired state with only one RS. It simply ignores other RS.
    
    Vanilla kubernetes implementation doesn't have this problem because it
    uses negation to select all - new as old RS to be scaled down, no matter
    if their template match deployment's or not. However, my model only sort
    out RS with template unmatched to be scaled down. This case is also a
    corner case caused by updating kubernetes itself, and slipped through
    our e2e test but can be captured by the pod monkey model. It's not found
    by verification, it's found by specification when I was trying to find a
    correct way to describe the current state.
    
    ### Flaky proof
    
    Due to the flakiness of Verus proof,
    `vdeployment_controller::exec::reconciler::reconcile_core` is masked,
    while it can be manually proved with
    ```shell
    verus src/v2_vdeployment_controller.rs -L dependency=src/deps_hack/target/debug/deps --extern=deps_hack="src/deps_hack/target/debug/libdeps_hack.rlib" --verify-only-module vdeployment_controller::exec::reconciler --no-lifetime --verify-function reconcile_core
    ```
    Detailed report can be found at
    https://github.com/verus-lang/verus/issues/1756.

--- src/v2/controllers/vdeployment_controller/trusted/util.rs
+++ src/v2/controllers/vdeployment_controller/trusted/util.rs
@@ -26,20 +26,19 @@ pub open spec fn filter_vrs_list(vrs_list: Seq<VReplicaSetView>, vd: VDeployment
         && vrs.well_formed())
 }
 
-pub open spec fn filter_old_and_new_vrs(vrs_list: Seq<VReplicaSetView>, vd: VDeploymentView) -> (res: (Seq<VReplicaSetView>, Seq<VReplicaSetView>))
-// we don't consider there is more than one new vrs controlled by vd, check discussion/kubernetes-model/deployment_controller.md for details
+pub open spec fn filter_old_and_new_vrs(vd: VDeploymentView, vrs_list: Seq<VReplicaSetView>) -> (res: (Option<VReplicaSetView>, Seq<VReplicaSetView>))
 {
-    // even if we know vrs controlled by vd should have spec.template.metadata.is_Some() because we add the pot-template-hash label
-    // we still need to check it here and pretend we don't know it
-
-    let new_spec_filter = |vrs: VReplicaSetView|
-        match_template_without_hash(vd, vrs);
-    let old_spec_filter = |vrs: VReplicaSetView|
-        !new_spec_filter(vrs)
-        && (vrs.spec.replicas.is_None() || vrs.spec.replicas.unwrap() > 0);
-    let new_vrs_list = vrs_list.filter(new_spec_filter);
-    let old_vrs_list = vrs_list.filter(old_spec_filter);
-    (new_vrs_list, old_vrs_list)
+    let new_vrs_list = vrs_list.filter(|vrs: VReplicaSetView| match_template_without_hash(vd, vrs));
+    let new_vrs = if new_vrs_list.len() == 0 {
+        None
+    } else {
+        Some(new_vrs_list.first())
+    };
+    let old_vrs_list = vrs_list.filter(|vrs: VReplicaSetView| {
+        &&& new_vrs.is_None() || vrs.metadata.uid != new_vrs.get_Some_0().metadata.uid
+        &&& vrs.spec.replicas.is_None() || vrs.spec.replicas.unwrap() > 0
+    });
+    (new_vrs, old_vrs_list)
 }
 
 pub open spec fn match_template_without_hash(vd: VDeploymentView, vrs: VReplicaSetView) -> bool {
