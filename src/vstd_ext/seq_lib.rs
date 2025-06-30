aa7ebb41754167c2062dd64c90e7ac849acde2a2
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-06-30 16:40:07 -0500

    prove commutativity_of_seq_drop_last_and_map

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -368,4 +368,20 @@ pub proof fn commutativity_of_seq_map_and_filter<A, B>(s: Seq<A>, pred: spec_fn(
     }
 }
 
+pub proof fn commutativity_of_seq_drop_last_and_map<A, B>(s: Seq<A>, pred: spec_fn(A) -> B)
+    requires s.len() > 0,
+    ensures s.drop_last().map_values(pred) == s.map_values(pred).drop_last(),
+    decreases s.len()
+{
+    broadcast use group_seq_properties;
+    if s.len() > 1 {
+        let subseq = s.drop_last();
+        commutativity_of_seq_drop_last_and_map(subseq, pred);
+        assert(s.map_values(pred).drop_last() == subseq.map_values(pred));
+    } else {
+        assert(s.drop_last().map_values(pred) == Seq::<B>::empty());
+        assert(s.map_values(pred).drop_last() == Seq::<B>::empty());
+    }
+}
+
 }
