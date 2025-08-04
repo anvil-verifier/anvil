dfd5820735f1e019f3725d1bce2e62dc8dd5ec5c
Xudong Sun <xudongs3@illinois.edu>
2025-08-04 19:21:56 +0800

    Finish the proof
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/kubernetes_cluster/proof/composition.rs
+++ src/kubernetes_cluster/proof/composition.rs
@@ -6,39 +6,96 @@ use vstd::prelude::*;
 verus! {
 
 pub struct ControllerSpec {
-    pub progress_guarantee: TempPred<ClusterState>,
-    pub progress_rely: TempPred<ClusterState>,
-    pub non_interference_guarantee: TempPred<ClusterState>,
-    pub non_interference_partial_rely: spec_fn(int) -> TempPred<ClusterState>,
+    pub liveness_guarantee: TempPred<ClusterState>,
+    pub liveness_rely: TempPred<ClusterState>,
+    pub safety_guarantee: TempPred<ClusterState>,
+    pub safety_partial_rely: spec_fn(int) -> TempPred<ClusterState>,
     pub fairness: TempPred<ClusterState>,
     pub membership: spec_fn(Cluster) -> bool,
 }
 
 pub open spec fn composable(spec: TempPred<ClusterState>, cluster: Cluster, controller_specs: Map<int, ControllerSpec>) -> bool {
-    (
-        forall_on_map(controller_specs, |i:int, c: ControllerSpec| (c.membership)(cluster))
+    &&& forall_on_map(controller_specs, |i:int, c: ControllerSpec| (c.membership)(cluster))
         && spec.entails(lift_state(cluster.init()))
         && spec.entails(always(lift_action(cluster.next())))
         && forall_on_map(controller_specs, |i:int, c: ControllerSpec| spec.entails(c.fairness))
         && forall_on_map(controller_specs,
                 |i:int, c: ControllerSpec| forall_on_map(cluster.controller_models.remove_keys(controller_specs.dom()),
-                    |other, oc: ControllerModel| spec.entails((c.non_interference_partial_rely)(other)))
+                    |other, oc: ControllerModel| spec.entails((c.safety_partial_rely)(other)))
             )
-    ) ==> (
-        forall_on_map(controller_specs, |i:int, c: ControllerSpec| spec.entails(c.progress_guarantee))
-        && forall_on_map(controller_specs, |i:int, c: ControllerSpec| spec.entails(c.non_interference_guarantee))
-    )
+        ==> forall_on_map(controller_specs, |i:int, c: ControllerSpec| spec.entails(c.liveness_guarantee))
+    &&& forall_on_map(controller_specs, |i:int, c: ControllerSpec| (c.membership)(cluster))
+        && spec.entails(lift_state(cluster.init()))
+        && spec.entails(always(lift_action(cluster.next())))
+        ==> forall_on_map(controller_specs, |i:int, c: ControllerSpec| spec.entails(c.safety_guarantee))
 }
 
-#[verifier(external_body)]
-proof fn horizontal_composition<HC>(spec: TempPred<ClusterState>, cluster: Cluster, id: int, c: ControllerSpec, composed: Map<int, ControllerSpec>)
+proof fn horizontal_composition<HC>(spec: TempPred<ClusterState>, cluster: Cluster)
     where
         HC: HorizontalComposition,
     requires
+        !HC::composed().contains_key(HC::id()),
         composable(spec, cluster, HC::composed()),
     ensures
         composable(spec, cluster, HC::composed().insert(HC::id(), HC::c())),
-{}
+{
+    let composed = HC::composed();
+    let new_composed = HC::composed().insert(HC::id(), HC::c());
+
+    if forall_on_map(new_composed, |i:int, c: ControllerSpec| (c.membership)(cluster))
+        && spec.entails(lift_state(cluster.init()))
+        && spec.entails(always(lift_action(cluster.next())))
+    {
+        assert((HC::c().membership)(cluster)) by {
+            assert(new_composed.contains_key(HC::id()));
+        }
+        assert forall |i| #[trigger] composed.contains_key(i) implies (composed[i].membership)(cluster) by {
+            assert(new_composed.contains_key(i));
+        }
+        
+        HC::safety_is_guaranteed(spec, cluster);
+
+        if forall_on_map(new_composed, |i:int, c: ControllerSpec| spec.entails(c.fairness))
+            && forall_on_map(new_composed,
+                    |i:int, c: ControllerSpec| forall_on_map(cluster.controller_models.remove_keys(new_composed.dom()),
+                        |other, oc: ControllerModel| spec.entails((c.safety_partial_rely)(other)))
+                )
+        {
+            HC::no_internal_interference(spec, cluster);
+
+            assert forall |i| #[trigger] composed.contains_key(i)
+            implies spec.entails(composed[i].liveness_guarantee) by {
+                assert forall |i| #[trigger] composed.contains_key(i) implies spec.entails(composed[i].fairness) by {
+                    assert(new_composed.contains_key(i));
+                }
+
+                assert forall |i| #[trigger] composed.contains_key(i)
+                implies forall_on_map(cluster.controller_models.remove_keys(composed.dom()),
+                    |other, oc: ControllerModel| spec.entails((composed[i].safety_partial_rely)(other)))
+                by {
+                    assert(new_composed.contains_key(i));
+                    assert forall |other| #[trigger] cluster.controller_models.remove_keys(composed.dom()).contains_key(other)
+                    implies spec.entails((composed[i].safety_partial_rely)(other)) by {
+                        if cluster.controller_models.remove_keys(new_composed.dom()).contains_key(other) {}
+                    }
+                }
+            }
+
+            assert(spec.entails(HC::c().liveness_guarantee)) by {
+                assert(spec.entails(HC::c().fairness)) by {
+                    assert(new_composed.contains_key(HC::id()));
+                }
+
+                assert forall |other_id| cluster.controller_models.remove(HC::id()).contains_key(other_id)
+                implies spec.entails((HC::c().safety_partial_rely)(other_id)) by {
+                    if cluster.controller_models.remove_keys(new_composed.dom()).contains_key(other_id) {}
+                }
+
+                HC::liveness_is_guaranteed(spec, cluster);
+            }
+        }
+    }
+}
 
 pub trait HorizontalComposition: Sized {
     spec fn id() -> int;
@@ -47,39 +104,39 @@ pub trait HorizontalComposition: Sized {
 
     spec fn composed() -> Map<int, ControllerSpec>;
 
-    proof fn progress_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
+    proof fn liveness_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
             spec.entails(lift_state(cluster.init())),
             spec.entails(always(lift_action(cluster.next()))),
             spec.entails(Self::c().fairness),
             forall |other_id| cluster.controller_models.remove(Self::id()).contains_key(other_id)
-                ==> spec.entails((Self::c().non_interference_partial_rely)(other_id)),
+                ==> spec.entails((Self::c().safety_partial_rely)(other_id)),
         ensures
-            spec.entails(Self::c().progress_guarantee),
+            spec.entails(Self::c().liveness_guarantee),
         ;
 
-    proof fn non_interference_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
+    proof fn safety_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
             spec.entails(lift_state(cluster.init())),
             spec.entails(always(lift_action(cluster.next()))),
         ensures
-            spec.entails(Self::c().non_interference_guarantee),
+            spec.entails(Self::c().safety_guarantee),
         ;
 
-    proof fn non_interference_is_compatible(spec: TempPred<ClusterState>, cluster: Cluster)
+    proof fn no_internal_interference(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
             spec.entails(lift_state(cluster.init())),
             spec.entails(always(lift_action(cluster.next()))),
-            spec.entails(Self::c().non_interference_guarantee),
+            spec.entails(Self::c().safety_guarantee),
             !Self::composed().contains_key(Self::id()),
             forall_on_map(Self::composed(), |i:int, c: ControllerSpec| (c.membership)(cluster)),
-            forall_on_map(Self::composed(), |i:int, c: ControllerSpec| spec.entails(c.non_interference_guarantee)),
+            forall_on_map(Self::composed(), |i:int, c: ControllerSpec| spec.entails(c.safety_guarantee)),
         ensures
-            forall_on_map(Self::composed(), |i, c: ControllerSpec| spec.entails((Self::c().non_interference_partial_rely)(i))
-                && spec.entails((c.non_interference_partial_rely)(Self::id())))
+            forall_on_map(Self::composed(), |i, c: ControllerSpec| spec.entails((Self::c().safety_partial_rely)(i))
+                && spec.entails((c.safety_partial_rely)(Self::id())))
         ;
 }
 
