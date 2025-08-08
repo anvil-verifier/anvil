45a8c0844258b72a12a19c1230b58cec69efbd86
Xudong Sun <xudongs3@illinois.edu>
2025-08-08 11:06:25 +0800

    comments
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/kubernetes_cluster/proof/composition.rs
+++ src/kubernetes_cluster/proof/composition.rs
@@ -5,37 +5,56 @@ use vstd::prelude::*;
 
 verus! {
 
+// A controller's compositional spec includes
+// (1) guarantee conditions (e.g., ESR),
+// (2) rely conditions (e.g., non-interference by other controllers),
+// (3) fairness conditions (e.g., weak fairness),
+// (4) membership conditions (e.g., the CR is installed).
 pub struct ControllerSpec {
     pub liveness_guarantee: TempPred<ClusterState>,
     pub liveness_rely: TempPred<ClusterState>,
     pub safety_guarantee: TempPred<ClusterState>,
+    // safety_partial_rely takes a controller id (int) and returns a (safety) condition
+    // stating that the input controller never interferes with our controller.
+    // The safety-rely condition is formalized as a conjunction of safety_partial_rely
+    // on all other controller ids in the environment.
     pub safety_partial_rely: spec_fn(int) -> TempPred<ClusterState>,
     pub fairness: TempPred<ClusterState>,
     pub membership: spec_fn(Cluster) -> bool,
 }
 
-pub open spec fn composable(spec: TempPred<ClusterState>, cluster: Cluster, controller_specs: Map<int, ControllerSpec>) -> bool {
-    &&& (forall |i| #[trigger] controller_specs.contains_key(i) ==> (controller_specs[i].membership)(cluster))
+// composable says that when the controllers run together (with other controllers)
+// (1) all controllers' safety_guarantee hold, and
+// (2) all controllers' liveness_guarantee hold assuming fairness and that other controllers don't interfere with them.
+pub open spec fn composable(spec: TempPred<ClusterState>, cluster: Cluster, composition: Map<int, ControllerSpec>) -> bool {
+    &&& (forall |i| #[trigger] composition.contains_key(i)
+        ==> (composition[i].membership)(cluster))
         && spec.entails(lift_state(cluster.init()))
         && spec.entails(always(lift_action(cluster.next())))
-        ==> (forall |i| #[trigger] controller_specs.contains_key(i) ==> spec.entails(controller_specs[i].safety_guarantee))
-    &&& (forall |i| #[trigger] controller_specs.contains_key(i) ==> (controller_specs[i].membership)(cluster))
+        ==> (forall |i| #[trigger] composition.contains_key(i)
+            ==> spec.entails(composition[i].safety_guarantee))
+    &&& (forall |i| #[trigger] composition.contains_key(i)
+        ==> (composition[i].membership)(cluster))
         && spec.entails(lift_state(cluster.init()))
         && spec.entails(always(lift_action(cluster.next())))
-        && (forall |i| #[trigger] controller_specs.contains_key(i) ==> spec.entails(controller_specs[i].fairness))
-        && (forall |i| #[trigger] controller_specs.contains_key(i) ==>
-            forall |j| #[trigger] cluster.controller_models.remove_keys(controller_specs.dom()).contains_key(j) ==>
-                spec.entails((controller_specs[i].safety_partial_rely)(j)))
-        ==> (forall |i| #[trigger] controller_specs.contains_key(i) ==> spec.entails(controller_specs[i].liveness_guarantee))
+        && (forall |i| #[trigger] composition.contains_key(i) ==> spec.entails(composition[i].fairness))
+        && (forall |i| #[trigger] composition.contains_key(i)
+            ==> forall |j| #[trigger] cluster.controller_models.remove_keys(composition.dom()).contains_key(j)
+                ==> spec.entails((composition[i].safety_partial_rely)(j)))
+        ==> (forall |i| #[trigger] composition.contains_key(i) ==> spec.entails(composition[i].liveness_guarantee))
 }
 
 pub trait Composition: Sized {
+    // The id of the controller we want to compose (into other verified controllers)
     spec fn id() -> int;
 
+    // The spec of the controller we want to compose
     spec fn c() -> ControllerSpec;
 
+    // The controllers that have been composed
     spec fn composed() -> Map<int, ControllerSpec>;
 
+    // safety_guarantee of the new controller holds
     proof fn safety_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
@@ -45,6 +64,8 @@ pub trait Composition: Sized {
             spec.entails(Self::c().safety_guarantee),
         ;
 
+    // The new controller doesn't interfere with composed controllers, or
+    // they satisfy each other's safety_partial_rely
     proof fn no_internal_interference(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
@@ -62,6 +83,8 @@ pub trait Composition: Sized {
 }
 
 pub trait HorizontalComposition: Sized + Composition {
+    // For HorizontalComposition, the new controller's liveness doesn't depend on
+    // other controllers' liveness
     proof fn liveness_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
@@ -76,6 +99,8 @@ pub trait HorizontalComposition: Sized + Composition {
 }
 
 pub trait VerticalComposition: Sized + Composition {
+    // For VerticalComposition, the new controller's liveness depends on
+    // other controllers' liveness
     proof fn liveness_is_guaranteed(spec: TempPred<ClusterState>, cluster: Cluster)
         requires
             (Self::c().membership)(cluster),
