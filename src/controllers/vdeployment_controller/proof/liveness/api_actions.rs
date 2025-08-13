7d1b3e0f3b7d12d6a4e4fc7edc49236b572a7f27
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-08-13 18:07:16 -0500

    fix aftermatch of rebase

--- src/controllers/vdeployment_controller/proof/liveness/api_actions.rs
+++ src/controllers/vdeployment_controller/proof/liveness/api_actions.rs
@@ -232,31 +232,12 @@ ensures
                                 }
                             }
                         },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-                        APIRequest::UpdateRequest(req) => {}, // vd controller doesn't send update req
->>>>>>> fe223b4 (prove lemma_api_request_other_than_pending_req_msg_maintains_objects_owned_by_vd)
-=======
->>>>>>> 98fff03 (unmask lemma_from_after_send_create_pod_req_to_receive_ok_resp)
                         _ => {},
                     }
                 }
             },
-<<<<<<< HEAD
-<<<<<<< HEAD
             _ => {},
         }
-=======
-            _ => {}, // somehow this branch is slow
-        }
-        assert(s_prime.resources().contains_key(key));
-        assert(s_prime.resources()[key] == etcd_obj);
->>>>>>> fe223b4 (prove lemma_api_request_other_than_pending_req_msg_maintains_objects_owned_by_vd)
-=======
-            _ => {},
-        }
->>>>>>> 98fff03 (unmask lemma_from_after_send_create_pod_req_to_receive_ok_resp)
     }
 }
 
