e7eb6625912d8260e1f1409c7446d1950c0efd2e
Xudong Sun <xudongs3@illinois.edu>
2025-08-15 11:10:32 +0800

    Fix
    Signed-off-by: Xudong Sun <xudongs3@illinois.edu>

--- src/controllers/vreplicaset_controller/trusted/exec_types.rs
+++ src/controllers/vreplicaset_controller/trusted/exec_types.rs
@@ -11,41 +11,19 @@ use vstd::prelude::*;
 
 verus! {
 
-#[verifier(external_body)]
-pub struct VReplicaSet {
-    inner: deps_hack::VReplicaSet
-}
+implement_object_wrapper_type!(
+    VReplicaSet,
+    deps_hack::VReplicaSet,
+    spec_types::VReplicaSetView
+);
 
-impl View for VReplicaSet {
-    type V = spec_types::VReplicaSetView;
-
-    uninterp spec fn view(&self) -> spec_types::VReplicaSetView;
-}
-
-implement_deep_view_trait!(VReplicaSet, spec_types::VReplicaSetView);
-
-impl std::clone::Clone for VReplicaSet {
-    #[verifier(external_body)]
-    fn clone(&self) -> (result: Self)
-        ensures result == self,
-    {
-        VReplicaSet { inner: self.inner.clone() }
-    }
-}
+implement_field_wrapper_type!(
+    VReplicaSetSpec,
+    deps_hack::VReplicaSetSpec,
+    spec_types::VReplicaSetSpecView
+);
 
 impl VReplicaSet {
-    #[verifier(external_body)]
-    pub fn default() -> (vreplicaset: VReplicaSet)
-        ensures vreplicaset@ == spec_types::VReplicaSetView::default(),
-    {
-        VReplicaSet {
-            inner: deps_hack::VReplicaSet {
-                metadata: deps_hack::k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta::default(),
-                spec: deps_hack::VReplicaSetSpec::default(),
-            }
-        }
-    }
-
     #[verifier(external_body)]
     pub fn well_formed(&self) -> (b: bool)
         ensures b == self@.well_formed(),
@@ -55,24 +33,10 @@ impl VReplicaSet {
     }
 
     #[verifier(external_body)]
-    pub fn metadata(&self) -> (metadata: ObjectMeta)
-        ensures metadata@ == self@.metadata,
-    {
-        ObjectMeta::from_kube(self.inner.metadata.clone())
-    }
-
-    #[verifier(external_body)]
     pub fn spec(&self) -> (spec: VReplicaSetSpec)
         ensures spec@ == self@.spec,
     {
         VReplicaSetSpec { inner: self.inner.spec.clone() }
-    }
-
-    #[verifier(external_body)]
-    pub fn api_resource() -> (res: ApiResource)
-        ensures res@.kind == spec_types::VReplicaSetView::kind(),
-    {
-        ApiResource::from_kube(deps_hack::kube::api::ApiResource::erase::<deps_hack::VReplicaSet>(&()))
     }
 
     #[verifier(external_body)]
@@ -84,41 +48,10 @@ impl VReplicaSet {
     }
 
     #[verifier(external_body)]
-    pub fn set_metadata(&mut self, metadata: ObjectMeta)
-        ensures self@ == old(self)@.with_metadata(metadata@),
-    {
-        self.inner.metadata = metadata.into_kube();
-    }
-
-    #[verifier(external_body)]
     pub fn set_spec(&mut self, spec: VReplicaSetSpec)
         ensures self@ == old(self)@.with_spec(spec@),
     {
         self.inner.spec = spec.into_kube();
-    }
-
-    // NOTE: This function assumes serde_json::to_string won't fail!
-    #[verifier(external_body)]
-    pub fn marshal(self) -> (obj: DynamicObject)
-        ensures obj@ == self@.marshal(),
-    {
-        // TODO: this might be unnecessarily slow
-        DynamicObject::from_kube(deps_hack::k8s_openapi::serde_json::from_str(&deps_hack::k8s_openapi::serde_json::to_string(&self.inner).unwrap()).unwrap())
-    }
-
-    #[verifier(external_body)]
-    pub fn unmarshal(obj: DynamicObject) -> (res: Result<VReplicaSet, UnmarshalError>)
-        ensures
-            res is Ok == spec_types::VReplicaSetView::unmarshal(obj@) is Ok,
-            res is Ok ==> res->Ok_0@ == spec_types::VReplicaSetView::unmarshal(obj@)->Ok_0,
-    {
-        let parse_result = obj.into_kube().try_parse::<deps_hack::VReplicaSet>();
-        if parse_result.is_ok() {
-            let res = VReplicaSet { inner: parse_result.unwrap() };
-            Ok(res)
-        } else {
-            Err(())
-        }
     }
 
     pub fn state_validation(&self) -> (res: bool)
@@ -163,23 +96,7 @@ impl VReplicaSet {
     }
 }
 
-#[verifier(external_body)]
-pub struct VReplicaSetSpec {
-    inner: deps_hack::VReplicaSetSpec,
-}
-
 impl VReplicaSetSpec {
-    pub uninterp spec fn view(&self) -> spec_types::VReplicaSetSpecView;
-
-    #[verifier(external_body)]
-    pub fn default() -> (vreplicaset_spec: VReplicaSetSpec)
-        ensures vreplicaset_spec@ == spec_types::VReplicaSetSpecView::default(),
-    {
-        VReplicaSetSpec {
-            inner: deps_hack::VReplicaSetSpec::default()
-        }
-    }
-
     #[verifier(external_body)]
     pub fn replicas(&self) -> (replicas: Option<i32>)
         ensures
@@ -228,13 +145,6 @@ impl VReplicaSetSpec {
     {
         self.inner.template = Some(template.into_kube());
     }
-
-    #[verifier(external)]
-    pub fn into_kube(self) -> deps_hack::VReplicaSetSpec { self.inner }
-
-    #[verifier(external)]
-    pub fn from_kube(inner: deps_hack::VReplicaSetSpec) -> VReplicaSetSpec { VReplicaSetSpec { inner: inner } }
-
 }
 
 }
