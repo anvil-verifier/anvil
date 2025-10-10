79c091878a87f5219cbbf2797c5b59c20bd618d3
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-10-09 20:41:19 -0500

    fix lemma_from_old_vrs_len_zero_at_scale_down_old_vrs_to_current_state_matches

--- src/controllers/vdeployment_controller/proof/liveness/resource_match.rs
+++ src/controllers/vdeployment_controller/proof/liveness/resource_match.rs
@@ -2007,9 +2007,13 @@ ensures
                 lemma_api_request_other_than_pending_req_msg_maintains_local_state_validity_and_coherence(s, s_prime, vd, cluster, controller_id, msg);
                 lemma_api_request_other_than_pending_req_msg_maintains_etcd_state(
                     s, s_prime, vd, cluster, controller_id, msg, Some((nv_uid_key.0, nv_uid_key.1, vd.spec.replicas.unwrap_or(int1!()))), nat0!()
-                )
+                );
+                lemma_api_request_other_than_pending_req_msg_maintains_object_owned_by_vd(
+                    s, s_prime, vd, cluster, controller_id, msg
+                );
             },
             Step::ControllerStep(input) => {
+                assume(false);
                 if input.0 == controller_id && input.1 == Some(resp_msg) && input.2 == Some(vd.object_ref()) {
                     VDeploymentReconcileState::marshal_preserves_integrity();
                     final_state_to_esr(vd, controller_id, Some((nv_uid_key.0, nv_uid_key.1, vd.spec.replicas.unwrap_or(int1!()))), nat0!(), s_prime);
