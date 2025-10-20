0f0497da03732db348ad53757674977a5fe4738a
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-10-20 17:20:12 -0500

    add lemma_homomorphism_of_map_values

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -433,4 +433,11 @@ pub proof fn same_filter_implies_same_result<A>(s: Seq<A>, f1: spec_fn(A) -> boo
         }
     }
 }
+
+#[verifier(external_body)]
+pub proof fn lemma_homomorphism_of_map_values<A, B, C>(s: Seq<A>, f1: spec_fn(A) -> B, f2: spec_fn(B) -> C, g: spec_fn(A)->C)
+    requires forall |e: A| #[trigger] s.contains(e) ==> f2(f1(e)) == g(e),
+    ensures s.map_values(g) == s.map_values(f1).map_values(f2),
+{}
+
 }
