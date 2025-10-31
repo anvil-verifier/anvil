09970a9782425aa5c4c911131b092d2467e07dce
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-10-30 22:08:03 -0500

    finally remove s.ongoing_reconciles(controller_id).contains_key(vd.object_ref()) from pre of lemma_api_request_other_than_pending_req_msg_maintains_objects_owned_by_vd

--- src/controllers/vdeployment_controller/proof/liveness/api_actions.rs
+++ src/controllers/vdeployment_controller/proof/liveness/api_actions.rs
@@ -478,7 +478,6 @@ requires
     forall |vd| helper_invariants::vd_reconcile_request_only_interferes_with_itself(controller_id, vd)(s),
     vd_rely_condition(cluster, controller_id)(s),
     msg.src != HostId::Controller(controller_id, vd.object_ref()),
-    s.ongoing_reconciles(controller_id).contains_key(vd.object_ref()),
     // (!Cluster::pending_req_msg_is(controller_id, s, vd.object_ref(), msg)
     //     || !s.ongoing_reconciles(controller_id).contains_key(vd.object_ref())),
 ensures
