8ac09797be1c430f61a62f6c41a95ef758a9eb07
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-11-01 23:02:11 -0500

    reduce warning

--- src/controllers/vdeployment_controller/proof/liveness/spec.rs
+++ src/controllers/vdeployment_controller/proof/liveness/spec.rs
@@ -232,12 +232,9 @@ pub proof fn spec_of_previous_phases_entails_eventually_new_invariants(provided_
 {
     let spec = provided_spec.and(spec_before_phase_n(i, vd, cluster, controller_id));
     // assert non-interference property on combined spec.
-    assert forall |other_id| 
-        (forall |other_id| cluster.controller_models.remove(controller_id).contains_key(other_id) 
-            ==> provided_spec.entails(always(lift_state(#[trigger] vd_rely(other_id)))))
-        implies 
-        cluster.controller_models.remove(controller_id).contains_key(other_id) 
-            ==> spec.entails(always(lift_state(#[trigger] vd_rely(other_id)))) by {
+    assert forall |other_id| cluster.controller_models.remove(controller_id).contains_key(other_id) 
+        && provided_spec.entails(always(lift_state(#[trigger] vd_rely(other_id))))
+        implies spec.entails(always(lift_state(#[trigger] vd_rely(other_id)))) by {
         if cluster.controller_models.remove(controller_id).contains_key(other_id) {
             assert(provided_spec.entails(always(lift_state(vd_rely(other_id)))));
             entails_and_different_temp(
