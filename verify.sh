127d28533fe8b521d7b732dcb84985887f0e0ef0
Nikhil Date <nikhil.s.date@gmail.com>
2025-11-06 00:14:11 -0600

    simplify logic in partition_pods

--- verify.sh
+++
@@ -1,9 +0,0 @@
-/home/ndate/Research/verus/source/target-verus/release/verus \
-    -L dependency=src/deps_hack/target/debug/deps \
-    --extern=deps_hack="src/deps_hack/target/debug/libdeps_hack.rlib" \
-    src/vstatefulset_controller.rs \
-    -Z macro-backtrace \
-    --trace \
-    --multiple-errors 100 \
-    --verify-only-module vstatefulset_controller::exec::reconciler \
-    --verify-function filter_pods
