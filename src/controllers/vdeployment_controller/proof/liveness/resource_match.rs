7fa2a88fb726278c27076aa8f17731b7413d8fc1
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-11-06 16:23:43 -0600

    add rlimit for lemma_from_old_vrs_len_zero_after_ensure_new_vrs_to_current_state_matches

--- src/controllers/vdeployment_controller/proof/liveness/resource_match.rs
+++ src/controllers/vdeployment_controller/proof/liveness/resource_match.rs
@@ -1868,7 +1868,8 @@ ensures
     );
 }
 
-#[verifier(rlimit(10))]
+#[verifier(rlimit(50))]
+#[verifier(spinoff_prover)]
 pub proof fn lemma_from_old_vrs_len_zero_after_ensure_new_vrs_to_current_state_matches(
     vd: VDeploymentView, spec: TempPred<ClusterState>, cluster: Cluster, controller_id: int, nv_uid_key: (Uid, ObjectRef)
 )
