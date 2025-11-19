f7512ac756e84f897c37349cfae33c49af8860d3
Nikhil Date <nikhil.s.date@gmail.com>
2025-11-19 16:37:52 -0600

    attempt generic filter

--- src/vstd_ext/vec_lib.rs
+++ src/vstd_ext/vec_lib.rs
@@ -1,5 +1,6 @@
 use vstd::prelude::*;
 use vstd::seq_lib::*;
+use super::seq_lib::lemma_filter_push;
 
 verus! {
 
@@ -8,33 +9,40 @@ trait VerusClone: View + Sized {
         ensures self == r;
 }
 
-fn vec_filter<V: VerusClone + View + Sized>(v: Vec<V>, f: impl Fn(&V)->bool, f_spec: spec_fn(V)->bool) -> (r: Vec<V>)
+fn vec_filter<V: VerusClone + View + Sized>(v: Vec<V>, f: impl Fn(&V) -> bool, f_spec: spec_fn(V) -> bool) -> (r: Vec<V>)
     requires
-        forall|v: V| #[trigger] f.requires((&v,)),
-        forall |v: V, r: bool| f.ensures((&v,), r) ==> f_spec(v) == r,
-    ensures r@.to_multiset() =~= v@.to_multiset().filter(f_spec)
+        forall |v: V| #[trigger] f.requires((&v,)),
+        forall |v: V, r: bool| f.ensures((&v,), r) <==> f_spec(v) == r // this says that f and f_spec are in conformance,
+    ensures r@ =~= v@.filter(f_spec)
 {
     let mut r = Vec::new();
     let mut i = 0;
-    broadcast use group_seq_properties;
     for i in 0..v.len()
         invariant
-            forall|v: V| #[trigger] f.requires((&v,)),
-            i <= v.len(),
-            r@.to_multiset() =~= v@.subrange(0, i as int).to_multiset().filter(f_spec),
-            forall |v:V,r:bool| f.ensures((&v,), r) ==> f_spec(v) == r,
+            forall |v: V| #[trigger] f.requires((&v,)),
+            r@ =~= v@.take(i as int).filter(f_spec),
+            forall |v: V, r: bool| f.ensures((&v,), r) <==> f_spec(v) == r,
     {
-        // This deprecated lemma_seq_properties cannot be replaced by
-        // broadcast use group_seq_properties;
-        proof { lemma_seq_properties::<V>(); }
-        let ghost pre_r = r@.to_multiset();
-        assert(
-            v@.subrange(0, i as int + 1)
-            =~=
-            v@.subrange(0, i as int).push(v@[i as int]));
-        if f(&v[i]) {
-            r.push(v[i].verus_clone());
+
+        let elem = &v[i];
+        let take = f(elem);
+        if take {
+            r.push(elem.verus_clone());
         }
+        proof {
+            assert(f_spec(*elem) == take);
+            let r_old = if take {
+                r@.drop_last()
+            } else {
+                r@
+            };
+            assert(r_old == v@.take(i as int).filter(f_spec));
+            lemma_filter_push(v@.take(i as int), f_spec, *elem);
+            assert(v@.take(i + 1 as int) == v@.take(i as int).push(*elem));
+        }
+    }
+    proof {
+        assert(v@.take(v.len() as int) == v@);
     }
     r
 }
