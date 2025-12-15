8e3f238feaecb0ad74fd690884b4a3a1e573c311
Nikhil Date <nikhil.s.date@gmail.com>
2025-12-14 19:21:57 -0600

    add vec filter back

---
+++ src/vstd_ext/vec_lib.rs
@@ -0,0 +1,40 @@
+use vstd::prelude::*;
+use vstd::seq_lib::*;
+
+verus! {
+
+trait VerusClone: View + Sized {
+    fn verus_clone(&self) -> (r: Self)
+        ensures self == r;
+}
+
+fn vec_filter<V: VerusClone + View + Sized>(v: Vec<V>, f: impl Fn(&V)->bool, f_spec: spec_fn(V)->bool) -> (r: Vec<V>)
+    requires
+        forall|v: V| #[trigger] f.requires((&v,)),
+        forall |v:V,r:bool| f.ensures((&v,), r) ==> f_spec(v) == r,
+    ensures r@.to_multiset() =~= v@.to_multiset().filter(f_spec)
+{
+    let mut r = Vec::new();
+    let mut i = 0;
+    broadcast use group_seq_properties;
+    for i in 0..v.len()
+        invariant
+            forall|v: V| #[trigger] f.requires((&v,)),
+            i <= v.len(),
+            r@.to_multiset() =~= v@.subrange(0, i as int).to_multiset().filter(f_spec),
+            forall |v:V,r:bool| f.ensures((&v,), r) ==> f_spec(v) == r,
+    {
+        let ghost pre_r = r@.to_multiset();
+        proof {
+            broadcast use group_to_multiset_ensures;
+        }
+        assert(v@.subrange(0, i as int + 1) =~= v@.subrange(0, i as int).push(v@[i as int]));
+        if f(&v[i]) {
+            r.push(v[i].verus_clone());
+        }
+    }
+    assert(v@.subrange(0, v.len() as int) =~= v@);
+    r
+}
+
+}
