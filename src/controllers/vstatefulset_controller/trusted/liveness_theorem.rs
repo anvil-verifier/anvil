22028712c851ff02c03d23f8c020e566182550e2
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-12-25 16:55:10 -0600

    simplify owned_dyn_pods_filter

--- src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
+++ src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
@@ -28,8 +28,10 @@ pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<Clus
             // 2. Bound PVCs exist
             &&& forall |i: int| #![trigger vsts.spec.volume_claim_templates->0[i]] 0 <= i < pvc_template_cnt ==> {
                 let pvc_template_name = vsts.spec.volume_claim_templates->0[i].metadata.name->0;
-                &&& exists |obj| #[trigger] s.resources().contains_value(obj)
-                &&& bound_dyn_pvc_filter(pvc_template_name, vsts, ord)(obj)
+                &&& exists |obj| {
+                    &&& s.resources().contains_value(obj)
+                    &&& #[trigger] bound_dyn_pvc_filter(pvc_template_name, vsts, ord)(obj)
+                }
             }
         }
         // 3. No condemned pod in etcd
@@ -40,6 +42,7 @@ pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<Clus
 }
 
 // filter_pods on dyn with additional filter on namespace
+// without naming requirements, because itself alone is already guarded by rely-conditions
 pub open spec fn owned_dyn_pods_filter(vsts: VStatefulSetView) -> spec_fn(DynamicObjectView) -> bool {
     |obj: DynamicObjectView| {
         &&& obj.kind == PodView::kind()
@@ -48,7 +51,6 @@ pub open spec fn owned_dyn_pods_filter(vsts: VStatefulSetView) -> spec_fn(Dynami
         &&& obj.metadata.namespace == vsts.metadata.namespace
         &&& obj.metadata.owner_references_contains(vsts.controller_owner_ref())
         &&& vsts.spec.selector.matches(obj.metadata.labels.unwrap_or(Map::empty()))
-        &&& exists |ord| obj.metadata.name->0 == #[trigger] pod_name(vsts.metadata.name->0, ord)
     }
 }
 
