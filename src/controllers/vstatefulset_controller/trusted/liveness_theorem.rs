6b50c13547473da0a87868e4292a78a1865671f0
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-12-25 16:44:04 -0600

    finish esr

--- src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
+++ src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
@@ -13,16 +13,33 @@ verus !{
 
 pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<ClusterState> {
     |s: ClusterState| {
-        // 1. managed pods exist
-        let owned_objs = s.resources().values().filter(owned_selector_match_is(vsts));
-        &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] 0 <= ord < vsts.spec.replicas.unwrap_or(1) ==>
-            exists |obj| #[trigger] owned_objs.contains(obj) && obj.metadata.name->0 == pod_name(vsts.metadata.name->0, ord)
-        // 2. managed PVCs exist
+        let owned_dyn_pods = s.resources().values().filter(owned_dyn_pods_filter(vsts));
+        let pvc_template_cnt = if vsts.spec.volume_claim_templates is Some {
+            vsts.spec.volume_claim_templates->0.len()
+        } else {0};
+        // 1. All needed pods exist and are up-to-date
+        &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] 0 <= ord < vsts.spec.replicas.unwrap_or(1) ==> {
+            &&& exists |obj| {
+                &&& #[trigger] owned_dyn_pods.contains(obj)
+                &&& obj.metadata.name->0 == pod_name(vsts.metadata.name->0, ord)
+                &&& PodView::unmarshal_spec(obj.spec) is Ok
+                &&& PodView::unmarshal_spec(obj.spec)->Ok_0 == vsts.spec.template.spec
+            }
+            // 2. Bound PVCs exist
+            &&& forall |i: int| #![trigger vsts.spec.volume_claim_templates->0[i]] 0 <= i < pvc_template_cnt ==> {
+                let pvc_template_name = vsts.spec.volume_claim_templates->0[i].metadata.name->0;
+                &&& exists |obj| #[trigger] s.resources().contains_value(obj) && bound_dyn_pvc_filter(pvc_template_name, vsts, ord)(obj)
+            }
+        }
+        // 3. No condemned pod in etcd
+        &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] ord >= vsts.spec.replicas.unwrap_or(1) ==> {
+            !exists |obj| #[trigger] owned_dyn_pods.contains(obj) && obj.metadata.name->0 == pod_name(vsts.metadata.name->0, ord)
+        }
     }
 }
 
 // filter_pods on dyn with additional filter on namespace
-pub open spec fn owned_selector_match_is(vsts: VStatefulSetView) -> spec_fn(DynamicObjectView) -> bool {
+pub open spec fn owned_dyn_pods_filter(vsts: VStatefulSetView) -> spec_fn(DynamicObjectView) -> bool {
     |obj: DynamicObjectView| {
         &&& obj.kind == PodView::kind()
         &&& obj.metadata.name is Some
@@ -31,6 +48,16 @@ pub open spec fn owned_selector_match_is(vsts: VStatefulSetView) -> spec_fn(Dyna
         &&& obj.metadata.owner_references_contains(vsts.controller_owner_ref())
         &&& vsts.spec.selector.matches(obj.metadata.labels.unwrap_or(Map::empty()))
         &&& exists |ord| obj.metadata.name->0 == #[trigger] pod_name(vsts.metadata.name->0, ord)
+    }
+}
+
+pub open spec fn bound_dyn_pvc_filter(pvc_template_name: StringView, vsts: VStatefulSetView, ord: nat) -> spec_fn(DynamicObjectView) -> bool {
+    |obj: DynamicObjectView| {
+        &&& obj.kind == PersistentVolumeClaimView::kind()
+        &&& obj.metadata.name is Some
+        &&& obj.metadata.namespace is Some
+        &&& obj.metadata.namespace == vsts.metadata.namespace
+        &&& obj.metadata.name->0 == pvc_name(pvc_template_name, vsts.metadata.name->0, ord)
     }
 }
 
