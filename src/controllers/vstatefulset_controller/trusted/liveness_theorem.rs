8a2a75cd2c56d79c2a83100888acbce13804d81a
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-12-25 21:24:30 -0600

    simplify esr

--- src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
+++ src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
@@ -13,54 +13,40 @@ verus !{
 
 pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<ClusterState> {
     |s: ClusterState| {
-        let owned_dyn_pods = s.resources().values().filter(owned_dyn_pods_filter(vsts));
         let pvc_template_cnt = if vsts.spec.volume_claim_templates is Some {
             vsts.spec.volume_claim_templates->0.len()
         } else {0};
         // 1. All needed pods exist and are up-to-date
         &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] 0 <= ord < vsts.spec.replicas.unwrap_or(1) ==> {
-            &&& exists |obj| {
-                &&& #[trigger] owned_dyn_pods.contains(obj)
-                &&& obj.metadata.name->0 == pod_name(vsts.metadata.name->0, ord)
-                &&& PodView::unmarshal_spec(obj.spec) is Ok
-                &&& PodView::unmarshal_spec(obj.spec)->Ok_0 == vsts.spec.template.spec
-            }
+            let key = ObjectRef {
+                kind: PodView::kind(),
+                name: pod_name(vsts.metadata.name->0, ord),
+                namespace: vsts.metadata.namespace->0
+            };
+            let obj = s.resources()[key];
+            &&& s.resources().contains_key(key)
+            // spec is updated
+            &&& PodView::unmarshal_spec(obj.spec) is Ok
+            &&& PodView::unmarshal_spec(obj.spec)->Ok_0 == vsts.spec.template.spec
             // 2. Bound PVCs exist
             &&& forall |i: int| #![trigger vsts.spec.volume_claim_templates->0[i]] 0 <= i < pvc_template_cnt ==> {
-                let pvc_template_name = vsts.spec.volume_claim_templates->0[i].metadata.name->0;
-                &&& exists |obj| {
-                    &&& s.resources().contains_value(obj)
-                    &&& #[trigger] bound_dyn_pvc_filter(pvc_template_name, vsts, ord)(obj)
-                }
+                let pvc_key = ObjectRef {
+                    kind: PersistentVolumeClaimView::kind(),
+                    name: vsts.spec.volume_claim_templates->0[i].metadata.name->0,
+                    namespace: vsts.metadata.namespace->0
+                };
+                &&& s.resources().contains_key(pvc_key)
             }
         }
         // 3. No condemned pod in etcd
-        &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] ord >= vsts.spec.replicas.unwrap_or(1) ==> {
-            !exists |obj| #[trigger] owned_dyn_pods.contains(obj) && obj.metadata.name->0 == pod_name(vsts.metadata.name->0, ord)
+        &&& !exists |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] ord >= vsts.spec.replicas.unwrap_or(1) ==> {
+            let key = ObjectRef {
+                kind: PodView::kind(),
+                name: pod_name(vsts.metadata.name->0, ord),
+                namespace: vsts.metadata.namespace->0
+            };
+            &&& s.resources().contains_key(key)
         }
-    }
-}
-
-// filter_pods on dyn with additional filter on namespace
-// without naming requirements, because itself alone is already guarded by rely-conditions
-pub open spec fn owned_dyn_pods_filter(vsts: VStatefulSetView) -> spec_fn(DynamicObjectView) -> bool {
-    |obj: DynamicObjectView| {
-        &&& obj.kind == PodView::kind()
-        &&& obj.metadata.name is Some
-        &&& obj.metadata.namespace is Some
-        &&& obj.metadata.namespace == vsts.metadata.namespace
-        &&& obj.metadata.owner_references_contains(vsts.controller_owner_ref())
-        &&& vsts.spec.selector.matches(obj.metadata.labels.unwrap_or(Map::empty()))
-    }
-}
-
-pub open spec fn bound_dyn_pvc_filter(pvc_template_name: StringView, vsts: VStatefulSetView, ord: nat) -> spec_fn(DynamicObjectView) -> bool {
-    |obj: DynamicObjectView| {
-        &&& obj.kind == PersistentVolumeClaimView::kind()
-        &&& obj.metadata.name is Some
-        &&& obj.metadata.namespace is Some
-        &&& obj.metadata.namespace == vsts.metadata.namespace
-        &&& obj.metadata.name->0 == pvc_name(pvc_template_name, vsts.metadata.name->0, ord)
     }
 }
 
