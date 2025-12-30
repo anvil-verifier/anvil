1ad8c47839b2da28a4c36ac58ff7f438f8f3ac1f
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-12-29 18:25:41 -0600

    add drafts of lemma_no_interference

--- src/controllers/vstatefulset_controller/proof/shield_lemma.rs
+++ src/controllers/vstatefulset_controller/proof/shield_lemma.rs
@@ -18,37 +18,35 @@ verus! {
 
 // In addition to rely conditions, we also need to prove vsts controller's internal guarantee:
 // all requests sent from one reconciliation do not interfere with other reconciliations on different CRs.
-pub open spec fn no_interfering_request_between_vsts(controller_id: int, vsts_key: ObjectRef) -> StatePred<ClusterState> {
+pub open spec fn no_interfering_request_between_vsts(controller_id: int, vsts: VStatefulSetView) -> StatePred<ClusterState> {
     |s: ClusterState| {
-        let vsts = VStatefulSetView::unmarshal(s.ongoing_reconciles(controller_id)[vsts_key].triggering_cr).unwrap();
         forall |msg| {
             &&& #[trigger] s.in_flight().contains(msg) 
             &&& msg.content is APIRequest
-            &&& msg.src == HostId::Controller(controller_id, vsts_key)
-            &&& s.ongoing_reconciles(controller_id).contains_key(vsts_key)
+            &&& msg.src == HostId::Controller(controller_id, vsts.object_ref())
         } ==> match msg.content->APIRequest_0 {
             APIRequest::ListRequest(_) | APIRequest::GetRequest(_) => true, // read-only requests
             APIRequest::CreateRequest(req) => {
-                &&& req.namespace == vsts_key.namespace
+                &&& req.namespace == vsts.object_ref().namespace
                 &&& req.obj.kind == Kind::PodKind ==> {
                     &&& req.obj.metadata.owner_references == Some(Seq::empty().push(vsts.controller_owner_ref()))
-                    &&& exists |ord: nat| req.obj.metadata.name == Some(#[trigger] pod_name(vsts_key.name, ord))
+                    &&& exists |ord: nat| req.obj.metadata.name == Some(#[trigger] pod_name(vsts.object_ref().name, ord))
                 }
                 &&& req.obj.kind == Kind::PersistentVolumeClaimKind ==> {
                     &&& exists |i: (int, nat)| // PVC template index, ordinal
-                        req.obj.metadata.name == Some(#[trigger] pvc_name(vsts.spec.volume_claim_templates->0[i.0].metadata.name->0, vsts_key.name, i.1))
+                        req.obj.metadata.name == Some(#[trigger] pvc_name(vsts.spec.volume_claim_templates->0[i.0].metadata.name->0, vsts.object_ref().name, i.1))
                 }
             },
             APIRequest::GetThenDeleteRequest(req) => {
-                &&& req.key().namespace == vsts_key.namespace
+                &&& req.key().namespace == vsts.object_ref().namespace
                 &&& req.key().kind == Kind::PodKind
-                &&& exists |ord: nat| req.key().name == #[trigger] pod_name(vsts_key.name, ord)
+                &&& exists |ord: nat| req.key().name == #[trigger] pod_name(vsts.object_ref().name, ord)
                 &&& req.owner_ref == vsts.controller_owner_ref()
             },
             APIRequest::GetThenUpdateRequest(req) => {
-                &&& req.namespace == vsts_key.namespace
+                &&& req.namespace == vsts.object_ref().namespace
                 &&& req.obj.kind == Kind::PodKind
-                &&& exists |ord: nat| req.name == #[trigger] pod_name(vsts_key.name, ord)
+                &&& exists |ord: nat| req.name == #[trigger] pod_name(vsts.object_ref().name, ord)
                 &&& req.owner_ref == vsts.controller_owner_ref()
             },
             // VSTS controller will not issue DeleteRequest, UpdateRequest
@@ -56,6 +54,8 @@ pub open spec fn no_interfering_request_between_vsts(controller_id: int, vsts_ke
         }
     }
 }
+
+uninterp spec fn make_vsts() -> VStatefulSetView;
 
 // shield lemma
 pub proof fn lemma_no_interference(
@@ -76,7 +76,7 @@ requires
     Cluster::pending_req_of_key_is_unique_with_unique_id(controller_id, vsts.object_ref())(s),
     Cluster::there_is_the_controller_state(controller_id)(s),
     Cluster::every_msg_from_key_is_pending_req_msg_of(controller_id, vsts.object_ref())(s),
-    forall |other_key| no_interfering_request_between_vsts(controller_id, other_key)(s),
+    forall |other_vsts| no_interfering_request_between_vsts(controller_id, other_vsts)(s),
     forall |other_id| vsts_rely(other_id)(s),
     msg.src != HostId::Controller(controller_id, vsts.object_ref()),
 ensures
@@ -125,6 +125,103 @@ ensures
         &&& s.resources()[k] == s_prime.resources()[k]
     },
 {
+    assert forall |k: ObjectRef| { // ==>
+        let obj = s.resources()[k];
+        &&& k.kind == Kind::PodKind
+        &&& #[trigger] s.resources().contains_key(k)
+        &&& obj.metadata.namespace == vsts.metadata.namespace
+        &&& obj.metadata.owner_references is Some
+        &&& obj.metadata.owner_references->0.filter(controller_owner_filter()) == seq![vsts.controller_owner_ref()]
+    } implies {
+        &&& s_prime.resources().contains_key(k)
+        &&& s.resources()[k] == s_prime.resources()[k]
+    } by {
+        let post = s_prime.resources().contains_key(k) && s_prime.resources()[k] == s.resources()[k];
+        let obj = s.resources()[k];
+        PodView::marshal_preserves_integrity();
+        assert(obj.metadata.owner_references->0.contains(vsts.controller_owner_ref())) by {
+            broadcast use group_seq_properties;
+            seq_filter_contains_implies_seq_contains(obj.metadata.owner_references->0, controller_owner_filter(), vsts.controller_owner_ref());
+        }
+        if msg.content is APIRequest && msg.dst is APIServer {
+            match msg.src {
+                HostId::Controller(id, cr_key) => {
+                    if id == controller_id {
+                        assert(cr_key != vsts.object_ref());
+                        // same controller, other vsts
+                        // every_msg_from_vd_controller_carries_vd_key
+                        let cr_key = msg.src->Controller_1;
+                        let other_vsts = VStatefulSetView {
+                            metadata: ObjectMetaView {
+                                name: Some(cr_key.name),
+                                namespace: Some(cr_key.namespace),
+                                ..make_vsts().metadata
+                            },
+                            ..make_vsts()
+                        };
+                        // so msg can only be list, create or get_then_update
+                        assert(no_interfering_request_between_vsts(controller_id, other_vsts)(s));
+                        match msg.content->APIRequest_0 {
+                            APIRequest::DeleteRequest(req) => assert(false), // vsts controller doesn't send delete req
+                            APIRequest::GetThenDeleteRequest(req) => admit(),
+                            APIRequest::GetThenUpdateRequest(req) => {
+                                // controller_owner_ref does not carry namespace, while object_ref does
+                                // so object_ref != is not enough to prove controller_owner_ref !=
+                                if cr_key.namespace == vsts.metadata.namespace->0 {
+                                    assert(!obj.metadata.owner_references_contains(req.owner_ref)) by {
+                                        if obj.metadata.owner_references_contains(req.owner_ref) {
+                                            assert(req.owner_ref != vsts.controller_owner_ref());
+                                            assert(obj.metadata.owner_references->0.filter(controller_owner_filter()).contains(req.owner_ref));
+                                        }
+                                    }
+                                } // or else, namespace is different, so should not be touched at all
+                            },
+                            _ => {},
+                        }
+                        assert(post);
+                    } else {
+                        let other_id = msg.src->Controller_0;
+                        // by every_in_flight_req_msg_from_controller_has_valid_controller_id, used by vd_rely
+                        assert(cluster.controller_models.contains_key(other_id));
+                        assert(vsts_rely(other_id)(s)); // trigger vd_rely_condition
+                        VStatefulSetReconcileState::marshal_preserves_integrity();
+                        match msg.content->APIRequest_0 {
+                            APIRequest::DeleteRequest(req) => {},
+                            APIRequest::GetThenDeleteRequest(req) => {
+                                if req.key.kind == Kind::PodKind {
+                                    assert(!obj.metadata.owner_references_contains(req.owner_ref)) by {
+                                        if obj.metadata.owner_references_contains(req.owner_ref) {
+                                            assert(req.owner_ref != vsts.controller_owner_ref());
+                                            assert(obj.metadata.owner_references->0.filter(controller_owner_filter()).contains(req.owner_ref));
+                                        }
+                                    }
+                                }
+                            },
+                            APIRequest::GetThenUpdateRequest(req) => {
+                                if req.obj.kind == Kind::PodKind {
+                                    // rely condition
+                                    assert({
+                                        &&& req.owner_ref.controller is Some
+                                        &&& req.owner_ref.controller->0
+                                        &&& req.owner_ref.kind != VStatefulSetView::kind()
+                                    });
+                                    assert(!obj.metadata.owner_references_contains(req.owner_ref)) by {
+                                        if obj.metadata.owner_references_contains(req.owner_ref) {
+                                            assert(req.owner_ref != vsts.controller_owner_ref());
+                                            assert(obj.metadata.owner_references->0.filter(controller_owner_filter()).contains(req.owner_ref));
+                                        }
+                                    }
+                                }
+                            },
+                            _ => {},
+                        }
+                        assert(post);
+                    }
+                },
+                _ => {},
+            }
+        }
+    }
     admit();
 }
     
