3b2bc9526aec45e1c9027883f8aeb5e235662079
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-01-06 14:51:09 -0600

    stage current progress and move to conformance test first

--- src/controllers/vstatefulset_controller/proof/liveness/state_predicates.rs
+++ src/controllers/vstatefulset_controller/proof/liveness/state_predicates.rs
@@ -28,4 +28,56 @@ pub open spec fn no_pending_req_in_cluster(vsts_key: ObjectRef, controller_id: i
     }
 }
 
+pub open spec fn req_msg_is_list_vrs_req(
+    vsts_key: ObjectRef, controller_id: int, msg: Message, s: ClusterState
+) -> bool {
+    let req = req_msg.content->APIRequest_0;
+    &&& req_msg.src == HostId::Controller(controller_id, vsts_key)
+    &&& req_msg.dst == HostId::APIServer
+    &&& req_msg.content is APIRequest
+    &&& req is ListRequest
+    &&& req->ListRequest_0 == ListRequest {
+        kind: Kind::PodKind,
+        namespace: vsts_key.namespace,
+    }
+}
+
+pub open spec fn pending_list_req_in_flight(
+    vsts_key: ObjectRef, controller_id: int
+) -> StatePred<ClusterState> {
+    |s: ClusterState| {
+        let req_msg = s.ongoing_reconciles(controller_id)[vsts_key].pending_req_msg->0;
+        &&& Cluster::pending_req_msg_is(controller_id, s, vsts_key, req_msg)
+        &&& s.in_flight().contains(req_msg)
+        &&& req_msg_is_list_vrs_req(vsts_key, controller_id, req_msg, s)
+    }
+}
+
+pub open spec fn req_msg_is_pending_list_req_in_flight(
+    vsts_key: ObjectRef, controller_id: int, req_msg: Message
+) -> StatePred<ClusterState> {
+    |s: ClusterState| {
+        &&& Cluster::pending_req_msg_is(controller_id, s, vsts_key, req_msg)
+        &&& s.in_flight().contains(req_msg)
+        &&& req_msg_is_list_vrs_req(vsts_key, controller_id, req_msg, s)
+    }
+}
+
+pub open spec fn exists_pending_list_resp_in_flight_and_match_req(
+    vsts_key: ObjectRef, controller_id: int
+) -> StatePred<ClusterState> {
+    |s: ClusterState| {
+        let req_msg = s.ongoing_reconciles(controller_id)[vsts_key].pending_req_msg->0;
+        // predicate on req_msg, it's not in_flight
+        &&& Cluster::pending_req_msg_is(controller_id, s, vsts_key, req_msg)
+        &&& req_msg_is_list_vrs_req(vsts_key, controller_id, req_msg, s)
+        // predicate on resp_msg
+        &&& exists |resp_msg| {
+            &&& #[trigger] s.in_flight().contains(resp_msg)
+            &&& resp_msg_matches_req_msg(resp_msg, req_msg)
+            // &&& resp_msg_is_ok_list_resp_containing_matched_vrs(vsts_key, controller_id, resp_msg, s)
+        }
+    }
+}
+
 }
