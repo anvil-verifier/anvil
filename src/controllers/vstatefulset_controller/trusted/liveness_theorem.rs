7bdae13dde0da27ff8cb35b3f364779718d9a6af
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-01-08 14:57:57 -0600

    draft state predicates

--- src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
+++ src/controllers/vstatefulset_controller/trusted/liveness_theorem.rs
@@ -17,6 +17,7 @@ pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<Clus
             vsts.spec.volume_claim_templates->0.len()
         } else {0};
         // 1. All needed pods exist and are up-to-date
+        // TODO: cover updates to pod.spec.{volumes|hostname|subdomain} and pod.metadata.labels
         &&& forall |ord: nat| #![trigger pod_name(vsts.metadata.name->0, ord)] 0 <= ord < vsts.spec.replicas.unwrap_or(1) ==> {
             let key = ObjectRef {
                 kind: PodView::kind(),
@@ -25,10 +26,11 @@ pub open spec fn current_state_matches(vsts: VStatefulSetView) -> StatePred<Clus
             };
             let obj = s.resources()[key];
             &&& s.resources().contains_key(key)
+            &&& obj.metadata.owner_references_contains(vsts.controller_owner_ref())
             // spec is updated
             &&& PodView::unmarshal_spec(obj.spec) is Ok
             &&& PodView::unmarshal_spec(obj.spec)->Ok_0 == vsts.spec.template.spec
-            &&& obj.metadata.owner_references_contains(vsts.controller_owner_ref())
+            // labels are updated
             // note: this can be easily proved with obj.metadata->0.labels == vsts.spec.template.metadata->0.labels
             &&& vsts.spec.selector.matches(obj.metadata.labels.unwrap_or(Map::empty()))
             // 2. Bound PVCs exist
