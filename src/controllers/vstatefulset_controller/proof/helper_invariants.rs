34a74de18e81ffd51558a1b77b1eaa2e48294ffa
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-01-09 15:14:09 -0600

    add all_pods_in_etcd_matching_vsts_have_correct_owner_ref_and_labels

--- src/controllers/vstatefulset_controller/proof/helper_invariants.rs
+++ src/controllers/vstatefulset_controller/proof/helper_invariants.rs
@@ -22,7 +22,23 @@ use vstd::prelude::*;
 
 verus! {
 
-pub open spec fn pod_name_collision_is_impossible(vsts: VStatefulSetView, pod: PodView) -> bool {
-    true
+// name collision prevention invariant, eventually holds
+// In the corner case when one vsts was created and then deleted, just before
+// another vsts with the same name comes, GC will delete pods owned by the previous vsts
+pub open spec fn all_pods_in_etcd_matching_vsts_have_correct_owner_ref_and_labels(vsts: VStatefulSetView) -> StatePred<ClusterState> {
+    |s: ClusterState| {
+        forall |pod_key: ObjectRef| {
+            &&& #[trigger] s.resources().contains_key(pod_key)
+            &&& pod_key.kind == Kind::PodKind
+            &&& pod_key.namespace == vsts.metadata.namespace->0
+            &&& vsts.metadata.name is Some
+            &&& vsts.metadata.namespace is Some
+            &&& get_ordinal(vsts.metadata.name->0, pod_key.name) is Some
+        } ==> {
+            let pod_obj = s.resources()[pod_key];
+            &&& pod_obj.metadata.owner_references_contains(vsts.controller_owner_ref())
+            &&& vsts.spec.selector.matches(pod_obj.metadata.labels.unwrap_or(Map::empty()))
+        }
+    }
 }
 }
