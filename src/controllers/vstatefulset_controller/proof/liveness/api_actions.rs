41523a5d1df98fc35fc82bcb99a0fd04f0fb5e8b
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-01-12 17:46:51 -0600

    speedrun lemma_from_after_send_get_pvc_req_to_receive_get_pvc_resp

--- src/controllers/vstatefulset_controller/proof/liveness/api_actions.rs
+++ src/controllers/vstatefulset_controller/proof/liveness/api_actions.rs
@@ -18,6 +18,7 @@ use crate::vstd_ext::{seq_lib::*, set_lib::*, map_lib::*, string_view::int_to_st
 
 verus! {
 
+// TODO: if req does not need to be exposed, remove it from input and output
 #[verifier(external_body)]
 pub proof fn lemma_list_pod_request_returns_ok_with_objs_matching_vsts(
     s: ClusterState, s_prime: ClusterState, vsts: VStatefulSetView, cluster: Cluster, controller_id: int, req_msg: Message,
@@ -34,4 +35,18 @@ ensures
 {
     return handle_create_request_msg(cluster.installed_types, req_msg, s.api_server).1;
 }
+
+#[verifier(external_body)]
+pub proof fn lemma_get_pvc_request_returns_ok_or_err_response(
+    s: ClusterState, s_prime: ClusterState, vsts: VStatefulSetView, cluster: Cluster, controller_id: int,
+)
+requires
+    req_msg_or_none(s, vsts, controller_id) is Some,
+    cluster.type_is_installed_in_cluster::<VStatefulSetView>(),
+    cluster.next_step(s, s_prime, Step::APIServerStep(req_msg_or_none(s, vsts, controller_id))),
+    pending_get_pvc_req_in_flight(vsts, controller_id)(s),
+    cluster_invariants_since_reconciliation(cluster, vsts, controller_id)(s),
+ensures
+    pending_get_pvc_resp_msg_in_flight(vsts, controller_id)(s_prime),
+{}
 }
