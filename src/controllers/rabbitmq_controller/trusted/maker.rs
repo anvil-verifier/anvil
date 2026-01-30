1360d4e397b022041a96bff1f5b8fa71e6aed119
Nikhil Date <nikhil.s.date@gmail.com>
2026-01-29 18:34:16 -0600

    model compiles

--- src/controllers/rabbitmq_controller/trusted/maker.rs
+++ src/controllers/rabbitmq_controller/trusted/maker.rs
@@ -10,27 +10,27 @@ use vstd::prelude::*;
 verus! {
 
 pub trait Maker {
-    uninterp spec fn make_headless_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_main_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_erlang_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_default_user_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_plugins_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_server_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_service_account_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_role_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_role_binding_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
-    uninterp spec fn make_stateful_set_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_headless_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_main_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_erlang_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_default_user_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_plugins_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_server_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_service_account_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_role_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_role_binding_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_stateful_set_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
 
-    uninterp spec fn make_headless_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
-    uninterp spec fn make_main_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
-    uninterp spec fn make_erlang_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
-    uninterp spec fn make_default_user_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
-    uninterp spec fn make_plugins_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
-    uninterp spec fn make_server_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
-    uninterp spec fn make_service_account(rabbitmq: RabbitmqClusterView) -> ServiceAccountView;
-    uninterp spec fn make_role(rabbitmq: RabbitmqClusterView) -> RoleView;
-    uninterp spec fn make_role_binding(rabbitmq: RabbitmqClusterView) -> RoleBindingView;
-    uninterp spec fn make_stateful_set(rabbitmq: RabbitmqClusterView, config_map_rv: StringView) -> StatefulSetView;
+    spec fn make_headless_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
+    spec fn make_main_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
+    spec fn make_erlang_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
+    spec fn make_default_user_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
+    spec fn make_plugins_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
+    spec fn make_server_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
+    spec fn make_service_account(rabbitmq: RabbitmqClusterView) -> ServiceAccountView;
+    spec fn make_role(rabbitmq: RabbitmqClusterView) -> RoleView;
+    spec fn make_role_binding(rabbitmq: RabbitmqClusterView) -> RoleBindingView;
+    spec fn make_stateful_set(rabbitmq: RabbitmqClusterView, config_map_rv: StringView) -> StatefulSetView;
 }
 
 }
