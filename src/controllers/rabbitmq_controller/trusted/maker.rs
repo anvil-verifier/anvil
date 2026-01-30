421ae2696522ca73a835cbdd7a6b9bed2924dd08
Nikhil Date <nikhil.s.date@gmail.com>
2026-01-29 18:36:48 -0600

    add maker back in

---
+++ src/controllers/rabbitmq_controller/trusted/maker.rs
@@ -0,0 +1,36 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: MIT
+use crate::kubernetes_api_objects::error::UnmarshalError;
+use crate::kubernetes_api_objects::spec::prelude::*;
+use crate::rabbitmq_controller::trusted::spec_types::RabbitmqClusterView;
+use crate::vstd_ext::{string_map::*, string_view::*};
+use deps_hack::kube::Resource;
+use vstd::prelude::*;
+
+verus! {
+
+pub trait Maker {
+    spec fn make_headless_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_main_service_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_erlang_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_default_user_secret_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_plugins_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_server_config_map_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_service_account_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_role_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_role_binding_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+    spec fn make_stateful_set_key(rabbitmq: RabbitmqClusterView) -> ObjectRef;
+
+    spec fn make_headless_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
+    spec fn make_main_service(rabbitmq: RabbitmqClusterView) -> ServiceView;
+    spec fn make_erlang_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
+    spec fn make_default_user_secret(rabbitmq: RabbitmqClusterView) -> SecretView;
+    spec fn make_plugins_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
+    spec fn make_server_config_map(rabbitmq: RabbitmqClusterView) -> ConfigMapView;
+    spec fn make_service_account(rabbitmq: RabbitmqClusterView) -> ServiceAccountView;
+    spec fn make_role(rabbitmq: RabbitmqClusterView) -> RoleView;
+    spec fn make_role_binding(rabbitmq: RabbitmqClusterView) -> RoleBindingView;
+    spec fn make_stateful_set(rabbitmq: RabbitmqClusterView, config_map_rv: StringView) -> StatefulSetView;
+}
+
+}
