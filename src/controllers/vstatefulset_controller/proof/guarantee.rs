504397347722d12be940efa8a7d50fd41c35ac0d
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-02-26 01:38:37 -0600

    simplify but strengthen guarantee

--- src/controllers/vstatefulset_controller/proof/guarantee.rs
+++ src/controllers/vstatefulset_controller/proof/guarantee.rs
@@ -39,31 +39,30 @@ pub open spec fn vsts_guarantee(controller_id: int) -> StatePred<ClusterState> {
 // and only creates PVC matching its PVC templates
 pub open spec fn vsts_guarantee_create_req(req: CreateRequest) -> bool {
     let owner_references = req.obj.metadata.owner_references->0;
-    &&& req.obj.kind == PodView::kind() ==> {
-        &&& req.obj.metadata.name is Some
-        &&& has_vsts_prefix(req.obj.metadata.name->0)
-        &&& req.obj.metadata.owner_references is Some
-        &&& exists |vsts: VStatefulSetView| #[trigger]
-            owner_references.contains(vsts.controller_owner_ref())
-    }
-    &&& req.obj.kind == PersistentVolumeClaimView::kind() ==> exists |vsts: VStatefulSetView|
-        #[trigger] pvc_name_match(req.obj.metadata.name->0, vsts.metadata.name->0)
+    &&& req.obj.metadata.name is Some
+    &&& has_vsts_prefix(req.obj.metadata.name->0)
+    &&& req.obj.kind == Kind::PodKind
+        ==> exists |vsts: VStatefulSetView| req.obj.metadata.owner_references == Some(Seq::empty().push(vsts.controller_owner_ref()))
+    &&& req.obj.kind == Kind::PersistentVolumeClaimKind
+        ==> req.obj.metadata.owner_references is None
 }
 
 // VSTS controller Only updates Pod owned by itself and does not update PVC
 pub open spec fn vsts_guarantee_get_then_update_req(req: GetThenUpdateRequest) -> bool {
-    &&& req.obj.kind == PodView::kind()
+    &&& req.obj.kind == Kind::PodKind
+    &&& req.obj.metadata.name is Some
     &&& has_vsts_prefix(req.obj.metadata.name->0)
     &&& exists |vsts: VStatefulSetView| {
         &&& req.owner_ref == #[trigger] vsts.controller_owner_ref()
         // do not change ownership
-        &&& req.obj.metadata.owner_references_contains(vsts.controller_owner_ref())
+        &&& req.obj.metadata.owner_references == Some(Seq::empty().push(req.owner_ref))
     }
 }
 
 // VSTS controller Only deletes Pod owned by itself
 pub open spec fn vsts_guarantee_get_then_delete_req(req: GetThenDeleteRequest) -> bool {
-    &&& req.key.kind == PodView::kind()
+    &&& req.key.kind == Kind::PodKind
+    &&& has_vsts_prefix(req.key.name)
     &&& exists |vsts: VStatefulSetView| req.owner_ref == #[trigger] vsts.controller_owner_ref()
 }
 
