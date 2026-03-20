06323e8db4b36186f185f59d9f7c30ac114b6068
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-03-20 13:51:28 -0500

    weaken current_state_matches_with_new_vrs_key

--- src/controllers/vdeployment_controller/trusted/liveness_theorem.rs
+++ src/controllers/vdeployment_controller/trusted/liveness_theorem.rs
@@ -67,7 +67,7 @@ pub open spec fn current_state_matches_with_new_vrs_key(vd: VDeploymentView, new
         &&& valid_owned_obj_key(vd, s)(new_vrs_key)
         &&& filter_new_vrs_keys(vd.spec.template, s)(new_vrs_key)
         &&& etcd_vrs.metadata.uid is Some
-        &&& (vd.spec.replicas.unwrap_or(1) > 0) == (etcd_vrs.spec.replicas.unwrap_or(1) > 0)
+        &&& vd.spec.replicas.unwrap_or(1) > 0 ==> etcd_vrs.spec.replicas.unwrap_or(1) > 0
         // no old vrs, including the 2nd new vrs (if any)
         &&& !exists |old_k: ObjectRef| {
             &&& #[trigger] s.resources().contains_key(old_k)
@@ -82,13 +82,10 @@ pub open spec fn inductive_current_state_matches(vd: VDeploymentView, controller
         let local_state = VDeploymentReconcileState::unmarshal(s.ongoing_reconciles(controller_id)[vd.object_ref()].local_state).unwrap();
         &&& current_state_matches_with_new_vrs_key(vd, new_vrs_key)(s)
         &&& s.ongoing_reconciles(controller_id).contains_key(vd.object_ref()) ==> {
-            &&& local_state.new_vrs is Some ==> {
-                if vd.spec.replicas.unwrap_or(1) > 0 {
-                    &&& local_state.new_vrs->0.object_ref() == new_vrs_key
-                    &&& local_state.new_vrs->0.spec.replicas.unwrap_or(1) > 0
-                } else {
-                    &&& local_state.new_vrs->0.spec.replicas.unwrap_or(1) == 0
-                }
+            // if vd has 0 replicas, local new vrs can have 0 replicas or not
+            &&& local_state.new_vrs is Some && vd.spec.replicas.unwrap_or(1) > 0 ==> {
+                &&& local_state.new_vrs->0.object_ref() == new_vrs_key
+                &&& local_state.new_vrs->0.spec.replicas.unwrap_or(1) > 0
             }
             &&& at_vd_step_with_vd(vd, controller_id, at_step_or![Init, AfterListVRS, AfterScaleNewVRS, AfterEnsureNewVRS, Done, Error])(s)
             &&& at_vd_step_with_vd(vd, controller_id, at_step![AfterEnsureNewVRS])(s)
