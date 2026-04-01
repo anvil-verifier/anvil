577b0546a4d8337e297afb52f3516d3be551366f
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-04-01 03:36:21 -0500

    rename to eventually_stable_reconciliation_holds_per_cr

--- src/controllers/rabbitmq_controller/proof/liveness/proof.rs
+++ src/controllers/rabbitmq_controller/proof/liveness/proof.rs
@@ -32,7 +32,7 @@ use vstd::{prelude::*, string::*};
 
 verus! {
 
-proof fn liveness_proof(spec: TempPred<ClusterState>, cluster: Cluster, controller_id: int, rabbitmq: RabbitmqClusterView)
+proof fn eventually_stable_reconciliation_holds_per_cr(spec: TempPred<ClusterState>, cluster: Cluster, controller_id: int, rabbitmq: RabbitmqClusterView)
     requires
         spec.entails(lift_state(cluster.init())),
         spec.entails(next_with_wf(cluster, controller_id)),
