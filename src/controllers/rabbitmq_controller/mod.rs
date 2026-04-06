9ce18bccedb385e7479fae56f9f5d27ab39f057a
Catoverflow <55503205+Catoverflow@users.noreply.github.com>
2026-04-06 12:49:40 -0500

    replace placeholder with spec

--- src/controllers/rabbitmq_controller/mod.rs
+++ src/controllers/rabbitmq_controller/mod.rs
@@ -7,4 +7,4 @@ pub mod model;
 pub mod proof;
 pub mod trusted;
 
-verus! { #[verifier(external_body)] proof fn trivial() {} } // makes verus recognize this as a mod
+verus! { spec fn trivial() ->bool {true} } // makes verus recognize this as a mod
