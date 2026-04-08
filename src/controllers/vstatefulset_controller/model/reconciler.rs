b16b70b2da03b67093bd05c28b9429e048b17b69
Nikhil Date <47920034+NikhilSDate@users.noreply.github.com>
2026-04-08 11:08:53 -0500

    update_storage (#783)

--- src/controllers/vstatefulset_controller/model/reconciler.rs
+++ src/controllers/vstatefulset_controller/model/reconciler.rs
@@ -739,31 +739,44 @@ pub open spec fn make_pvcs(vsts: VStatefulSetView, ordinal: nat) -> Seq<Persiste
     }
 }
 
+pub open spec fn vol_not_in_pvc_templates(vol: VolumeView, templates: Seq<PersistentVolumeClaimView>) -> bool {
+    forall |k: int| #![trigger templates[k]] 0 <= k < templates.len() ==> vol.name != templates[k].metadata.name->0
+}
+
+pub open spec fn volume_filter(templates: Seq<PersistentVolumeClaimView>) -> spec_fn(VolumeView) -> bool {
+    |vol: VolumeView| vol_not_in_pvc_templates(vol, templates)
+}
+
 pub open spec fn update_storage(vsts: VStatefulSetView, pod: PodView, ordinal: nat) -> PodView {
-    let pvcs = make_pvcs(vsts, ordinal);
-    let templates = vsts.spec.volume_claim_templates->0;
-    let current_volumes = if pod.spec->0.volumes is Some { pod.spec->0.volumes->0 } else { Seq::<VolumeView>::empty() };
-    let new_volumes = if vsts.spec.volume_claim_templates is Some {
-        Seq::new(templates.len(), |i| VolumeView {
-            name: templates[i].metadata.name->0,
-            persistent_volume_claim: Some(PersistentVolumeClaimVolumeSourceView {
-                claim_name: pvcs[i].metadata.name->0,
-                read_only: Some(false),
-            }),
-            ..VolumeView::default()
-        })
+    if pod.spec is None {
+        pod
     } else {
-        Seq::empty()
-    };
-    // We only want to keep the current volumes whose names don't appear in templates
-    let filtered_current_volumes = current_volumes
-        .filter(|vol: VolumeView| templates.all(|template: PersistentVolumeClaimView| vol.name != template.metadata.name->0));
-    PodView {
-        spec: Some(PodSpecView {
-            volumes: Some(new_volumes.add(filtered_current_volumes)),
-            ..pod.spec->0
-        }),
-        ..pod
+        let pvcs = make_pvcs(vsts, ordinal);
+        let templates = if vsts.spec.volume_claim_templates is Some { vsts.spec.volume_claim_templates->0 } else { Seq::empty() };
+        let current_volumes = if pod.spec->0.volumes is Some { pod.spec->0.volumes->0 } else { Seq::<VolumeView>::empty() };
+        let new_volumes = if vsts.spec.volume_claim_templates is Some {
+            Seq::new(templates.len(), |i| VolumeView {
+                name: templates[i].metadata.name->0,
+                persistent_volume_claim: Some(PersistentVolumeClaimVolumeSourceView {
+                    claim_name: pvcs[i].metadata.name->0,
+                    read_only: Some(false),
+                }),
+                ..VolumeView::default()
+            })
+        } else {
+            Seq::empty()
+        };
+
+        // We only want to keep the current volumes whose names don't appear in templates
+        let filtered_current_volumes = current_volumes.filter(volume_filter(templates));
+
+        PodView {
+            spec: Some(PodSpecView {
+                volumes: Some(new_volumes.add(filtered_current_volumes)),
+                ..pod.spec->0
+            }),
+            ..pod
+        }
     }
 }
 
